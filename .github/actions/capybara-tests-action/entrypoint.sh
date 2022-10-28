fad0b5281ad97c530ec309786a82126e62dcff46
Tom Iles <tom.iles@engineering.digital.dwp.gov.uk>
2022-10-28 16:47:41 +0100

    Add github rspec formatter

--- .github/actions/capybara-tests-action/entrypoint.sh
+++ .github/actions/capybara-tests-action/entrypoint.sh
@@ -6,5 +6,4 @@ echo "Starting X virtual framebuffer (Xvfb) for $SCREEN screen in background..."
 Xvfb -ac :99 -screen 0 $SCREEN > /dev/null 2>&1 &
 export DISPLAY=:99
 
-# exec "$@"
 bash -c "set -e;  set -o pipefail; $1"
