30b998176782a0692ac2acf0b37b835523c61225
Tom Iles <tom.iles@engineering.digital.dwp.gov.uk>
2022-10-28 14:34:10 +0100

    WIP: Test capybara test action

--- .github/actions/capybara-tests-action/entrypoint.sh
+++ .github/actions/capybara-tests-action/entrypoint.sh
@@ -6,5 +6,5 @@ echo "Starting X virtual framebuffer (Xvfb) for $SCREEN screen in background..."
 Xvfb -ac :99 -screen 0 $SCREEN > /dev/null 2>&1 &
 export DISPLAY=:99
 
-exec "$@"
-
+# exec "$@"
+bash -c "set -e;  set -o pipefail; $1"
--- capybara/run_test.sh
+++ capybara/run_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it --rm --name capybaray -e SIGNON_USERNAME -e SIGNON_PASSWORD -e SIGNON_OTP -v "$PWD":/usr/src/myapp -w /usr/src/myapp $(docker build -q ../.github/workflows/actions/capybara-tests-action/) /bin/bash -c 'bundle && rspec'
+docker run -it --rm --name capybarat -e SIGNON_USERNAME -e SIGNON_PASSWORD -e SIGNON_OTP -v "$PWD":/usr/src/myapp -w /usr/src/myapp $(docker build -q ../.github/actions/capybara-tests-action/) 'bundle install && bundle exec rspec spec/features/runner_spec.rb'
