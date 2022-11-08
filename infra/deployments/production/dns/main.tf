04567b9a7b7760c0c37db27d3cfdbf58d614cc34
alice-carr <alice.bc.carr@gmail.com>
2022-11-08 16:44:01 +0000

    add staging records to prod dns

--- infra/deployments/production/dns/main.tf
+++ infra/deployments/production/dns/main.tf
@@ -16,3 +16,15 @@ resource "aws_route53_record" "delegate_dev_domain" {
   ]
 }
 
+resource "aws_route53_record" "delegate_staging_domain" {
+  zone_id = data.aws_route53_zone.public.id
+  name    = "stage.forms.service.gov.uk."
+  type    = "NS"
+  ttl     = 60
+  records = [
+  "ns-1270.awsdns-30.org",
+  "ns-1681.awsdns-18.co.uk",
+  "ns-201.awsdns-25.com",
+  "ns-765.awsdns-31.net",
+  ]
+}
