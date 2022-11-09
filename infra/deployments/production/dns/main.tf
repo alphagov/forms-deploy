ced3192d0558a4ec9206a47821225ce657034823
Dan Worth <dan.worth@digital.cabinet-office.gov.uk>
2022-11-09 08:08:36 +0000

    Fix Linting

--- infra/deployments/production/dns/main.tf
+++ infra/deployments/production/dns/main.tf
@@ -22,9 +22,9 @@ resource "aws_route53_record" "delegate_staging_domain" {
   type    = "NS"
   ttl     = 60
   records = [
-  "ns-1270.awsdns-30.org",
-  "ns-1681.awsdns-18.co.uk",
-  "ns-201.awsdns-25.com",
-  "ns-765.awsdns-31.net",
+    "ns-1270.awsdns-30.org",
+    "ns-1681.awsdns-18.co.uk",
+    "ns-201.awsdns-25.com",
+    "ns-765.awsdns-31.net",
   ]
 }
