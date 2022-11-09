9f3ba97b6d8b8f015520794d6fd2e061a8019901
Dan Worth <dan.worth@digital.cabinet-office.gov.uk>
2022-11-09 14:11:26 +0000

    Add NAT Gateways
    Public NAT Gateways allow the private ECS task ENIs to reach the public
    internet which is necessary to connect to Notify.
    
    Deploy a NAT Gateway per availability zone (AZ) and route traffic from
    each AZ's private subnet to the corresponding NAT Gateway for that
    AZ. This ensures an AZ independent setup as per AWS guidance.

--- infra/modules/environment/private-subnets.tf
+++ infra/modules/environment/private-subnets.tf
@@ -1,10 +1,3 @@
-resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.forms.id
-  tags = {
-    Name = "private-${var.env_name}"
-  }
-}
-
 resource "aws_subnet" "private_a" {
   vpc_id            = aws_vpc.forms.id
   cidr_block        = "10.10.4.0/24"
@@ -12,6 +5,24 @@ resource "aws_subnet" "private_a" {
   tags = {
     Name = "private-a-${var.env_name}"
   }
+}
+
+resource "aws_route_table" "private_a" {
+  vpc_id = aws_vpc.forms.id
+  tags = {
+    Name = "private-${var.env_name}"
+  }
+}
+
+resource "aws_route_table_association" "private_a" {
+  subnet_id      = aws_subnet.private_a.id
+  route_table_id = aws_route_table.private_a.id
+}
+
+resource "aws_route" "to_nat_gateway_a" {
+  route_table_id         = aws_route_table.private_a.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat_a.id
 }
 
 resource "aws_subnet" "private_b" {
@@ -23,6 +34,24 @@ resource "aws_subnet" "private_b" {
   }
 }
 
+resource "aws_route_table" "private_b" {
+  vpc_id = aws_vpc.forms.id
+  tags = {
+    Name = "private-b-${var.env_name}"
+  }
+}
+
+resource "aws_route_table_association" "private_b" {
+  subnet_id      = aws_subnet.private_b.id
+  route_table_id = aws_route_table.private_b.id
+}
+
+resource "aws_route" "to_nat_gateway_b" {
+  route_table_id         = aws_route_table.private_b.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat_b.id
+}
+
 resource "aws_subnet" "private_c" {
   vpc_id            = aws_vpc.forms.id
   cidr_block        = "10.10.6.0/24"
@@ -32,18 +61,21 @@ resource "aws_subnet" "private_c" {
   }
 }
 
-resource "aws_route_table_association" "private_a" {
-  subnet_id      = aws_subnet.private_a.id
-  route_table_id = aws_route_table.private.id
-}
-
-resource "aws_route_table_association" "private_b" {
-  subnet_id      = aws_subnet.private_b.id
-  route_table_id = aws_route_table.private.id
+resource "aws_route_table" "private_c" {
+  vpc_id = aws_vpc.forms.id
+  tags = {
+    Name = "private-c-${var.env_name}"
+  }
 }
 
 resource "aws_route_table_association" "private_c" {
   subnet_id      = aws_subnet.private_c.id
-  route_table_id = aws_route_table.private.id
+  route_table_id = aws_route_table.private_c.id
+}
+
+resource "aws_route" "to_nat_gateway_c" {
+  route_table_id         = aws_route_table.private_c.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat_c.id
 }
 
