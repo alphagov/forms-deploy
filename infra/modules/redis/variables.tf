3074c85c35de1c2858fcf0a8d72652007ceb3212
Dan Worth <dan.worth@digital.cabinet-office.gov.uk>
2022-11-09 15:45:17 +0000

    Add ElastiCache Redis to Development
    Creates a common Elasticache module to provide a replication group. The
    default parameters will provide three nodes across each of the three
    availability zones within eu-west-2.
    
    Create a deployment to deploy the Redis module to the development
    environment.

---
+++ infra/modules/redis/variables.tf
@@ -0,0 +1,74 @@
+variable "env_name" {
+  type = string
+
+  validation {
+    condition     = contains(["dev", "staging", "prod"], var.env_name)
+    error_message = "Valid values for env_name are: dev, staging, prod"
+  }
+}
+
+variable "redis_version" {
+  type    = string
+  default = "3.2.6"
+}
+
+variable "apply_immediately" {
+  type        = bool
+  description = "If false then changes are applied during the maintenance window"
+  default     = true
+}
+
+variable "availability_zones" {
+  type        = list(string)
+  description = "The availbility zones to run the redis cluster in"
+  default     = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
+}
+
+variable "engine_version" {
+  default     = "6.2"
+  description = "The version of Redis to use"
+}
+
+variable "snapshot_retention_limit" {
+  default = 30
+}
+
+variable "number_cache_clusters" {
+  type        = string
+  description = "How many cache clusters to run, must >= number of AZs"
+  default     = 3
+}
+
+variable "automatic_failover_enabled" {
+  default     = true
+  description = "If the primary node fails it will failover to a replica"
+}
+
+variable "parameter_group_family" {
+  default     = "redis6.x"
+  description = "Must be compatible with the major redis engine version"
+}
+
+variable "redis_parameters" {
+  description = "Redis parameters to change from the defaults"
+  type        = list(map(any))
+  default     = []
+}
+
+variable "redis_node_type" {
+  type        = string
+  description = "Controls the type and size of the instances which each node runs on"
+  default     = "cache.t3.micro"
+}
+
+variable "redis_maintenance_window" {
+  type        = string
+  default     = "mon:06:00-mon:07:00"
+  description = "When planned maintenance will take place such as minor version upgardes"
+}
+
+variable "redis_snapshot_window" {
+  description = "The daily time range (in UTC) during which ElastiCache will begin taking a daily snapshot of your cache cluster. The minimum snapshot window is a 60 minute period"
+  type        = string
+  default     = "04:30-05:30"
+}
