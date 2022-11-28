39158a96b617df243ae7d8f9be0d911a66a81226
Dan Worth <dan.worth@digital.cabinet-office.gov.uk>
2022-11-28 08:22:30 +0000

    Engineer Access to Forms Deploy Account
    Initial engineer access to the GOV.UK Forms Deploy account. This account
    will be used to deploy components to development, staging and production
    accounts. Admin access granted to Dan and Alice whilst they setup the
    environment. More granular roles to be explored before going live.

---
+++ infra/deployments/deploy/engineer-access/roles.tf
@@ -0,0 +1,29 @@
+locals {
+  # Users must have an IAM user within the gds-users account before they can be
+  # given access to the GOV.UK Forms accounts. To request an IAM user:
+  # https://gds-request-an-aws-account.cloudapps.digital/user
+
+  # The deploy account has access to deploy to production. As always access
+  # should be based on least privilege for a given user to perform their duties
+  admins = [
+    "alice.carr", # Admin whilst setting up the deploy account
+    "dan.worth",  # Admin whilst setting up the deploy account
+  ]
+
+  readonly = [
+    "alice.carr",
+    "alistair.laing",
+    "dan.worth",
+    "david.biddle",
+    "james.sheppard",
+    "samuel.culley",
+    "tom.iles",
+    "tristram.oaten"
+  ]
+}
+
+module "engineer_access" {
+  source   = "../../../modules/engineer-access"
+  admins   = local.admins
+  readonly = local.readonly
+}
