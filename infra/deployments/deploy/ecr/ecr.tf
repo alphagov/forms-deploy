21373455e2ae5b58e2954675ea7f779678b1d5af
alice-carr <alice.carr@digital.cabinet-office.gov.uk>
2022-11-29 10:43:37 +0000

    add policies to ecr repos to allow dev ecs roles to pull them

--- infra/deployments/deploy/ecr/ecr.tf
+++ infra/deployments/deploy/ecr/ecr.tf
@@ -21,3 +21,75 @@ resource "aws_ecr_repository" "forms_admin" {
     scan_on_push = true
   }
 }
+
+resource "aws_ecr_repository_policy" "aws_ecr_repository_policy_api" {
+  repository = aws_ecr_repository.forms_api.name
+
+  policy = <<EOF
+{
+    "Version": "2008-10-17",
+    "Statement": [
+        {
+            "Sid": "AllowPull",
+            "Effect": "Allow",
+            "Principal": {
+              "AWS": "arn:aws:iam::498160065950:role/dev-forms-api-ecs-task-execution"
+              },
+            "Action": [
+                "ecr:GetDownloadUrlForLayer",
+                "ecr:BatchGetImage",
+                "ecr:BatchCheckLayerAvailability"
+            ]
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_ecr_repository_policy" "aws_ecr_repository_policy_admin" {
+  repository = aws_ecr_repository.forms_admin.name
+
+  policy = <<EOF
+{
+    "Version": "2008-10-17",
+    "Statement": [
+        {
+            "Sid": "AllowPull",
+            "Effect": "Allow",
+            "Principal": {
+              "AWS": "arn:aws:iam::498160065950:role/dev-forms-admin-ecs-task-execution"
+              },
+            "Action": [
+                "ecr:GetDownloadUrlForLayer",
+                "ecr:BatchGetImage",
+                "ecr:BatchCheckLayerAvailability"
+            ]
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_ecr_repository_policy" "aws_ecr_repository_policy_runner" {
+  repository = aws_ecr_repository.forms_runner.name
+
+  policy = <<EOF
+{
+    "Version": "2008-10-17",
+    "Statement": [
+        {
+            "Sid": "AllowPull",
+            "Effect": "Allow",
+            "Principal": {
+              "AWS": "arn:aws:iam::498160065950:role/dev-forms-runner-ecs-task-execution"
+              },
+            "Action": [
+                "ecr:GetDownloadUrlForLayer",
+                "ecr:BatchGetImage",
+                "ecr:BatchCheckLayerAvailability"
+            ]
+        }
+    ]
+}
+EOF
+}
