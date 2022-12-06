a7cae735cf8f78adb53d0fad64c9dc64fc323a8a
alice-carr <alice.carr@digital.cabinet-office.gov.uk>
2022-12-06 11:43:13 +0000

    add rds to staging environment

---
+++ infra/deployments/staging/rds/site.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_version = "= 1.2.8"
+
+  required_providers {
+    aws = ">= 4.37.0"
+  }
+
+  # Comment out when bootstrapping
+  backend "s3" {
+    bucket = "gds-forms-staging-tfstate"
+    key    = "rds.tfstate"
+    region = "eu-west-2"
+  }
+}
+
+provider "aws" {
+  allowed_account_ids = ["972536609845"]
+
+  default_tags {
+    tags = {
+      Environment = "staging"
+      Deployment  = "staging/rds"
+    }
+  }
+}
+
