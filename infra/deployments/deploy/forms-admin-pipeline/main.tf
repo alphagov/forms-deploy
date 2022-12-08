66536c6c24f5378c9dbeae44126dc847eba66ce3
Dan Worth <dan.worth@digital.cabinet-office.gov.uk>
2022-12-08 14:50:48 +0000

    Deploy Forms-API and Forms-Admin to Staging Environment
    Deploy forms-api and forms-admin to staging environment whenever a
    changs is merged to main branch.

--- infra/deployments/deploy/forms-admin-pipeline/main.tf
+++ infra/deployments/deploy/forms-admin-pipeline/main.tf
@@ -1,8 +1,6 @@
 module "pipeline" {
   source = "../../../modules/app-deploy-pipeline"
 
-  development_deployer_role_arn = "arn:aws:iam::498160065950:role/deployer-dev"
-
   app_name            = "forms-admin"
   forms_deploy_branch = "code_pipeline"
 }
--- infra/deployments/staging/deployer-access/main.tf
+++ infra/deployments/staging/deployer-access/main.tf
@@ -3,10 +3,9 @@ module "deployer_access" {
   env_name = "staging"
 
   codebuild_terraform_arns = [
-    #    "arn:aws:iam::711966560482:role/codebuild-forms-api-deploy-staging",
-    #    "arn:aws:iam::711966560482:role/codebuild-forms-admin-deploy-staging",
-    "arn:aws:iam::711966560482:role/codebuild-forms-runner-deploy-staging",
-    "arn:aws:iam::498160065950:role/dan.worth-admin"
+    "arn:aws:iam::711966560482:role/codebuild-forms-api-deploy-staging",
+    "arn:aws:iam::711966560482:role/codebuild-forms-admin-deploy-staging",
+    "arn:aws:iam::711966560482:role/codebuild-forms-runner-deploy-staging"
   ]
 
 }
