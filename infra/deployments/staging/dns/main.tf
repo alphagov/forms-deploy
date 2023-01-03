29753b35a4fcf8e6d0dd239b448401277895ec58
Dan Worth <dan.worth@digital.cabinet-office.gov.uk>
2023-01-03 14:23:24 +0000

    Checkov DNS
    Skip:
    - DNS query logging because it isn't necessary
    - DNSSEC because any client who's DNS request is spoofed by
      person-in-the-middle attacks and is served a malicious IP would
      subsequently validate the server certificate returned by the malicious
      server. As such an attacker would need a valid x509 cert for our
      domain which reduces the likelihood of this attach.

--- infra/deployments/staging/dns/main.tf
+++ infra/deployments/staging/dns/main.tf
@@ -1,4 +1,6 @@
 resource "aws_route53_zone" "public" {
+  #checkov:skip=CKV2_AWS_38:DNSSEC is not currently necessary
+  #checkov:skip=CKV2_AWS_39:DNS query logging not necessary
   name = "stage.forms.service.gov.uk."
 }
 
