97ffdf0644300688f8fb28ab9d16d55fb750a92b
alice-carr <alice.carr@digital.cabinet-office.gov.uk>
2023-01-09 15:43:52 +0000

    add csls log subscription filter to ecs

--- infra/modules/ecs-service/logging.tf
+++ infra/modules/ecs-service/logging.tf
@@ -4,3 +4,9 @@ resource "aws_cloudwatch_log_group" "log" {
   retention_in_days = 14
 }
 
+resource "aws_cloudwatch_log_subscription_filter" "csls_log_subscription" {
+  name            = "csls_log_subscription"
+  log_group_name  = "${var.application}-${var.env_name}"
+  filter_pattern  = ""
+  destination_arn = "arn:aws:logs:eu-west-2:885513274347:destination:csls_cw_logs_destination_prodpython"
+}
