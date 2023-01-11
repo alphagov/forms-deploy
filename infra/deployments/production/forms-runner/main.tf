b75db8eb8b9fb17b9ccb8c9ddcda3609c493a63d
Dan Worth <dan.worth@digital.cabinet-office.gov.uk>
2023-01-11 12:43:40 +0000

    Deploy Forms-Runner to Production
    Allow forms-runner task execution role to pull images form deploy
    forms-runner ECR registry.
    
    Run forms-runner with minimal capacity whilst building the environment.

---
+++ infra/deployments/production/forms-runner/main.tf
@@ -0,0 +1,14 @@
+variable "image_tag" {
+  type        = string
+  description = "The image tag to deploy"
+}
+
+module "forms_runner" {
+  source             = "../../../modules/forms-runner"
+  env_name           = "production"
+  image_tag          = var.image_tag
+  desired_task_count = 1 #TODO: update before migration
+  cpu                = 256
+  memory             = 512
+  api_base_url       = "https://api.prod-temp.forms.service.gov.uk" #TODO: update before migration
+}
---
+++ infra/deployments/production/forms-runner/site.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = "= 1.2.8"
+
+  required_providers {
+    aws = ">= 4.37.0"
+  }
+
+  # Comment out when bootstrapping
+  backend "s3" {
+    bucket = "gds-forms-production-tfstate"
+    key    = "forms_runner.tfstate"
+    region = "eu-west-2"
+  }
+}
+
+provider "aws" {
+  allowed_account_ids = ["443944947292"]
+
+  default_tags {
+    tags = {
+      Environment = "production"
+      Deployment  = "production/forms-runner"
+    }
+  }
+}
+
+
