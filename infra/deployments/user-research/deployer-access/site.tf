341f7927b485ec7a92e8d2936fb2a86086a0d273
Dan Worth <dan.worth@digital.cabinet-office.gov.uk>
2023-01-17 14:58:22 +0000

    Deploy Applications To User Research
    After deploying to development environment update the application in the
    User Research environment.
    
    This will keep the environment current. The automatic deployment can be
    disabled if necessary. Smoke tests are not included because we need to
    implement basic auth and update the end-to-end tests first (if we feel
    running the end-to-end post deployment into user research is necessary
    in future).

---
+++ infra/deployments/user-research/deployer-access/site.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = "= 1.2.8"
+
+  backend "s3" {
+    bucket = "gds-forms-user-research-tfstate"
+    key    = "deployer_access.tfstate"
+    region = "eu-west-2"
+  }
+}
+
+provider "aws" {
+  allowed_account_ids = ["619109835131"]
+
+  default_tags {
+    tags = {
+      Environment = "user-research"
+      Deployment  = "user-research/deployer_access"
+    }
+  }
+}
+
+
+
