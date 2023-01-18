88311b2e15a13a28063a5d3e6c9bf01caeeddc6a
Dan Worth <dan.worth@digital.cabinet-office.gov.uk>
2023-01-18 08:52:48 +0000

    Script To Build and Upload Application Docker Image
    Occassionally we will need to deploy local source code for an
    application into the development or user-research environment. To
    achieve this we must:
    1. build the local source code into a Docker image and push it into the
       ECR registry within our deploy account.
    2. Apply the application Terraform to update the ECS service in the
       choosen environment, either development or user-research, to use the
       new image.
    
    This script helps to accomplish the first task which is multistep
    process and benefits from being automated. The second step is a simple
    `terraform apply` operation. For now the frequency we'll be pushing
    local source code to either environment is low but we can revisit this
    need at a later date and consider CodePipelines to fully automate
    deploying from development branches.

---
+++ infra/scripts/build_and_push_to_ecr.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+APP="$1"
+
+function usage() {
+  echo "
+  Builds the local source code for an application into a Docker image and
+  pushes into the appropriate ECR repository in the deploy account.
+
+  The image can then be deployed by applying the application Terraform and
+  providing the image tag when prompted. The application Terraform can be found under
+  forms-deploy/infra/deployments/<environment>/forms-[app|admin|runner]
+
+  Dev images must only be used in Development and User Research environments.
+
+  Local directory structure must be
+  .
+  ├── forms-deploy
+  ├── forms-admin
+  ├── forms-api
+  ├── forms-runner
+
+  Usage:
+     Run in a authorized shell for the deploy account using gds-cli or aws-vault
+     $0 app-name
+
+     app-name, the name of the application to build forms-api | forms-admin | forms-runner
+
+     Example:
+     gds aws gds-forms-deploy-admin -- ${0} forms-api"
+  exit 0
+}
+
+if [ "$APP" != "forms-api" ] && [ "$APP" != "forms-admin" ] && [ "$APP" != "forms-runner" ]; then
+  echo "APP must be either 'forms-api', 'forms-admin' or 'forms-runner'."
+  usage
+fi
+
+if ! docker info > /dev/null 2>&1 ; then
+  echo "Docker is not running. Start Docker and try again. more info here:
+  https://www.docker.com/products/docker-desktop/"
+  exit 1
+fi
+
+echo "Building ${APP} docker image"
+
+APP_DIR="../../../${APP}"
+GIT_SHA="$(git --git-dir "${APP_DIR}/.git" rev-parse HEAD)"
+DEFAULT_TAG_SUFFIX="${GIT_SHA}"
+
+echo "
+All development image tags begin 'dev_'. You can choose the suffix. Image
+tags are immutable and no two images can have the same tag. Images with tags
+beginning 'dev_' will be periodically deleted from ECR.
+"
+
+read -rp "Enter tag suffix (defaults to GIT SHA): [${DEFAULT_TAG_SUFFIX}]: " TAG_SUFFIX
+TAG_SUFFIX=${TAG_SUFFIX:-${DEFAULT_TAG_SUFFIX}}
+TAG="dev_${TAG_SUFFIX}"
+
+docker build "${APP_DIR}" -t "${APP}:${TAG}"
+
+echo "Logging into Deploy ECR"
+aws ecr get-login-password --region eu-west-2 \
+  | docker login --username AWS --password-stdin 711966560482.dkr.ecr.eu-west-2.amazonaws.com
+
+docker tag "${APP}:${TAG}" "711966560482.dkr.ecr.eu-west-2.amazonaws.com/${APP}-deploy:${TAG}"
+
+docker push "711966560482.dkr.ecr.eu-west-2.amazonaws.com/${APP}-deploy:${TAG}"
+
