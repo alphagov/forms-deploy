d66f6e6c08edf688f9ce917322327889bea0f14e
Alistair Laing <aliuk2012@users.noreply.github.com>
2023-01-18 10:20:55 +0000

    Add SETTINGS__GOVUK_NOTIFY__API_KEY to forms-runner AWS

--- infra/modules/forms-runner/main.tf
+++ infra/modules/forms-runner/main.tf
@@ -67,6 +67,10 @@ module "ecs_service" {
       valueFrom = "arn:aws:ssm:eu-west-2:${data.aws_caller_identity.current.account_id}:parameter/forms-runner-${var.env_name}/notify-api-key"
     },
     {
+      name      = "SETTINGS__GOVUK_NOTIFY__API_KEY",
+      valueFrom = "arn:aws:ssm:eu-west-2:${data.aws_caller_identity.current.account_id}:parameter/forms-runner-${var.env_name}/notify-api-key"
+    },
+    {
       name      = "SENTRY_DSN",
       valueFrom = "arn:aws:ssm:eu-west-2:${data.aws_caller_identity.current.account_id}:parameter/forms-runner-${var.env_name}/sentry/dsn"
     }
