fc7ab74fe87681a625e2dfedf8e56f85ed0f7ca3
alice-carr <alice.carr@digital.cabinet-office.gov.uk>
2023-01-30 15:58:03 +0000

    add in sqs queue and bucket notification

---
+++ infra/modules/environment/sqs.tf
@@ -0,0 +1,41 @@
+resource "aws_sqs_queue" "sqs" {
+  name                        = "sqs-${var.env_name}"
+  visibility_timeout_seconds  = 90
+  redrive_policy             = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.sqs_deadletter.arn}\",\"maxReceiveCount\":1}"
+}
+
+resource "aws_sqs_queue" "sqs_deadletter" {
+  name = "sqs-deadletter-${var.env_name}"
+}
+
+resource "aws_sqs_queue_policy" "sqs_policy" {
+  queue_url = aws_sqs_queue.sqs.id
+
+  policy = data.aws_iam_policy_document.queue_policy.json
+}
+
+data "aws_iam_policy_document" "queue_policy" {
+
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+    actions   = ["sqs:SendMessage"]
+    resources = [aws_sqs_queue.sqs.arn]
+    condition {
+      test     = "ArnEquals"
+      variable = "aws:SourceArn"
+      values   = [module.logs_bucket.arn]
+    }
+  }
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = module.logs_bucket.name
+  queue {
+    queue_arn     = aws_sqs_queue.sqs.arn
+    events        = ["s3:ObjectCreated:*"]
+  }
+}
