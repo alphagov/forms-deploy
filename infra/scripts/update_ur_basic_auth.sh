0ac5f5d129bc397805e5125a10d88a528b391a1a
Dan Worth <dan.worth@digital.cabinet-office.gov.uk>
2023-02-16 08:21:25 +0000

    Helper Script to Update Basic Auth Creds in User Research
    We need to periodically update the basic auth username and password in
    the user-research environment. These credentials are held in AWS SSM
    Parameter Store and retrieved by AWS ECS each time it launches a new
    task for forms-admin.
    
    This script makes updating the username and password simpler by updating
    Parameter Store values and then redeploying forms-admin in the
    user-research environment.

---
+++ infra/scripts/update_ur_basic_auth.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+set -e
+
+if [[ -z "$1" ]] || [[ -z "$2" ]]; then
+  echo "
+  Update the basic auth username and password and redeploy forms-admin in the
+  user-research environment.
+
+  You will need admin permission in the user-research AWS account and be
+  connected to the VPN.
+
+  Usage:
+     $0 new-username new-password
+
+     Example:
+     ${0} 'user' 'top-secret'
+   "
+  exit 0
+fi
+
+if ! command -v jq &> /dev/null; then
+  echo "Install 'jq' before proceeding. Using brew run:
+  brew install jq
+  "
+  exit 1
+fi
+
+NEW_USERNAME="$1"
+NEW_PASSWORD="$2"
+
+echo "Updating username"
+gds-cli aws gds-forms-user-research-admin -- \
+  aws ssm put-parameter \
+    --type SecureString \
+    --name "/forms-admin-user-research/basic-auth/username" \
+    --value "${NEW_USERNAME}" \
+    --overwrite
+
+echo "Updating password"
+gds-cli aws gds-forms-user-research-admin -- \
+  aws ssm put-parameter \
+    --type SecureString \
+    --name "/forms-admin-user-research/basic-auth/password" \
+    --value "${NEW_PASSWORD}" \
+    --overwrite
+
+echo "Redeploying forms-admin"
+gds-cli aws gds-forms-user-research-admin -- \
+  aws ecs update-service \
+  --service "forms-admin" \
+  --cluster "forms-user-research" \
+  --force-new-deployment \
+  --no-cli-pager > /dev/null
+
+echo "Redeploy instruction sent. Wait for update to finish"
+gds-cli aws gds-forms-user-research-admin -- \
+  ../../infra/modules/code-build-deploy-ecs/scripts/wait-for-deploy.sh "forms-admin" "forms-user-research"
+
