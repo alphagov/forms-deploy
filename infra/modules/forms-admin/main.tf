0347d55fd5cf28d1cdb97c1292af4f2a4161d345
Alistair Laing <aliuk2012@users.noreply.github.com>
2023-02-25 09:17:03 +0000

    Add settings for forms_api__auth_key

--- infra/modules/forms-admin/main.tf
+++ infra/modules/forms-admin/main.tf
@@ -81,6 +81,10 @@ module "ecs_service" {
       valueFrom = "arn:aws:ssm:eu-west-2:${data.aws_caller_identity.current.account_id}:parameter/forms-admin-${var.env_name}/forms-api-key"
     },
     {
+      name      = "SETTINGS__FORMS_API__AUTH_KEY",
+      valueFrom = "arn:aws:ssm:eu-west-2:${data.aws_caller_identity.current.account_id}:parameter/forms-admin-${var.env_name}/forms-api-key"
+    },
+    {
       name      = "SETTINGS__GOVUK_NOTIFY__API_KEY",
       valueFrom = "arn:aws:ssm:eu-west-2:${data.aws_caller_identity.current.account_id}:parameter/forms-admin-${var.env_name}/notify-api-key"
     },
