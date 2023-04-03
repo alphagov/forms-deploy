472a60fcca74972ade6fcd120ef7a68d503db52b
Dan Worth <dan.worth@digital.cabinet-office.gov.uk>
2023-04-03 11:19:30 +0100

    Correct Engineer IAM Access
    This fixes a few issues with the original commit to move the
    gds-users-role assume role policy to the clearer
    `aws_iam_policy_document` resource. There are two conditions
    - the user has MFA which is mandatory
    - the source IP is in an allowed list, this is optional per environment
      and we do not enforce it for development. This condition is therefore
      only added when necessary using a Terraform dynamic block.
    
      When planned against our environments Terraform reports no changes
      necessary as expected.

--- infra/modules/gds-user-role/iam.tf
+++ infra/modules/gds-user-role/iam.tf
@@ -4,28 +4,20 @@ locals {
   role_name       = "${local.role_prefix}-${var.role_suffix}"
   trust_principal = "arn:aws:iam::622626885786:user/${var.email}"
 
-  gds_ip_restriction_policy_fragment = <<-EOF
-  "IpAddress": {
-    "aws:SourceIp": [
-      "213.86.153.211/32",
-      "213.86.153.212/32",
-      "213.86.153.213/32",
-      "213.86.153.214/32",
-      "213.86.153.231/32",
-      "213.86.153.235/32",
-      "213.86.153.236/32",
-      "213.86.153.237/32",
-      "51.149.8.0/25",
-      "51.149.8.128/29",
-      "51.149.9.112/29",
-      "51.149.9.240/29"
-    ]
-  },
-  EOF
-
-  maybe_source_ip_restriction = (var.restrict_to_gds_ips
-    ? local.gds_ip_restriction_policy_fragment
-  : "")
+  permitted_source_ips = [
+    "213.86.153.211/32",
+    "213.86.153.212/32",
+    "213.86.153.213/32",
+    "213.86.153.214/32",
+    "213.86.153.231/32",
+    "213.86.153.235/32",
+    "213.86.153.236/32",
+    "213.86.153.237/32",
+    "51.149.8.0/25",
+    "51.149.8.128/29",
+    "51.149.9.112/29",
+    "51.149.9.240/29"
+  ]
 }
 
 resource "aws_iam_role" "gds_user_role" {
@@ -41,15 +33,25 @@ data "aws_iam_policy_document" "assume_role" {
     actions = [
       "sts:AssumeRole"
     ]
-    Principals {
+    principals {
       type        = "AWS"
       identifiers = [local.trust_principal]
     }
     effect = "Allow"
+
     condition {
       test     = "Bool"
-      values   = [local.maybe_source_ip_restriction]
+      values   = ["true"]
       variable = "aws:MultiFactorAuthPresent"
+    }
+
+    dynamic "condition" {
+      for_each = var.restrict_to_gds_ips ? [1] : []
+      content {
+        test     = "IpAddress"
+        values   = local.permitted_source_ips
+        variable = "aws:SourceIp"
+      }
     }
   }
 }
