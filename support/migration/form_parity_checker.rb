629375854134af475002a3890a59ca903c15e2e0
Catalina Garcia <catalina.garcia@digital.cabinet-office.gov.uk>
2023-04-26 10:47:45 +0100

    Rename function and add comments

--- support/migration/form_parity_checker.rb
+++ support/migration/form_parity_checker.rb
@@ -47,7 +47,7 @@ def extract_form_names(forms)
   end
 end
 
-def compare_forms(aws_forms, paas_forms)
+def check_forms_exist(aws_forms, paas_forms)
   total_forms = Hash.new do |hash, key|
     hash[key] = {
       aws: false,
@@ -56,11 +56,11 @@ def compare_forms(aws_forms, paas_forms)
   end
 
   aws_forms.each do |form|
-    total_forms[form['name']][:aws] = true
+    total_forms[form['id']][:aws] = true
   end
 
   paas_forms.each do |form|
-    total_forms[form['name']][:paas] = true
+    total_forms[form['id']][:paas] = true
   end
   total_forms
 end
@@ -77,9 +77,18 @@ puts "Checking parity of #{environment} forms".green
 aws_forms = list_forms(ENVIRONMENTS[environment.to_sym][:aws], aws_key)
 paas_forms = list_forms(ENVIRONMENTS[environment.to_sym][:paas], paas_key)
 
-results = compare_forms(aws_forms, paas_forms)
-results.each do | form_name, result |
-  puts "#{form_name} paas:#{result[:paas]} aws:#{result[:aws]}"
+count = 0
+results = check_forms_exist(aws_forms, paas_forms)
+# Print all forms, tell us whether or not a form with a given id exists in paas/aws and highlight in red the ones that are missing
+results.each do | form_id, result |
+  if result[:paas] != result[:aws]
+    puts "#{form_id} paas:#{result[:paas]} aws:#{result[:aws]}".red
+    count += 1
+  else
+    puts "#{form_id} paas:#{result[:paas]} aws:#{result[:aws]}"
+  end
 end
 
+puts "All forms are the same!".green if paas_forms == aws_forms
+puts "There are #{count} mismatches between AWS and PaaS"
 
