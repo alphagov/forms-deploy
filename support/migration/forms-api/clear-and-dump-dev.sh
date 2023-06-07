745c0f936949ceb98d91301b43732141cd5c0105
Catalina Garcia <catalina.garcia@digital.cabinet-office.gov.uk>
2023-06-07 10:27:45 +0100

    Generalise scripts and refactor login flow
    We can login to the relevant environments before running the scripts. We can
    use the ENVIRONMENT variable to generalise for multiple environments.

--- support/migration/forms-api/clear-and-dump-dev.sh
+++ support/migration/forms-api/clear-and-dump-dev.sh
@@ -1,15 +1,36 @@
 #!/bin/bash
 
-# Performs the forms-api database migration for the development
-# environment (extract has to be applied via Data API console).
-echo "Clearing dev and generating pg_dump"
-./clear-tables.sh "dev"
-./reset-table-sequences.sh "dev"
-./pg-dump-data.sh "dev"
+# Migrates the forms-api database from PaaS to AWS for a given environment.
+# It outputs a pgdump file. The contents have to be manually copied to the AWS RDS Query editor for the corresponding RDS instance.
 
-read -r -p "Once SQL has been run via data api press ENTER or ctl-C to quit"
+# Requires
+# - forms-cli installed and using the alias "forms"
+# - using an authenticated shell for the AWS account you need (via gds-cli or aws-vault)
+
+if [ $# -eq 0 ]; then
+    >&2 echo "No ENVIRONMENT set"
+    exit 1
+fi
+
+if [[ "$(cf target)" =~ "FAILED" ]]; then
+    exit 1
+fi
+
+ENVIRONMENT="$1"
+ORG="gds-govuk-forms"
+SPACE="forms-api-${ENVIRONMENT}"
+
+target_paas=$(cf target -o "${ORG}" -s "${SPACE}")
+
+echo "Clearing ${ENVIRONMENT} and generating pg_dump"
+./clear-tables.sh
+./reset-table-sequences.sh
+./pg-dump-data.sh ${ENVIRONMENT}
+
+read -r -p "Once SQL has been run via the AWS RDS Query editor press ENTER or ctl-C to quit"
 
 echo "continuing"
 
-./reset-table-sequences.sh "dev"
+# The order in the tables matters because we rely on the primary keys to identify the forms.
+./reset-table-sequences.sh
 
