58cfe6fa2fefa7f002330b0c36eea1a260e045cc
Catalina Garcia <catalina.garcia@digital.cabinet-office.gov.uk>
2023-07-03 09:24:03 +0100

    Adds missing flag

--- infra/deployments/staging/forms-admin/main.tf
+++ infra/deployments/staging/forms-admin/main.tf
@@ -14,4 +14,5 @@ module "forms_admin" {
   runner_base                 = "https://submit.staging.forms.service.gov.uk"
   govuk_app_domain            = "staging.publishing.service.gov.uk"
   enable_maintenance_mode     = true
+  secret_kind                 = "perm/"
 }
