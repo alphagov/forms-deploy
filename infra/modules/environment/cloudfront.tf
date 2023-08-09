ad06d89d32cbebd0dd0e327cf40d04ab0599b00a
Dan Worth <dan.worth@digital.cabinet-office.gov.uk>
2023-08-09 15:29:08 +0100

    Allow ACM Creation in Different Regions
    CloudFront requires its certificate to be in us-east-1 whilst the rest
    of our infrastructure and ALB certificate is in eu-west-2. This change
    makes use of Terraform provider functionality by passing multiple aws
    providers to the acm-cert-with-dns-validation so that calling modules
    can control where the certificate is created.
    
    This also solves a dependency issue and calling modules now correctly
    wait until the certificate is validated by making the output depend on
    the aws_acm_certificate_validation resource.

--- infra/modules/environment/cloudfront.tf
+++ infra/modules/environment/cloudfront.tf
@@ -1,6 +1,10 @@
 module "cloudfront" {
   count  = var.enable_cloudfront ? 1 : 0
   source = "../cloudfront"
+  providers = {
+    aws           = aws
+    aws.us-east-1 = aws.us-east-1 # Create the certificate in us-east-1 for CloudFront
+  }
 
   env_name      = var.env_name
   domain_name   = "${lookup(local.domain_names, var.env_name)}forms.service.gov.uk"
@@ -8,8 +12,4 @@ module "cloudfront" {
   ip_rate_limit = var.ip_rate_limit
 
   subject_alternative_names = lookup(local.subject_alternative_names, var.env_name)
-  providers = {
-    aws           = aws
-    aws.us-east-1 = aws.us-east-1
-  }
 }
