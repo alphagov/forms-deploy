de72b102b43a4295532fe7145482c7f4629d1af6
alice-carr <alice.carr@digital.cabinet-office.gov.uk>
2023-08-22 11:39:59 +0100

    update list of application names to include forms-product-page

--- infra/modules/ecs-service/variables.tf
+++ infra/modules/ecs-service/variables.tf
@@ -21,8 +21,8 @@ variable "application" {
   type        = string
   description = "The name of the application e.g. forms-admin"
   validation {
-    condition     = contains(["forms-admin", "forms-runner", "forms-api"], var.application)
-    error_message = "Valid values for application are: forms-admin, forms-runner, forms-api"
+    condition     = contains(["forms-admin", "forms-runner", "forms-api", "forms-product-page"], var.application)
+    error_message = "Valid values for application are: forms-admin, forms-runner, forms-api, forms-product-page"
   }
 }
 
