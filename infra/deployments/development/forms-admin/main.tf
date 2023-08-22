d782f2463a0c8ce892874678823d52159dc61a88
Tom Iles <tom.iles@engineering.digital.dwp.gov.uk>
2023-08-22 14:40:10 +0100

    Change forms-admin-dev to use auth0 login
    A decision has been made to use Auth0 for authentication.
    
    Change the development server to use the auth0 authentication provider
    instead of GDS SSO.
    
    This commit re-applies 5a56be32, we had to revert it because the
    end-to-end tests failed, but we think we fixed those in
    alphagov/forms-e2e-tests#4.

--- infra/deployments/development/forms-admin/main.tf
+++ infra/deployments/development/forms-admin/main.tf
@@ -4,15 +4,17 @@ variable "image_tag" {
 }
 
 module "forms_admin" {
-  source                      = "../../../modules/forms-admin"
-  env_name                    = "dev"
-  image_tag                   = var.image_tag
-  desired_task_count          = 2
-  cpu                         = 256
-  memory                      = 512
-  api_base_url                = "https://api.dev.forms.service.gov.uk"
-  runner_base                 = "https://submit.dev.forms.service.gov.uk"
-  govuk_app_domain            = "integration.publishing.service.gov.uk"
-  enable_maintenance_mode     = false
+  source                        = "../../../modules/forms-admin"
+  env_name                      = "dev"
+  image_tag                     = var.image_tag
+  desired_task_count            = 2
+  cpu                           = 256
+  memory                        = 512
+  api_base_url                  = "https://api.dev.forms.service.gov.uk"
+  runner_base                   = "https://submit.dev.forms.service.gov.uk"
+  govuk_app_domain              = "integration.publishing.service.gov.uk"
+  enable_maintenance_mode       = false
   details_guidance_feature_flag = true
+  auth_provider                 = "auth0"
+  previous_auth_provider        = "gds_sso"
 }
