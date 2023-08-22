20db7e4559815938ef86d0fbeb33e4de21635afe
alice-carr <alice.carr@digital.cabinet-office.gov.uk>
2023-08-22 11:43:53 +0100

    create module for forms-product-page ecs

---
+++ infra/modules/forms-product-page/main.tf
@@ -0,0 +1,46 @@
+data "aws_caller_identity" "current" {}
+
+locals {
+  deploy_account_id = "711966560482"
+}
+
+module "ecs_service" {
+  source                 = "../ecs-service"
+  env_name               = var.env_name
+  application            = "forms-product-page"
+  sub_domain             = "www"
+  desired_task_count     = var.desired_task_count
+  image                  = "${local.deploy_account_id}.dkr.ecr.eu-west-2.amazonaws.com/forms-product-page-deploy:${var.image_tag}"
+  cpu                    = var.cpu
+  memory                 = var.memory
+  container_port         = 9292
+  permit_internet_egress = true # Required for Sentry.io and AWS SSM
+  permit_postgres_egress = true
+
+  environment_variables = [
+    {
+      name  = "RACK_ENV",
+      value = "production"
+    },
+    {
+      name  = "RAILS_ENV",
+      value = "production"
+    },
+    {
+      name  = "RAILS_LOG_TO_STDOUT",
+      value = "true"
+    },
+    {
+      name  = "SETTINGS__FORMS_ENV",
+      value = var.env_name
+    },
+  ]
+
+  secrets = [
+    {
+      name      = "SECRET_KEY_BASE",
+      valueFrom = "arn:aws:ssm:eu-west-2:${data.aws_caller_identity.current.account_id}:parameter/forms-product-page-${var.env_name}/secret-key-base"
+    }
+  ]
+}
+
