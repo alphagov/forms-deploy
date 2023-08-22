7cd9a3dc3f6387e98a33ec47911a923755c5cd93
alice-carr <alice.carr@digital.cabinet-office.gov.uk>
2023-08-22 11:46:14 +0100

    add deployments for forms-product-pages ecs to dev, staging and production

---
+++ infra/deployments/development/forms-product-page/site.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = "= 1.2.8"
+
+  required_providers {
+    aws = ">= 4.37.0"
+  }
+
+  # Comment out when bootstrapping
+  backend "s3" {
+    bucket = "gds-forms-development-tfstate"
+    key    = "forms_product_page.tfstate"
+    region = "eu-west-2"
+  }
+}
+
+provider "aws" {
+  allowed_account_ids = ["498160065950"]
+
+  default_tags {
+    tags = {
+      Environment = "development"
+      Deployment  = "development/forms-product-page"
+    }
+  }
+}
+
+
