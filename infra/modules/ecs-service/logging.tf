e203f537b41ad96e759514aeee039a2cbd96a707
alice-carr <alice.carr@digital.cabinet-office.gov.uk>
2023-08-22 11:41:30 +0100

    add depends on to csls log subscription to ensure log group is created before subscription filter

--- infra/modules/ecs-service/logging.tf
+++ infra/modules/ecs-service/logging.tf
@@ -9,4 +9,7 @@ resource "aws_cloudwatch_log_subscription_filter" "csls_log_subscription" {
   log_group_name  = "${var.application}-${var.env_name}"
   filter_pattern  = ""
   destination_arn = "arn:aws:logs:eu-west-2:885513274347:destination:csls_cw_logs_destination_prodpython"
+  depends_on = [
+    aws_cloudwatch_log_group.log
+  ]
 }
