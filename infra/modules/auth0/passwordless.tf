f190a573c4a75f10aa829698a2c5c4162e1ff8a8
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2023-09-07 14:54:06 +0300

    Add content for Auth0 passwordless email template
    Currently we're just using the default passwordless email content, but
    we will want to change the content in future, so we should capture the
    current state in Terraform.

--- infra/modules/auth0/passwordless.tf
+++ infra/modules/auth0/passwordless.tf
@@ -1,9 +1,27 @@
+locals {
+  en = {
+    email_templates = {
+      passwordless_email = merge(
+        jsondecode(file("${path.module}/content/en/email_templates/passwordless_email.json")),
+        {
+          body = file("${path.module}/content/en/email_templates/passwordless_email_body.html")
+        }
+      )
+    }
+  }
+}
+
 resource "auth0_connection" "passwordless_email" {
   strategy = "email"
   name     = "email" # name must be "email" to be visible in the Auth0 web console
 
   options {
     name = "email"
+
+    syntax   = "liquid"
+    from     = local.en.email_templates.passwordless_email.from
+    subject  = local.en.email_templates.passwordless_email.subject
+    template = local.en.email_templates.passwordless_email.body
 
     auth_params = {
       scope = "openid profile"
