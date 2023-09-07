d61983d3260d9f5962cbf3ef5e0df9521e02b41b
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2023-09-07 15:18:40 +0300

    Configure Auth0 branding to use GOV.UK crown logo

--- infra/modules/auth0/forms-admin.tf
+++ infra/modules/auth0/forms-admin.tf
@@ -1,6 +1,7 @@
 resource "auth0_client" "forms_admin" {
   name     = "forms-admin-${var.env_name}"
   app_type = "regular_web"
+  logo_uri = "${var.admin_base_url}${var.app_logo_path}"
 
   allowed_logout_urls = [
     "${var.admin_base_url}"
--- infra/modules/auth0/tenant.tf
+++ infra/modules/auth0/tenant.tf
@@ -1,3 +1,4 @@
 resource "auth0_tenant" "tenant" {
   friendly_name = "GOV.UK Forms (${var.env_name})"
+  picture_url   = "${var.admin_base_url}/${var.app_logo_path}"
 }
