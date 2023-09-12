6e0db9c764333c4a47229a93f696bfe286703866
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2023-09-12 11:17:30 +0300

    Configure Terraform to get Auth0 management secrets from variables
    Instead of reading the Auth0 secrets directly from the environment use
    Terraform variables.
    
    The values provided for the variables do not appear to end up in the
    Terraform state file (checked using `terraform state list`), so this
    isn't less secure.
    
    Note that we're hardcoding the domain rather than requiring it to be
    provided by the caller. The domain is public, and by constraining it it
    makes it harder to terraform the wrong environment. Thanks @danworth for
    the suggestion (see [comment in #294][1]).
    
    [1]: https://github.com/alphagov/forms-deploy/pull/294#discussion_r1324242641

--- infra/deployments/development/auth0/main.tf
+++ infra/deployments/development/auth0/main.tf
@@ -1,3 +1,15 @@
+variable "auth0_client_id" {
+  description = "The client ID for the Auth0 'Terraform access' app for this environment"
+  type        = string
+  sensitive   = true
+}
+
+variable "auth0_client_secret" {
+  description = "The client secret for the Auth0 'Terraform access' app for this environment"
+  type        = string
+  sensitive   = true
+}
+
 module "auth0" {
   source = "../../../modules/auth0"
 
