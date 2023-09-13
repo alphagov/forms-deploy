3ea354d0befa1e1640f166bc80163df82624e1a1
Radha Kotyankar <radha.kotyankar@digital.cabinet-office.gov.uk>
2023-09-13 14:19:54 +0100

    Increased product page to 3 tasks in production
    Updated product pages ECS service to run 3 tasks in production to maintain
    consistency with other apps.

--- infra/deployments/production/forms-product-page/main.tf
+++ infra/deployments/production/forms-product-page/main.tf
@@ -7,7 +7,7 @@ module "forms_product_page" {
   source             = "../../../modules/forms-product-page"
   env_name           = "production"
   image_tag          = var.image_tag
-  desired_task_count = 2
+  desired_task_count = 3
   cpu                = 256
   memory             = 512
 }
