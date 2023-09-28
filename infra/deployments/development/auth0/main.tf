2800aa72437e3b8a038e44fceba49e475ab85733
Catalina Garcia <catalina.garcia@digital.cabinet-office.gov.uk>
2023-09-28 11:25:58 +0100

    Add Cat

--- infra/deployments/development/auth0/main.tf
+++ infra/deployments/development/auth0/main.tf
@@ -16,6 +16,7 @@ module "ses" {
   verified_email_addresses = [
     "alice.carr@digital.cabinet-office.gov.uk",
     "laurence.debruxelles@digital.cabinet-office.gov.uk",
+    "catalina.garcia@digital.cabinet-office.gov.uk",
     "forms--test-automation-groupt@digital.cabinet-office.gov.uk", # smoke tests user
   ]
 }
