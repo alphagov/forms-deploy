7b78a9176ba95ea5e722280be3260fb3bd5420bf
Catalina Garcia <catalina.garcia@digital.cabinet-office.gov.uk>
2023-10-03 14:46:49 +0100

    Add missing IPs
    We missed a couple of IPs (or did they add some when we were not looking?)

--- infra/modules/common-values/locals.tf
+++ infra/modules/common-values/locals.tf
@@ -4,6 +4,8 @@ locals {
     "217.196.229.77/32", # GovWifi
     "217.196.229.79/32", # Brattain (Whitechapel Building Wifi)
     "217.196.229.80/32", # BYOD VPN
-    "217.196.229.81/32"  # Managed device VPN
+    "217.196.229.81/32", # Managed device VPN
+    "51.149.8.0/25",     # Managed device VPN
+    "51.149.8.128/29"    # BYOD VPN
   ]
 }
