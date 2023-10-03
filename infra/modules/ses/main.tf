438b8b146ddacae8f40d972314139c8f6d9e616f
Catalina Garcia <catalina.garcia@digital.cabinet-office.gov.uk>
2023-10-03 15:51:07 +0100

    Use team email address

--- infra/modules/ses/main.tf
+++ infra/modules/ses/main.tf
@@ -75,5 +75,5 @@ resource "aws_sns_topic" "ses_notifications" {
 resource "aws_sns_topic_subscription" "email" {
   topic_arn = aws_sns_topic.ses_notifications.arn
   protocol  = "email"
-  endpoint  = "catalina.garcia@digital.cabinet-office.gov.uk"
+  endpoint  = "govuk-forms-bounce-emails@digital.cabinet-office.gov.uk"
 }
