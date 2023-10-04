cc3a16fe143264457431c4acb636c12022138114
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2023-10-04 13:31:59 +0300

    Allow all users to sign in to dev using Auth0
    Make sure all users in users file with access to dev have their email
    addresses verified in AWS SES, so that they can get the passwordless
    sign in code emails from Auth0 and sign in to the admin app in the dev
    environment.

--- infra/deployments/development/ses/main.tf
+++ infra/deployments/development/ses/main.tf
@@ -1,12 +1,18 @@
+module "users" {
+  source = "../../../modules/users"
+}
+
 module "ses" {
   source = "../../../modules/ses"
 
-  verified_email_addresses = [
-    "alice.carr@digital.cabinet-office.gov.uk",
-    "laurence.debruxelles@digital.cabinet-office.gov.uk",
-    "catalina.garcia@digital.cabinet-office.gov.uk",
-    "forms--test-automation-groupt@digital.cabinet-office.gov.uk", # smoke tests user
-  ]
+  verified_email_addresses = concat(
+    [
+      for user in module.users.for_env["dev"] : "${user}@digital.cabinet-office.gov.uk"
+    ],
+    [
+      "forms--test-automation-groupt@digital.cabinet-office.gov.uk", # smoke tests user
+    ],
+  )
 
   smtp_user    = "auth0"
   from_address = "no-reply@dev.forms.service.gov.uk"
