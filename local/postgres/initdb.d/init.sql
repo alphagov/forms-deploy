68e776c77eddf2d43c8ac7760d9190d80a94b679
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2023-10-10 14:16:09 +0300

    Append environment name to database names
    This aligns with the defaults in the app `config/database.yml` files.

--- local/postgres/initdb.d/init.sql
+++ local/postgres/initdb.d/init.sql
@@ -1,10 +1,10 @@
 CREATE USER forms_admin WITH PASSWORD 'forms_admin' CREATEDB;
-CREATE DATABASE forms_admin;
-GRANT ALL PRIVILEGES ON DATABASE forms_admin TO forms_admin;
+CREATE DATABASE forms_admin_development;
+GRANT ALL PRIVILEGES ON DATABASE forms_admin_development TO forms_admin;
 
 CREATE USER forms_api WITH PASSWORD 'forms_api' CREATEDB;
-CREATE DATABASE forms_api;
-GRANT ALL PRIVILEGES ON DATABASE forms_api TO forms_api;
+CREATE DATABASE forms_api_development;
+GRANT ALL PRIVILEGES ON DATABASE forms_api_development TO forms_api;
 
-ALTER DATABASE forms_admin OWNER TO forms_admin;
-ALTER DATABASE forms_api OWNER TO forms_api;
+ALTER DATABASE forms_admin_development OWNER TO forms_admin;
+ALTER DATABASE forms_api_development OWNER TO forms_api;
