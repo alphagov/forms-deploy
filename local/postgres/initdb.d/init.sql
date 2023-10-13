02a2f900d9ce4ef762e7a4dee3a79f4b639c9232
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2023-10-13 16:08:51 +0300

    wip! Use app setup scripts in command
    This lets us simplify the default command for each container, and the
    init.sql script for setting up the PostgreSQL database, as for each app
    part of the `bin/setup` script is creating the required database if it
    doesn't already exist.
    
    Note that we remove from `init.sql` the lines about granting privileges
    to the database, but they are unneeded as the user we give the user we
    create for each app the ability to create databases, and when a user
    creates a database they will automatically be the owner and have all
    privileges already.

--- local/postgres/initdb.d/init.sql
+++ local/postgres/initdb.d/init.sql
@@ -1,10 +1,2 @@
 CREATE USER forms_admin WITH PASSWORD 'forms_admin' CREATEDB;
-CREATE DATABASE forms_admin_development;
-GRANT ALL PRIVILEGES ON DATABASE forms_admin_development TO forms_admin;
-
 CREATE USER forms_api WITH PASSWORD 'forms_api' CREATEDB;
-CREATE DATABASE forms_api_development;
-GRANT ALL PRIVILEGES ON DATABASE forms_api_development TO forms_api;
-
-ALTER DATABASE forms_admin_development OWNER TO forms_admin;
-ALTER DATABASE forms_api_development OWNER TO forms_api;
