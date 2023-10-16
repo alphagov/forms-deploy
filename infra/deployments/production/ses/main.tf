0cfc38dd552d9155fdee2aa8750dd3d084b9d7f3
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2023-10-16 14:55:27 +0300

    Add AWS SES configuration for production

---
+++ infra/deployments/production/ses/main.tf
@@ -0,0 +1,18 @@
+module "users" {
+  source = "../../../modules/users"
+}
+
+module "ses" {
+  source = "../../../modules/ses"
+
+  verified_email_addresses = concat(
+    [
+      for user in module.users.for_env["prod"] : "${user}@digital.cabinet-office.gov.uk"
+    ],
+    [
+      "forms--test-automation-groupt@digital.cabinet-office.gov.uk", # smoke tests user
+    ],
+  )
+
+  from_address = "no-reply@forms.service.gov.uk"
+}
