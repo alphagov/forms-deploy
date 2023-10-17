f2d02b82d41cf563a83c16c47aa2b8e2145dcfd3
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2023-10-17 10:16:20 +0300

    Add Auth0 tenant for production environment

---
+++ infra/deployments/production/auth0/main.tf
@@ -0,0 +1,7 @@
+module "auth0" {
+  source = "../../../modules/auth0"
+
+  admin_base_url    = "https://admin.forms.service.gov.uk"
+  env_name          = "production"
+  smtp_from_address = "no-reply@forms.service.gov.uk"
+}
