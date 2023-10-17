9891568ce2d60e5a5ff41cf6e1050c8ba402634e
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2023-10-17 14:38:42 +0100

    Add feature flag for email confirmations
    Adds a feature flag for the "Confirm that a form's been submitted"
    feature, and sets it to false in all environments.

--- infra/deployments/production/forms-runner/main.tf
+++ infra/deployments/production/forms-runner/main.tf
@@ -4,13 +4,14 @@ variable "image_tag" {
 }
 
 module "forms_runner" {
-  source                  = "../../../modules/forms-runner"
-  env_name                = "production"
-  image_tag               = var.image_tag
-  desired_task_count      = 3
-  cpu                     = 256
-  memory                  = 512
-  api_base_url            = "https://api.forms.service.gov.uk"
-  admin_base_url          = "https://admin.forms.service.gov.uk"
-  enable_maintenance_mode = false
+  source                      = "../../../modules/forms-runner"
+  env_name                    = "production"
+  image_tag                   = var.image_tag
+  desired_task_count          = 3
+  cpu                         = 256
+  memory                      = 512
+  api_base_url                = "https://api.forms.service.gov.uk"
+  admin_base_url              = "https://admin.forms.service.gov.uk"
+  enable_maintenance_mode     = false
+  email_confirmations_enabled = false
 }
