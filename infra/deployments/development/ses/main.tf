1679884fbb3fb9e77cdf14d901cdc09d5587697f
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2023-10-19 15:18:04 +0300

    Parametrise DNS configuration for SES module
    Move the DNS records needed for the SES module from the deployment
    `main.tf` file into the module itself, so that the module consumer
    doesn't need to think about them.
    
    No changes should need to be applied as a result of this change, as the
    state has been moved using the Terraform `state rm` and `import`
    commands.

--- infra/deployments/development/ses/main.tf
+++ infra/deployments/development/ses/main.tf
@@ -5,6 +5,10 @@ module "users" {
 module "ses" {
   source = "../../../modules/ses"
 
+  environment = "dev"
+
+  email_domain = "dev.forms.service.gov.uk"
+  from_address = "no-reply@dev.forms.service.gov.uk"
   verified_email_addresses = concat(
     [
       for user in module.users.for_env["dev"] : "${user}@digital.cabinet-office.gov.uk"
@@ -13,38 +17,4 @@ module "ses" {
       "forms--test-automation-groupt@digital.cabinet-office.gov.uk", # smoke tests user
     ],
   )
-
-  email_domain = "dev.forms.service.gov.uk"
-  from_address = "no-reply@dev.forms.service.gov.uk"
-  environment  = "dev"
-}
-
-# this zone should be managed in the dns deployment module for the environment
-data "aws_route53_zone" "public" {
-  name = "dev.forms.service.gov.uk."
-}
-
-resource "aws_ses_domain_identity" "ses" {
-  domain = "dev.forms.service.gov.uk"
-}
-
-resource "aws_ses_domain_dkim" "ses" {
-  domain = aws_ses_domain_identity.ses.domain
-}
-
-resource "aws_route53_record" "ses" {
-  count   = 3
-  zone_id = data.aws_route53_zone.public.id
-  name    = "${aws_ses_domain_dkim.ses.dkim_tokens[count.index]}._domainkey"
-  type    = "CNAME"
-  ttl     = 600
-  records = ["${aws_ses_domain_dkim.ses.dkim_tokens[count.index]}.dkim.amazonses.com"]
-}
-
-resource "aws_route53_record" "ses_email_receiving" {
-  zone_id = data.aws_route53_zone.public.id
-  name    = aws_ses_domain_identity.ses.domain
-  type    = "MX"
-  records = ["10 inbound-smtp.eu-west-2.amazonaws.com"]
-  ttl     = 3600
 }
