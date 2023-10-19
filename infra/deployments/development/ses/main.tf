87c6c2ab382c5fff44267f99e0e234b30c3ca6d2
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2023-10-19 12:50:05 +0300

    Move SES DNS configuration from DNS module to SES module
    Make the DNS records used for directing mail and DKIM the responsibility
    of the SES module.
    
    No changes should need to be applied as a result of this change, as the
    state has been moved using the Terrafrom `state rm` and `import`
    commands.

--- infra/deployments/development/ses/main.tf
+++ infra/deployments/development/ses/main.tf
@@ -18,3 +18,33 @@ module "ses" {
   from_address = "no-reply@dev.forms.service.gov.uk"
   environment  = "dev"
 }
+
+# this zone should be managed in the dns deployment module for the environment
+data "aws_route53_zone" "public" {
+  name = "dev.forms.service.gov.uk."
+}
+
+resource "aws_ses_domain_identity" "ses" {
+  domain = "dev.forms.service.gov.uk"
+}
+
+resource "aws_ses_domain_dkim" "ses" {
+  domain = aws_ses_domain_identity.ses.domain
+}
+
+resource "aws_route53_record" "ses" {
+  count   = 3
+  zone_id = data.aws_route53_zone.public.id
+  name    = "${aws_ses_domain_dkim.ses.dkim_tokens[count.index]}._domainkey"
+  type    = "CNAME"
+  ttl     = 600
+  records = ["${aws_ses_domain_dkim.ses.dkim_tokens[count.index]}.dkim.amazonses.com"]
+}
+
+resource "aws_route53_record" "ses_email_receiving" {
+  zone_id = data.aws_route53_zone.public.id
+  name    = aws_ses_domain_identity.ses.domain
+  type    = "MX"
+  records = ["10 inbound-smtp.eu-west-2.amazonaws.com"]
+  ttl     = 3600
+}
