385240880c3c6d80556dc932504f507d01a54867
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2023-10-23 11:55:11 +0300

    Update SES configuration for staging environment
    This commit updates the deployment module for AWS Simple Email Service
    in staging, to allow us to apply the required DNS configuration (from
    PR #352) and the SQS queues for complaints and bounces (from PR #339).

--- infra/deployments/staging/ses/main.tf
+++ infra/deployments/staging/ses/main.tf
@@ -5,6 +5,10 @@ module "users" {
 module "ses" {
   source = "../../../modules/ses"
 
+  environment = "staging"
+
+  email_domain = "staging.forms.service.gov.uk"
+  from_address = "no-reply@staging.forms.service.gov.uk"
   verified_email_addresses = concat(
     [
       for user in module.users.for_env["staging"] : "${user}@digital.cabinet-office.gov.uk"
@@ -13,6 +17,4 @@ module "ses" {
       "forms--test-automation-groupt@digital.cabinet-office.gov.uk", # smoke tests user
     ],
   )
-
-  from_address = "no-reply@staging.forms.service.gov.uk"
 }
