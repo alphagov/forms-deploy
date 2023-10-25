2acc5b928918ac4c16aa1ff4125044395bbe5ed3
Andy Hunt <andy.hunt@digital.cabinet-office.gov.uk>
2023-10-25 16:47:08 +0100

    Update AWS CodeBuild container versions

--- infra/modules/code-build-deploy-ecs/codebuild.tf
+++ infra/modules/code-build-deploy-ecs/codebuild.tf
@@ -25,7 +25,7 @@ resource "aws_codebuild_project" "terraform" {
 
   environment {
     compute_type = "BUILD_GENERAL1_SMALL"
-    image        = "aws/codebuild/amazonlinux2-x86_64-standard:4.0"
+    image        = "aws/codebuild/amazonlinux2-x86_64-standard:5.0"
     type         = "LINUX_CONTAINER"
 
     environment_variable {
--- infra/modules/utility-images-pipeline/codepipeline.tf
+++ infra/modules/utility-images-pipeline/codepipeline.tf
@@ -62,7 +62,7 @@ module "docker_build_end_to_end_tests" {
   build_directory                = "infra/modules/code-build-run-smoke-tests/dockerfile"
 
   # The end-to-end image installs Chrome for x86 so use the following when building it.
-  code_build_project_image        = "aws/codebuild/amazonlinux2-x86_64-standard:4.0"
+  code_build_project_image        = "aws/codebuild/amazonlinux2-x86_64-standard:5.0"
   code_build_project_compute_size = "BUILD_GENERAL1_SMALL"
   code_build_project_compute_arch = "LINUX_CONTAINER"
 }
