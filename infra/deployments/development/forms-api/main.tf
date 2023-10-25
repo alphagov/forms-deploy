86d370b72aa86da3da0e97496e8816ef465649c1
Dan Worth <dan.worth@digital.cabinet-office.gov.uk>
2023-10-25 14:42:01 +0100

    Increase Forms-Api to 6

--- infra/deployments/development/forms-api/main.tf
+++ infra/deployments/development/forms-api/main.tf
@@ -7,7 +7,7 @@ module "forms_api" {
   source             = "../../../modules/forms-api"
   env_name           = "dev"
   image_tag          = var.image_tag
-  desired_task_count = 3
+  desired_task_count = 6
   cpu                = 512
   memory             = 1024
 }
