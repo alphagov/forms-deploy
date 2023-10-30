bebf7403595a845a23fc0a349e3c63574e6eb165
Catalina Garcia <catalina.garcia@digital.cabinet-office.gov.uk>
2023-10-30 13:58:20 +0000

    Increase production instance size and thread count
    After load testing, we believe this increase will give us a better chance of
    success if we need to scale horizontally.
    
    This increases the CPU and memory of forms-runner and forms-api, as well as the
    thread count for rails in forms-runner

--- infra/deployments/production/forms-runner/main.tf
+++ infra/deployments/production/forms-runner/main.tf
@@ -8,10 +8,11 @@ module "forms_runner" {
   env_name                    = "production"
   image_tag                   = var.image_tag
   desired_task_count          = 3
-  cpu                         = 256
-  memory                      = 512
+  cpu                         = 1024
+  memory                      = 2048
   api_base_url                = "https://api.forms.service.gov.uk"
   admin_base_url              = "https://admin.forms.service.gov.uk"
   enable_maintenance_mode     = false
   email_confirmations_enabled = false
+  rails_max_threads           = 25
 }
