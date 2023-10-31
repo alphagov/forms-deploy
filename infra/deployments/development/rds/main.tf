1123c5677acadc5df7387971ed996e443a7c67a6
Catalina Garcia <catalina.garcia@digital.cabinet-office.gov.uk>
2023-10-31 14:30:12 +0000

    Apply RDS changes during the maintenance window
    We want changes to be applied by default during the maintenance window. If and
    when we need to apply something immediately we can overwrite it

--- infra/deployments/development/rds/main.tf
+++ infra/deployments/development/rds/main.tf
@@ -3,5 +3,4 @@ module "rds" {
   env_name                 = "dev"
   auto_pause               = false
   seconds_until_auto_pause = 300
-  apply_immediately        = true
 }
--- infra/deployments/production/rds/main.tf
+++ infra/deployments/production/rds/main.tf
@@ -1,5 +1,4 @@
 module "rds" {
   source            = "../../../modules/rds"
   env_name          = "production"
-  apply_immediately = true #TODO: consider whether applying out of hours in maintenance window might be better.
 }
