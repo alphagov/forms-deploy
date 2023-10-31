c00d654e12d2b230a94d6491acee49ffc24ab253
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2023-10-31 08:59:42 +0200

    Fix typo
    Remove unneeded slash. `var.app_logo_path` should be an absolute path,
    so will already have a slash.

--- infra/modules/auth0/tenant.tf
+++ infra/modules/auth0/tenant.tf
@@ -2,7 +2,7 @@ data "auth0_tenant" "tenant" {} # only ever allowed to access one tenant, based 
 
 resource "auth0_tenant" "tenant" {
   friendly_name         = "GOV.UK Forms (${var.env_name})"
-  picture_url           = "${var.admin_base_url}/${var.app_logo_path}"
+  picture_url           = "${var.admin_base_url}${var.app_logo_path}"
   idle_session_lifetime = var.idle_session_lifetime
   session_lifetime      = var.session_lifetime
 }
