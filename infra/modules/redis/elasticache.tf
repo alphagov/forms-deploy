018c5dcdffc4a68d849a5788836098cc52dbf127
Catalina Garcia <catalina.garcia@digital.cabinet-office.gov.uk>
2023-11-06 13:39:53 +0000

    Change to a parameter group that is compatible

--- infra/modules/redis/elasticache.tf
+++ infra/modules/redis/elasticache.tf
@@ -17,7 +17,7 @@ resource "aws_elasticache_replication_group" "forms-runner" {
   transit_encryption_enabled = true
   engine_version             = var.engine_version
   port                       = local.redis_port
-  parameter_group_name       = aws_elasticache_parameter_group.redis_parameter_group["redis6"].id
+  parameter_group_name       = aws_elasticache_parameter_group.redis_parameter_group["redis7"].id
   subnet_group_name          = aws_elasticache_subnet_group.redis.id
   security_group_ids         = [aws_security_group.forms_runner_redis.id]
   apply_immediately          = var.apply_immediately
