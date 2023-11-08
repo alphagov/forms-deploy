0a12375eac6921dbb3ae29e9cca29d9ac77f123c
Catalina Garcia <catalina.garcia@digital.cabinet-office.gov.uk>
2023-11-08 10:13:27 +0000

    Increase capacity to match higher demand
    We expect the demand to runner/api could increase on the week commencing
    13/11/2023. After load testing we think this increase will cover us

--- infra/deployments/production/forms-runner/main.tf
+++ infra/deployments/production/forms-runner/main.tf
@@ -7,7 +7,7 @@ module "forms_runner" {
   source                      = "../../../modules/forms-runner"
   env_name                    = "production"
   image_tag                   = var.image_tag
-  desired_task_count          = 3
+  desired_task_count          = 9
   cpu                         = 1024
   memory                      = 2048
   api_base_url                = "https://api.forms.service.gov.uk"
