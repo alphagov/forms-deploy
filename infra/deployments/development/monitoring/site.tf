85077c4a78f755da4824fa36ab8142de52ebbfb2
Radha Kotyankar <radha.kotyankar@digital.cabinet-office.gov.uk>
2023-11-09 14:11:44 +0000

    added dasboards

---
+++ infra/deployments/development/monitoring/site.tf
@@ -0,0 +1,19 @@
+terraform {
+  backend "s3" {
+    bucket = "gds-forms-development-tfstate"
+    key    = "monitoring.tfstate"
+    region = "eu-west-2"
+  }
+}
+
+provider "aws" {
+  allowed_account_ids = ["498160065950"]
+
+  default_tags {
+    tags = {
+      Environment = "development"
+      Deployment  = "development/monitoring"
+    }
+  }
+}
+
