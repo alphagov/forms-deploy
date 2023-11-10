d832e9a37d031ddcc70a21463414543545f3c9cb
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2023-11-10 09:10:41 +0200

    Fix forms-api domains
    These domains have changed since we completed migration to AWS.

--- support/forms-cli/lib/commands/forms_api_tokens.rb
+++ support/forms-cli/lib/commands/forms_api_tokens.rb
@@ -11,8 +11,8 @@ require "uri"
 class FormsApiTokens
   FORMS_API_DOMAINS = {
     "dev" => "api.dev.forms.service.gov.uk",
-    "staging" => "api.stage.forms.service.gov.uk",
-    "production" => "api.prod-temp.forms.service.gov.uk",
+    "staging" => "api.staging.forms.service.gov.uk",
+    "production" => "api.forms.service.gov.uk",
     "user-research" => "api.research.forms.service.gov.uk",
   }.freeze
 
