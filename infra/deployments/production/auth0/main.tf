b63e6b454fe9af001e514aca2b6df65eee24aad5
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2023-11-23 08:28:23 +0200

    Allow email domains for potential partners access to GOV.UK Forms
    We have some partners we're working with, but who don't have access to
    the platform because their organisation doesn't have an email domain
    that ends with `.gov.uk`. We want to allow these people in, as they'll
    be needing to use the platform soon.
    
    This commit adds the relevant email domains to the list of domains
    allowed by our Auth0 pre-registration action.
    
    This commit only allows access to production system; we prefer not to
    give users outside of the GOV.UK Forms team access to the staging and
    development systems [[1]].
    
    [1]: https://github.com/alphagov/forms-team/wiki/How-to-access-GOV.UK-Forms#our-environments

--- infra/deployments/production/auth0/main.tf
+++ infra/deployments/production/auth0/main.tf
@@ -1,8 +1,22 @@
 module "auth0" {
   source = "../../../modules/auth0"
 
-  admin_base_url        = "https://admin.forms.service.gov.uk"
-  env_name              = "production"
-  smtp_from_address     = "no-reply@forms.service.gov.uk"
-  allowed_email_domains = [".gov.uk"]
+  admin_base_url    = "https://admin.forms.service.gov.uk"
+  env_name          = "production"
+  smtp_from_address = "no-reply@forms.service.gov.uk"
+
+  allowed_email_domains = [
+    ".gov.uk",
+    "cefas.co.uk",
+    "certoffice.org",
+    "ddc-mod.uk",
+    "hs2.org.uk",
+    "innovateuk.ukri.org",
+    "mod.uk",
+    "nationalhighways.co.uk",
+    "naturalengland.co.uk",
+    "slc.co.uk",
+    "ukces.org.uk",
+    "ukri.org",
+  ]
 }
