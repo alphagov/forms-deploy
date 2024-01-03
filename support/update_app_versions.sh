92790e9dfa4ffe8332c85b10b867ceb98b5de92c
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2024-01-03 16:11:42 +0200

    Refactor update_app_versions

--- support/update_app_versions.sh
+++ support/update_app_versions.sh
@@ -20,11 +20,14 @@ fi
 
 # Update these values as necessary and then run the script
 NEW_RUBY_VERSION="3.2.2"
-DOCKER_ALPINE_VERSION="3.18"
+NEW_ALPINE_VERSION="3.18"
 
 # Constants that should not need to be updated
-GIT_BRANCH_NAME="bump_ruby_to_${NEW_RUBY_VERSION}"
 APPS=(forms-api forms-admin forms-runner forms-product-page forms-e2e-tests)
+DOCKER_IMAGE_NAME=ruby
+DOCKER_IMAGE_TAG="${NEW_RUBY_VERSION}-alpine${NEW_ALPINE_VERSION}"
+GIT_BRANCH_NAME="bump_base_image_to_${DOCKER_IMAGE_TAG}"
+GIT_COMMIT_MSG="Bump base image"
 
 # Checks out main branch and pulls latests. Then creates a new
 # branch for the updates. If the branch already exists it continues
@@ -37,22 +40,14 @@ setup_git_branch () {
 }
 
 # Find the manifest list sha for the version of ruby and alpine
-get_new_docker_base_image_sha () {
-  docker buildx imagetools inspect ruby:${NEW_RUBY_VERSION}-alpine${DOCKER_ALPINE_VERSION} \
+get_new_docker_image_digest () {
+  docker buildx imagetools inspect "${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}" \
     | grep Digest \
     | cut -d : -f 2-3 \
     | tr -d ' '
 }
 
-# Main script begins here
-echo "Updating to ${NEW_RUBY_VERSION}"
-MANIFEST_LIST_SHA="$(get_new_docker_base_image_sha)"
-for app in "${APPS[@]}"; do
-  echo -e "\n\n****** BEGINNING UPDATE OF ${app} ********"
-  cd "../../${app}"
-
-  setup_git_branch
-
+update_ruby_version () {
   echo "Updating .ruby-version file"
   echo "$NEW_RUBY_VERSION" > .ruby-version
 
@@ -63,16 +58,32 @@ for app in "${APPS[@]}"; do
   bundle install > /dev/null
 
   echo "Updating version in Dockerfile"
-  sed -i '' 's/^FROM ruby:.* AS/FROM ruby:'"${NEW_RUBY_VERSION}"'-alpine'"${DOCKER_ALPINE_VERSION}"'@'"${MANIFEST_LIST_SHA}"' AS/' Dockerfile
+  sed -i '' 's/^FROM '"${DOCKER_IMAGE_NAME}"':.* AS/FROM '"${DOCKER_BASE_IMAGE}"' AS/' Dockerfile
 
   echo "Committing changes"
   git add .ruby-version
   git add Dockerfile
   git add Gemfile
   git add Gemfile.lock
-  git commit -S -m "Bump Ruby to ${NEW_RUBY_VERSION}" \
+  git commit -S -m "${GIT_COMMIT_MSG}" \
     && git push -u origin "${GIT_BRANCH_NAME}" \
     || echo "There are no commits, perhaps you've already run this script?"
+}
+
+# Main script begins here
+NEW_DOCKER_IMAGE_DIGEST="$(get_new_docker_image_digest)"
+DOCKER_BASE_IMAGE="${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}@${NEW_DOCKER_IMAGE_DIGEST}"
+
+echo "Updating to ${DOCKER_BASE_IMAGE}"
+
+for app in "${APPS[@]}"; do
+  echo -e "\n\n****** BEGINNING UPDATE OF ${app} ********"
+  cd "../../${app}"
+
+  setup_git_branch
+
+  update_ruby_version
+
   echo -e "****** FINISHED UPDATING ${app} ********\n\n"
   cd - > /dev/null
 done
