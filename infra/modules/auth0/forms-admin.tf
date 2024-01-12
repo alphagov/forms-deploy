840da3ad53cc1acd6b748a73eaa463e8e235a8fb
Jamie Wilkinson <jamie.wilkinson@digital.cabinet-office.gov.uk>
2024-01-12 15:44:45 +0000

    Add default Auth0 tenant login and logout URIs

--- infra/modules/auth0/forms-admin.tf
+++ infra/modules/auth0/forms-admin.tf
@@ -3,10 +3,8 @@ resource "auth0_client" "forms_admin" {
   app_type = "regular_web"
   logo_uri = "${var.admin_base_url}${var.app_logo_path}"
 
-  initiate_login_uri = "${var.admin_base_url}${var.app_auth_path}"
-  allowed_logout_urls = [
-    "${var.admin_base_url}"
-  ]
+  initiate_login_uri  = "${var.admin_base_url}${var.app_auth_path}"
+  allowed_logout_urls = [var.admin_base_url]
   callbacks = [
     "${var.admin_base_url}${var.app_auth_callback_path}"
   ]
