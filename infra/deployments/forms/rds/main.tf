84de8625e9812b686ea71933cc3bb619017737ff
Andy Hunt <andy.hunt@digital.cabinet-office.gov.uk>
2024-01-24 13:16:24 +0000

    Make it possible to apply RDS changes immediately
    By default we want all changes to RDS to be applied in the next maintenance
    window, but in some cases we'd like to be able to override that behaviour and
    have them take effect immediately (e.g. while we test upgrade dev).
    
    This can now be done by setting the 'TF_VAR_apply_immediately' environment
    variable to 'true' (default is false), and there is a message reminding operators of this when
    they apply/plan the 'forms/rds' root.

--- infra/deployments/forms/rds/main.tf
+++ infra/deployments/forms/rds/main.tf
@@ -1,7 +1,14 @@
+variable "apply_immediately" {
+  type        = bool
+  description = "Whether to apply change to the database immediately, or in the next maintenance window."
+  default     = false
+}
+
 module "rds" {
   source   = "../../../modules/rds"
   env_name = var.environment_name
 
+  apply_immediately        = var.apply_immediately
   auto_pause               = var.environmental_settings.pause_databases_on_inactivity
   seconds_until_auto_pause = var.environmental_settings.pause_databases_after_inactivity_seconds
   backup_retention_period  = var.environmental_settings.database_backup_retention_period_days
