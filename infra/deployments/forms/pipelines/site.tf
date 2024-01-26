83b8d8d9e2f7bc68274986c1e1ae8e35e7b8f36a
Andy Hunt <andy.hunt@digital.cabinet-office.gov.uk>
2024-01-26 14:07:18 +0000

    Introduce 'pipelines' root in 'forms' deployment
    The 'pipelines' root will contain all of the pipeline definitions used in the
    environment.

---
+++ infra/deployments/forms/pipelines/site.tf
@@ -0,0 +1,20 @@
+terraform {
+    backend "s3" {
+        # bucket set in backend config file
+        key    = "pipelines.tfstate"
+        region = "eu-west-2"
+    }
+}
+
+provider "aws" {
+    allowed_account_ids = var.allowed_account_ids
+
+    default_tags {
+        tags = merge(var.default_tags,
+            {
+                Deployment = "${var.environment_name}/pipelines"
+            }
+        )
+    }
+}
+
