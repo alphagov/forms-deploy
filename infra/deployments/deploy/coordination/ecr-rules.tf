cef4d03d70576989798f115f26d0e5eaefc192b0
Andy Hunt <andy.hunt@digital.cabinet-office.gov.uk>
2024-02-07 14:01:49 +0000

    Log all ECR push events to CloudWatch
    AWS EventBridge has no event logging functionality out of the box. The best way
    to log events for the purposes of debugging and investigation, is to write them
    to a CloudWatch log group.

--- infra/deployments/deploy/coordination/ecr-rules.tf
+++ infra/deployments/deploy/coordination/ecr-rules.tf
@@ -1,13 +1,22 @@
-resource "aws_cloudwatch_event_rule" "distribute_ecr_events" {
-  name        = "send-ecr-events-to-other-acounts"
-  description = "Send ECR events to the event buses of the other accounts"
-  role_arn    = aws_iam_role.eventbridge_actor.arn
-  event_pattern = jsonencode({
+locals {
+  ecr_push_events_pattern = jsonencode({
     source = ["aws.ecr", "uk.gov.service.forms"]
     detail = {
       action-type = ["PUSH"]
     }
   })
+}
+resource "aws_cloudwatch_event_rule" "distribute_ecr_events" {
+  name          = "send-ecr-events-to-other-acounts"
+  description   = "Send ECR events to the event buses of the other accounts"
+  role_arn      = aws_iam_role.eventbridge_actor.arn
+  event_pattern = local.ecr_push_events_pattern
+}
+
+resource "aws_cloudwatch_event_rule" "log_ecr_events" {
+  name        = "log-ecr-events-in-cloudwatch"
+  description = "Send ECR events to CloudWatch"
+  event_pattern = local.ecr_push_events_pattern
 }
 
 resource "aws_cloudwatch_event_target" "other_account_event_bus" {
@@ -15,4 +24,42 @@ resource "aws_cloudwatch_event_target" "other_account_event_bus" {
   rule     = aws_cloudwatch_event_rule.distribute_ecr_events.name
   role_arn = aws_iam_role.eventbridge_actor.arn
   arn      = "arn:aws:events:eu-west-2:${each.value}:event-bus/default"
+}
+
+resource "aws_cloudwatch_log_group" "ecr_push_events" {
+  name              = "/aws/events/ecr-events"
+  retention_in_days = 14
+}
+
+resource "aws_cloudwatch_log_resource_policy" "allow_delivery_from_eventbridge" {
+  policy_document = data.aws_iam_policy_document.log_group_policy.json
+  policy_name     = "eventbridge-publishing-policy"
+}
+
+resource "aws_cloudwatch_event_target" "log_events_to_cloudwatch" {
+  target_id = "log-to-cloudwatch"
+  rule      = aws_cloudwatch_event_rule.log_ecr_events.name
+  arn       = aws_cloudwatch_log_group.ecr_push_events.arn
+}
+
+data "aws_iam_policy_document" "log_group_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+
+    resources = [
+      "${aws_cloudwatch_log_group.ecr_push_events.arn}:*"
+    ]
+
+    principals {
+      type = "Service"
+      identifiers = [
+        "events.amazonaws.com",
+        "delivery.logs.amazonaws.com"
+      ]
+    }
+  }
 }
--- infra/deployments/forms/pipelines/eventbridge.tf
+++ infra/deployments/forms/pipelines/eventbridge.tf
@@ -22,3 +22,52 @@ resource "aws_cloudwatch_event_bus_policy" "default_bus_policy" {
   policy         = data.aws_iam_policy_document.allow_receiving_from_deploy_account.json
   event_bus_name = "default"
 }
+
+resource "aws_cloudwatch_log_group" "ecr_push_events" {
+  name              = "/aws/events/${var.environment_name}/ecr-events"
+  retention_in_days = 14
+}
+
+resource "aws_cloudwatch_log_resource_policy" "allow_delivery_from_eventbridge" {
+  policy_document = data.aws_iam_policy_document.log_group_policy.json
+  policy_name     = "eventbridge-publishing-policy"
+}
+
+resource "aws_cloudwatch_event_rule" "ecr_push_events" {
+  name        = "all-ecr-push-events-${var.environment_name}"
+  description = "Matches all ECR push events"
+  event_pattern = jsonencode({
+    source = ["aws.ecr", "uk.gov.service.forms"]
+    detail = {
+      action-type = ["PUSH"]
+    }
+  })
+}
+
+resource "aws_cloudwatch_event_target" "log_events_to_cloudwatch" {
+  target_id = "${var.environment_name}-log-to-cloudwatch"
+  rule      = aws_cloudwatch_event_rule.ecr_push_events.name
+  arn       = aws_cloudwatch_log_group.ecr_push_events.arn
+}
+
+data "aws_iam_policy_document" "log_group_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+
+    resources = [
+      "${aws_cloudwatch_log_group.ecr_push_events.arn}:*"
+    ]
+
+    principals {
+      type = "Service"
+      identifiers = [
+        "events.amazonaws.com",
+        "delivery.logs.amazonaws.com"
+      ]
+    }
+  }
+}
