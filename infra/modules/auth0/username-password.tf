f4bc9b7db0eab8c78748fe080d2c5cee9e8603b9
Andy Hunt <andy.hunt@digital.cabinet-office.gov.uk>
2024-02-08 11:45:25 +0000

    Ensure the externally-managed Terraform Auth0 connection is maintained
    We've gone from a model of appending to a list to one of providing the list
    idempotently. This means we need to always know all of the values that the list
    needs to contain, which in this case means knowing a value that is managed
    outside of Terraform.
    
    We've added a new input to the Auth0 module to accept the extra values.

--- infra/modules/auth0/username-password.tf
+++ infra/modules/auth0/username-password.tf
@@ -10,8 +10,8 @@ resource "auth0_connection" "username_password_connection" {
 
 resource "auth0_connection_clients" "username_password_connection_clients" {
   connection_id = auth0_connection.username_password_connection.id
-  enabled_clients = [
+  enabled_clients = concat(var.additional_username_and_password_client_ids, [
     auth0_client.forms_admin.id,
     auth0_client.forms_admin_e2e.id
-  ]
+  ])
 }
