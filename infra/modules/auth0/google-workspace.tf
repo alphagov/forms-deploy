d02bdad4d8f2a1dd3b19a48e1e84495c3481e76c
Jamie Wilkinson <jamie.wilkinson@digital.cabinet-office.gov.uk>
2024-02-13 16:22:17 +0000

    Add google workspace Auth0 connection and connection-client associations

--- infra/modules/auth0/google-workspace.tf
+++ infra/modules/auth0/google-workspace.tf
@@ -1,3 +1,25 @@
+resource "auth0_connection" "google_workspace" {
+  name           = "Google-Workspace"
+  strategy       = "google-apps"
+  show_as_button = false
+  options {
+    api_enable_users         = true
+    client_id                = data.aws_ssm_parameter.google_oauth_client_id.value
+    client_secret            = data.aws_ssm_parameter.google_oauth_client_secret.value
+    domain                   = "digital.cabinet-office.gov.uk"
+    domain_aliases           = ["digital.cabinet-office.gov.uk"]
+    set_user_root_attributes = "on_each_login"
+    tenant_domain            = "digital.cabinet-office.gov.uk"
+  }
+}
+
+resource "auth0_connection_clients" "google_workspace_connection_clients" {
+  connection_id = auth0_connection.google_workspace.id
+  enabled_clients = [
+    auth0_client.forms_admin.id
+  ]
+}
+
 resource "aws_ssm_parameter" "google_oauth_client_id" {
   #checkov:skip=CKV_AWS_337:The parameter is already using the default key
   # Value is set externally.
