2499e853a8031026f9b18b33c5a8fe80ff3309af
Andy Hunt <andy.hunt@digital.cabinet-office.gov.uk>
2024-02-16 15:58:26 +0000

    Send notifications to Slack when pipeline statuses change
    We have notifications via CodeStar Connection Notifications for pipelines in
    the deploy account (which will eventually be retired), but we also need
    notifcations for pipelines in other accounts.
    
    We use AWS ChatBot for sending messages to Slack, but that doesn't have an API
    and we've had to manually configure it in the AWS Console. For that reason, we
    only want to have one configuration of it globally, so we need to send all of
    our notifications through it.
    
    AWS CodeStar Connection Notifications sends messages to Slack by sending a
    message to an AWS SNS topic with a known payload format. We can do the same
    thing to achieve the same result using AWS EventBridge.

--- infra/deployments/deploy/coordination/codepipeline.tf
+++ infra/deployments/deploy/coordination/codepipeline.tf
@@ -1,10 +1,10 @@
 module "log_codepipeline_events" {
-    source = "../../../modules/eventbridge-log-to-cloudwatch"
+  source = "../../../modules/eventbridge-log-to-cloudwatch"
 
-    environment_name  = "deploy"
-    log_group_subject = "codepipeline"
+  environment_name  = "deploy"
+  log_group_subject = "codepipeline"
 
-    event_pattern = jsonencode({
-        source = ["aws.codepipeline"]
-    })
+  event_pattern = jsonencode({
+    source = ["aws.codepipeline"]
+  })
 }
---
+++ infra/deployments/deploy/coordination/notifications.tf
@@ -0,0 +1,84 @@
+locals {
+  # We have configured AWS ChatBot for sending messages to Slack.
+  # AWS ChatBot does not have an API we can use in Terraform, so we
+  # configured it by hand in the one place and hardcoded the SNS topic here.
+  chatbot_sns_topic = "arn:aws:sns:eu-west-2:711966560482:CodeStarNotifications-govuk-forms-deployments-c383f287ab987f0b12d32e4533a145b1c918167d"
+
+  chatbot_message_input_paths = {
+    pipeline = "$.detail.pipeline"
+    account  = "$.account"
+    time     = "$.time"
+  }
+}
+
+resource "aws_cloudwatch_event_rule" "pipeline_completion" {
+  name        = "pipeline-success-events"
+  description = "Send pipeline success messages to Slack"
+  event_pattern = jsonencode({
+    source      = ["aws.codepipeline", "uk.gov.service.forms"],
+    detail-type = ["CodePipeline Pipeline Execution State Change"]
+    detail = {
+      state = ["SUCCEEDED"]
+    }
+  })
+}
+
+resource "aws_cloudwatch_event_target" "send_pipeline_completion_to_slack" {
+  target_id = "send-to-slack"
+  rule      = aws_cloudwatch_event_rule.pipeline_completion.name
+  arn       = local.chatbot_sns_topic
+
+  input_transformer {
+    input_paths    = local.chatbot_message_input_paths
+    input_template = <<EOF
+{
+    "version": "1.0",
+    "source": "custom",
+    "content": {
+        "textType": "client-markdown",
+        "title": ":tada: SUCCESS: <pipeline>",
+        "description": "Pipeline <pipeline> completed at <time>",
+        "nextSteps": [
+            "https://eu-west-2.console.aws.amazon.com/codesuite/codepipeline/pipelines/<pipeline>/view?region=eu-west-2"
+        ]
+    }
+}
+EOF
+  }
+}
+
+resource "aws_cloudwatch_event_rule" "pipeline_failure" {
+  name        = "pipeline-failure-events"
+  description = "Send pipeline failure messages to Slack"
+  event_pattern = jsonencode({
+    source      = ["aws.codepipeline", "uk.gov.service.forms"],
+    detail-type = ["CodePipeline Pipeline Execution State Change"]
+    detail = {
+      state = ["FAILED"]
+    }
+  })
+}
+
+resource "aws_cloudwatch_event_target" "send_pipeline_failure_to_slack" {
+  target_id = "send-to-slack"
+  rule      = aws_cloudwatch_event_rule.pipeline_failure.name
+  arn       = local.chatbot_sns_topic
+
+  input_transformer {
+    input_paths    = local.chatbot_message_input_paths
+    input_template = <<EOF
+        {
+            "version": "1.0",
+            "source": "custom",
+            "content": {
+                "textType": "client-markdown",
+                "title": ":fail: FAILURE: <pipeline>",
+                "description": "Pipeline <pipeline> failed at <time>",
+                "nextSteps": [
+                    "https://eu-west-2.console.aws.amazon.com/codesuite/codepipeline/pipelines/<pipeline>/view?region=eu-west-2"
+                ]
+            }
+        }
+        EOF
+  }
+}
