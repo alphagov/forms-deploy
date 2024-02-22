8f4e62a8705fd8dd41f3290748200aa240532e09
Andy Hunt <andy.hunt@digital.cabinet-office.gov.uk>
2024-02-22 15:14:57 +0000

    Add a CloudWatch alarm to alert us when an Event Bridge target fails
    We would like to know when this happens because Event Bridge coordinates our
    deployment pipelines, so if it's broken then so is our ability to react to
    incidents.

---
+++ infra/deployments/deploy/coordination/eventbridge-monitoring.tf
@@ -0,0 +1,32 @@
+resource "aws_cloudwatch_metric_alarm" "event_bridge_dlq_delivery_alarm" {
+  alarm_name        = "event-bridge-delivered-dead-letters"
+  alarm_description = <<EOF
+    When EventBridge fails to invoke a target, it will log information about
+    why it failed to invoke it and what happened in a message delivered to a
+    dead letter queue.
+
+    This alarm will enter the alarm state when there is a new message on the
+    queue.
+
+    NEXT STEPS:
+    1. Go look at the message in SQS console by visiting the URL below and
+    presing "Poll for messages"
+
+    https://eu-west-2.console.aws.amazon.com/sqs/v3/home?region=eu-west-2#/queues/${urlencode("https://sqs.eu-west-2.amazonaws.com/711966560482/${aws_sqs_queue.event_bridge_dlq.name}")}/send-receive
+    
+    2. When you've resolved the problem, delete the message from the queue
+EOF
+
+  namespace           = "AWS/Events"
+  metric_name         = "InvocationsSentToDlq"
+  comparison_operator = "GreaterThanThreshold"
+  threshold           = 0
+  statistic           = "Sum"
+  period              = 60 * 60 # 1 hour buckets
+  evaluation_periods  = 24      # Across 24 hours
+  datapoints_to_alarm = 1
+
+  treat_missing_data = "notBreaching"
+
+  alarm_actions = [local.chatbot_alerts_channel_sns_topic]
+}
