d1488bf9e18f93594296aa7a522235f417e29479
Andy Hunt <andy.hunt@digital.cabinet-office.gov.uk>
2024-03-04 14:26:32 +0000

    Add a CodePipeline readonly role to every environment

---
+++ infra/deployments/account/roles.tf
@@ -0,0 +1,20 @@
+resource "aws_iam_role" "codepipeline-readonly" {
+  name = "codepipeline-readonly"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::711966560482:role/deploy-pipeline-visualiser-ecs-task"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "codepipeline-readonly-policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodePipeline_ReadOnlyAccess"
+  role       = aws_iam_role.codepipeline-readonly.name
+}
