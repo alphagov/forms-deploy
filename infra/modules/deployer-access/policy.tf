7f7beae72b8c11bd9391c4aaeda3616435582ccb
Catalina Garcia <catalina.garcia@digital.cabinet-office.gov.uk>
2024-03-05 14:46:13 +0000

    Allow codebuild to pass the deployer role

--- infra/modules/deployer-access/policy.tf
+++ infra/modules/deployer-access/policy.tf
@@ -377,7 +377,7 @@ data "aws_iam_policy_document" "code-build-modules" {
       "arn:aws:iam::${lookup(local.account_ids, var.env_name)}:role/codebuild-*",
       "arn:aws:iam::${lookup(local.account_ids, var.env_name)}:role/${var.env_name}-event-bridge-*",
       "arn:aws:iam::${lookup(local.account_ids, var.env_name)}:role/event-bridge-actor",
-
+      "arn:aws:iam::${lookup(local.account_ids, var.env_name)}:role/deployer-${var.env_name}"
     ]
   }
   statement {
