90884f52bf248364ad50217f80914f7b013c91f7
Catalina Garcia <catalina.garcia@digital.cabinet-office.gov.uk>
2024-03-21 10:31:24 +0000

    Create a pipeline to build the forms-api container
    This pipeline builds a new container and tags it to beging the process of
    deploying, testing and promoting an image when a new commit is added to the
    'main' branch of the repo

--- infra/deployments/deploy/image-builders/main.tf
+++ infra/deployments/deploy/image-builders/main.tf
@@ -11,3 +11,10 @@ module "build-forms-runner-container" {
   container_repository = "forms-runner-deploy"
   source_repository    = "alphagov/forms-runner"
 }
+
+module "build-forms-api-container" {
+  source               = "../../../modules/image-builder-pipeline"
+  application_name     = "forms-api"
+  container_repository = "forms-api-deploy"
+  source_repository    = "alphagov/forms-api"
+}
