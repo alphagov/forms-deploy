01a207e3e72cfa86a2c1cbd59ec716825088f664
Andy Hunt <andy.hunt@digital.cabinet-office.gov.uk>
2024-03-25 13:21:23 +0000

    Comply with new checks from new Checkov version

--- infra/deployments/deploy/account/dns.tf
+++ infra/deployments/deploy/account/dns.tf
@@ -1,4 +1,6 @@
 resource "aws_route53_zone" "tools_zone" {
+  #checkov:skip=CKV2_AWS_39:No desire for DNS query logging
+  #checkov:skip=CKV2_AWS_38:No need for DNSSEC
   name = "tools.forms.service.gov.uk."
   lifecycle {
     prevent_destroy = true
