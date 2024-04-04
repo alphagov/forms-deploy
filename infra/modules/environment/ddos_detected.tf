764e7d08ba2891b15bf3b79c90d294571792bc31
Sarah Young <sarah.young1@digital.cabinet-office.gov.uk>
2024-04-04 09:57:09 +0100

    Move ddos_detected file to /environments
    * This allows us to set the provider on the alarm to us-east-1 since this is required for the alarm creation

--- infra/modules/environment/ddos_detected.tf
+++ infra/modules/environment/ddos_detected.tf
@@ -1,12 +1,13 @@
 resource "aws_cloudwatch_metric_alarm" "ddos_detected" {
-  alarm_name          = "ddos_detected_in_${var.environment}"
+  provider            = aws.us-east-1
+  alarm_name          = "ddos_detected_in_${var.env_name}"
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods  = 1
   metric_name         = "DDoSDetected"
   namespace           = "AWS/DDoSProtection"
   period              = 300
   statistic           = "Sum"
-  threshold           = 1
+  threshold           = 0
 
   actions_enabled = false
 }
