22ec0aee6fcda0e5d154520fb952347ae3ad564e
Sarah Young <sarah.young1@digital.cabinet-office.gov.uk>
2024-04-04 11:49:44 +0100

    Consolidate Shield related alarms into the same file
    * Move the DDoSDetected alarm into the shield.tf file
    * Follows the pattern of cloudfront_5xx_error_rate CloudWatch alarm
    * This allows me to more easily create a Route53 health check from the alarm and add it to our list of aggregated checks

--- infra/modules/environment/shield.tf
+++ infra/modules/environment/shield.tf
@@ -191,6 +191,20 @@ resource "aws_route53_health_check" "cloudfront_5xx_error_rate" {
   insufficient_data_health_status = "Healthy"
 }
 
+resource "aws_cloudwatch_metric_alarm" "ddos_detected" {
+  provider            = aws.us-east-1
+  alarm_name          = "ddos_detected_in_${var.env_name}"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "DDoSDetected"
+  namespace           = "AWS/DDoSProtection"
+  period              = 300
+  statistic           = "Sum"
+  threshold           = 0
+
+  actions_enabled = false
+}
+
 # The alb_healthy_host_count CloudWatch alarm is configured in modules/alerts/health-host-count.tf
 # There is a hidden dependency on the cloudwatch_alarm_name between this resource and the alarm
 // TODO: Resolve the hidden dependency on cloudwatch_alarm_name. One option: create an output and module declaration
