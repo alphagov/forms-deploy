aba7e9ac8f1acfeaee80f10ff8f712646d29421b
Sarah Young <sarah.young1@digital.cabinet-office.gov.uk>
2024-04-19 13:28:52 +0100

    Import AWSServiceRoleForAWSShield
    * Add AWSServiceRoleForAWSShield import block to import existing autogenerated AWSServiceRoleForAWSShield service linked roles
    * We will have to delete this in a follow-up commit
    
    Signed-off-by: Sarah Young <sarah.young1@digital.cabinet-office.gov.uk>

--- infra/deployments/forms/environment/main.tf
+++ infra/deployments/forms/environment/main.tf
@@ -13,3 +13,17 @@ module "environment" {
   enable_shield_advanced_healthchecks = var.environmental_settings.enable_shield_advanced_healthchecks
 }
 
+# TODO: Remove import and locals once AWSServiceRoleForAWSShield linked role is deployed
+import {
+  to = module.environment.aws_iam_service_linked_role.shield
+  id = "arn:aws:iam::${lookup(local.account_ids, var.environment_name)}:role/aws-service-role/shield.amazonaws.com/AWSServiceRoleForAWSShield"
+}
+
+locals {
+  account_ids = {
+    "dev"           = "498160065950"
+    "staging"       = "972536609845"
+    "production"    = "443944947292"
+    "user-research" = "619109835131"
+  }
+}
