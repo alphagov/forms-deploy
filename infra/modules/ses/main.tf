f64867bb431af6310bf6150fc295ff27d5056342
Sarah Young <sarah.young1@digital.cabinet-office.gov.uk>
2024-04-25 13:06:55 +0100

    Refactor SES and SQS resource names
    * Remove resource types from resource names since Terraform discourages using resource types in resource names: https://developer.hashicorp.com/terraform/language/style#resource-naming
    * Rename aws_ses_configuration_set to a noun which describes the rule in the Configuration Set
    * Rename aws_ses_event_destination to clarify the type of notification –– the destination is implied in the resource with the `sns_destination` block and is explicit in the AWS console UI
    * Update deployer policy to reflect SNS resource naming changes and add missing action from ManageSESPolicies
    
    Signed-off-by: Sarah Young <sarah.young1@digital.cabinet-office.gov.uk>

--- infra/modules/ses/main.tf
+++ infra/modules/ses/main.tf
@@ -17,7 +17,7 @@ data "aws_iam_policy_document" "ses_sender" {
     ]
     resources = [
       "arn:aws:ses:eu-west-2:${local.account_id}:identity/*",
-      "arn:aws:ses:eu-west-2:${local.account_id}:configuration-set/ses_bounces_and_complaints_topic"
+      "arn:aws:ses:eu-west-2:${local.account_id}:configuration-set/bounces_and_complaints_handling_rule"
     ]
     condition {
       test     = "ForAnyValue:StringEquals"
@@ -33,20 +33,20 @@ resource "aws_iam_policy" "ses_sender" {
   policy      = data.aws_iam_policy_document.ses_sender.json
 }
 
-resource "aws_ses_event_destination" "sns" {
-  name                   = "ses-sns"
-  configuration_set_name = aws_ses_configuration_set.ses_bounces_and_complaints_topic.name
+resource "aws_ses_event_destination" "failed_email_notification" {
+  name                   = "failed_email_notification"
+  configuration_set_name = aws_ses_configuration_set.bounces_and_complaints_handling_rule.name
   enabled                = true
   matching_types         = ["bounce", "complaint", "reject"]
 
   sns_destination {
-    topic_arn = aws_sns_topic.ses_bounces_and_complaints_topic.arn
+    topic_arn = aws_sns_topic.ses_bounces_and_complaints.arn
   }
 }
 
-resource "aws_ses_configuration_set" "ses_bounces_and_complaints_topic" {
+resource "aws_ses_configuration_set" "bounces_and_complaints_handling_rule" {
   #checkov:skip=CKV_AWS_365 We'll look at this later
-  name = "ses_bounces_and_complaints_topic"
+  name = "bounces_and_complaints_handling_rule"
 
   reputation_metrics_enabled = true
 }
