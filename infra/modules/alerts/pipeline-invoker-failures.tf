597f8f97a1c7b26da342cc1e2d0b5ed4381befc0
Sarah Young <sarah.young1@digital.cabinet-office.gov.uk>
2024-05-03 15:17:12 +0100

    Add alarm on pipeline-invoker failures
    * Create CloudWatch metric alarm which alerts when CloudWatch metrics records an error in the Lambda, indicating a failed Lambda invocation
    
    Signed-off-by: Sarah Young <sarah.young1@digital.cabinet-office.gov.uk>

---
+++ infra/modules/alerts/pipeline-invoker-failures.tf
@@ -0,0 +1,31 @@
+resource "aws_cloudwatch_metric_alarm" "pipeline_invoker_failure" {
+  alarm_name          = "pipeline-invoker-failed-invocation"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 1
+  metric_name         = "Errors"
+  namespace           = "AWS/Lambda"
+  period              = 60
+  statistic           = "Sum"
+  threshold           = 1
+  alarm_description   = <<EOF
+    We have a Lambda (pipeline_invoker) which receives events from AWS EventBridge and invokes CodePipeline.
+    We use it to trigger the production pipelines when the staging pipelines have completed.
+
+    This alarm will enter the alarm state when CloudWatch metrics records an error in the Lambda, indicating a
+    failed Lambda invocation.
+
+    NEXT STEPS:
+    1. Navigate to the Lambda "Monitoring" tab to view invocations, error count, and links to Lambda logs in CloudWatch:
+
+    https://eu-west-2.console.aws.amazon.com/lambda/home?region=eu-west-2#/functions/${urlencode("${var.environment}")}-pipeline-invoker?tab=monitoring
+
+    2. From "Monitoring" tab click "View in CloudWatch logs" to review logs and diagnose errors
+
+EOF
+
+  dimensions = {
+    FunctionName = "${var.environment}-pipeline-invoker"
+  }
+
+  alarm_actions = [aws_sns_topic.alert_zendesk.arn]
+}
