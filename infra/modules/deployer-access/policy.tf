e54bf6255e74630b62e143795061980eea6d84ce
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2024-05-21 13:31:43 +0300

    Allow deployer access policy to update Lambda function configuration
    Permission to do this action is needed when changing the runtime of an
    AWS Lambda function.

--- infra/modules/deployer-access/policy.tf
+++ infra/modules/deployer-access/policy.tf
@@ -452,6 +452,7 @@ data "aws_iam_policy_document" "pipelines" {
       "lambda:TagResource",
       "lambda:UntagResource",
       "lambda:UpdateFunctionCode",
+      "lambda:UpdateFunctionConfiguration",
     ]
 
     resources = [
