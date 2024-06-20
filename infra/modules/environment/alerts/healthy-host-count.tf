0e80406a8f4dc94ae93e09ab9e18a66144692cf1
Sarah Young <sarah.young1@digital.cabinet-office.gov.uk>
2024-06-20 13:16:06 +0100

    Reformat file
    * `terraform fmt` reformatted the alarm_name, alarm_description, and EOF spacing
    
    Signed-off-by: Sarah Young <sarah.young1@digital.cabinet-office.gov.uk>

--- infra/modules/environment/alerts/healthy-host-count.tf
+++ infra/modules/environment/alerts/healthy-host-count.tf
@@ -1,9 +1,9 @@
 resource "aws_cloudwatch_metric_alarm" "healthy_host_alarms" {
   for_each = data.aws_lb_target_group.target_groups
 
-  alarm_name          = "alb_healthy_host_count_${each.value.name}"
-  alarm_description   = <<EOF
-    Less than ${var.minimum_healthy_host_count} healthy instances of ${each.value.name}"
+  alarm_name        = "alb_healthy_host_count_${each.value.name}"
+  alarm_description = <<EOF
+    Less than ${var.minimum_healthy_host_count} healthy instances of ${each.value.name}
 
     Check the application logs and ECS event logs to understand why the task stopped
   EOF
