82974257df144a9cef4756a2a78141473920e9ae
Andy Hunt <andy.hunt@digital.cabinet-office.gov.uk>
2024-06-21 11:30:14 +0100

    Force environment module to forget alerts, monitoring and shield resources
    We're going to move the alerts, monitoring, and shield resources to a new Terraform
    root. We don't want Terraform to destroy and re-create them, so that we don't
    have a gap in alert coverage. Instead we want Terraform to simply forget them,
    so we can import them in a later commit.

--- infra/modules/environment/monitoring.tf
+++ infra/modules/environment/monitoring.tf
@@ -1,8 +1,7 @@
-module "monitoring" {
-  source = "./monitoring"
+removed {
+  from = module.monitoring
 
-  environment_name               = var.env_name
-  environment_type               = var.env_type
-  scheduled_smoke_tests_settings = var.scheduled_smoke_tests_settings
-  smoke_test_alarm_sns_topic_arn = module.alerts.sns_topic_alert_pagerduty.arn
+  lifecycle {
+    destroy = false
+  }
 }
