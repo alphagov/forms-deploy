alphagov/forms-deploy

commit 67c3f4bb01a8ae85e203b1fca2257d155fc25485

Add settings flag for act as user functionality
Co-authored-by: Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
Co-authored-by: Macro Tranchino <marco.tranchino@digital.cabinet-office.gov.uk>

infra/deployments/forms/tfvars/staging.tfvars
@@ -35,7 +35,8 @@ forms_admin_settings = {
   auth_provider              = "auth0"
   previous_auth_provider     = "gds_sso"
   cloudwatch_metrics_enabled = true
-  analytics_enabled          = true
+  analytics_enabled          = false
+  act_as_user_enabled        = true
   govuk_app_domain           = "staging.publishing.service.gov.uk"
   synchronize_to_mailchimp   = false
 }

infra/deployments/forms/forms-admin/main.tf
@@ -23,5 +23,6 @@ module "forms_admin" {
   previous_auth_provider         = var.forms_admin_settings.previous_auth_provider
   cloudwatch_metrics_enabled     = var.forms_admin_settings.cloudwatch_metrics_enabled
   analytics_enabled              = var.forms_admin_settings.analytics_enabled
+  act_as_user_enabled            = var.forms_admin_settings.act_as_user_enabled
   enable_mailchimp_sync          = var.forms_admin_settings.synchronize_to_mailchimp
 }

infra/deployments/forms/inputs.tf
@@ -91,6 +91,7 @@ variable "forms_admin_settings" {
     previous_auth_provider     = string
     cloudwatch_metrics_enabled = bool
     analytics_enabled          = bool
+    act_as_user_enabled        = bool
     govuk_app_domain           = string
     synchronize_to_mailchimp   = bool
   })

infra/deployments/forms/tfvars/dev.tfvars
@@ -34,6 +34,7 @@ forms_admin_settings = {
   previous_auth_provider     = "gds_sso"
   cloudwatch_metrics_enabled = false
   analytics_enabled          = false
+  act_as_user_enabled        = false
   govuk_app_domain           = "integration.publishing.service.gov.uk"
   synchronize_to_mailchimp   = false
 }

infra/deployments/forms/tfvars/production.tfvars
@@ -52,6 +52,7 @@ forms_admin_settings = {
   previous_auth_provider     = "gds_sso"
   cloudwatch_metrics_enabled = true
   analytics_enabled          = false
+  act_as_user_enabled        = false
   govuk_app_domain           = "publishing.service.gov.uk"
   synchronize_to_mailchimp   = true
 }

infra/deployments/forms/tfvars/user-research.tfvars
@@ -34,6 +34,7 @@ forms_admin_settings = {
   previous_auth_provider     = null
   cloudwatch_metrics_enabled = false
   analytics_enabled          = true
+  act_as_user_enabled        = false
   govuk_app_domain           = ""
   synchronize_to_mailchimp   = false
 }

infra/modules/forms-admin/main.tf
@@ -153,6 +153,10 @@ module "ecs_service" {
     {
       name  = "SETTINGS__ANALYTICS_ENABLED",
       value = var.analytics_enabled
+    },
+    {
+      name  = "SETTINGS__ACT_AS_USER_ENABLED",
+      value = var.act_as_user_enabled
     }
   ]
 

infra/modules/forms-admin/variables.tf
@@ -83,6 +83,12 @@ variable "analytics_enabled" {
   default     = false
 }
 
+variable "act_as_user_enabled" {
+  description = "Enables act as user functionality for super admins"
+  type        = bool
+  default     = false
+}
+
 variable "enable_mailchimp_sync" {
   description = "Whether to synchronise the MailChimp mailing lists from the forms-admin user data"
   type        = bool

commit ead4aab3130cadd52b68742a2c911f7a0c8d699f

Enable google-analytics in forms-admin
Commit 67c3f4b accidentally turned off the analtics_enabled flag for
forms-admin in staging (probably due to a faulty rebase). I think it is
supposed to be enabled, this commits reverts that change.

infra/deployments/forms/tfvars/staging.tfvars
@@ -35,7 +35,7 @@ forms_admin_settings = {
   auth_provider              = "auth0"
   previous_auth_provider     = "gds_sso"
   cloudwatch_metrics_enabled = true
-  analytics_enabled          = false
+  analytics_enabled          = true
   act_as_user_enabled        = true
   govuk_app_domain           = "staging.publishing.service.gov.uk"
   synchronize_to_mailchimp   = false