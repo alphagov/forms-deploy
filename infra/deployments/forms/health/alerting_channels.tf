4efb1cee404f2fc33b8e2f48499056c6e9a6134d
Andy Hunt <andy.hunt@digital.cabinet-office.gov.uk>
2024-07-24 10:36:59 +0100

    Remove now-unnecessary import and removed blocks
    The import blocks are unnecessary becuase they've run through all of the
    environments and taken effect.
    
    The removed blocks are unnecessary because the resources have been removed from
    the state files.

--- infra/deployments/forms/health/alerting_channels.tf
+++ infra/deployments/forms/health/alerting_channels.tf
@@ -14,24 +14,3 @@ resource "aws_sns_topic_subscription" "email" {
   protocol  = "email"
   endpoint  = data.aws_ssm_parameter.email_zendesk.value
 }
-
-import {
-  id = "arn:aws:sns:us-east-1:${data.aws_caller_identity.current.account_id}:cloudwatch-alarms"
-  to = aws_sns_topic.cloudwatch_alarms
-}
-
-locals {
-  zendesk_subscription_guids = {
-    "dev" : "23357c1f-7245-4563-9d84-ce19e31535e4"
-    "staging" : null,
-    "production" : "5fa4dd7d-a3f7-4f7c-a332-32a79ade0bfb",
-    "user-research" : null,
-  }
-}
-
-import {
-  for_each = local.zendesk_subscription_guids[var.environment_name] != null ? [1] : []
-
-  id = "${aws_sns_topic.cloudwatch_alarms.arn}:${local.zendesk_subscription_guids[var.environment_name]}"
-  to = aws_sns_topic_subscription.email
-}
