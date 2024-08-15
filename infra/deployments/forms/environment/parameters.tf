7325927ab97d248a57b0dde0d650408245216620
Catalina Garcia <catalina.garcia@digital.cabinet-office.gov.uk>
2024-08-15 15:08:58 +0100

    Stop managing some parameters in terraform temporarily
    We imported some ssm parameters incorrectly
    (https://github.com/alphagov/forms-deploy/pull/919) by using the parameter id
    instead of the `name`.
    
    We're removing them from terraform (but not destroying them from aws) so that we
    can re-import them correctly

--- infra/deployments/forms/environment/parameters.tf
+++ infra/deployments/forms/environment/parameters.tf
@@ -1,34 +1,17 @@
 data "aws_caller_identity" "current" {}
 
-# Auth0
-# These are the client ID and client secret of the machine to machine application in the GOV.UK Forms tenant for the account you are terraforming
-# The values are different in each account, and some accounts may not used them (for example, User Research)
-resource "aws_ssm_parameter" "auth0_access_client_id" {
-  #checkov:skip=CKV_AWS_337:The parameter is already using the default key
-
-  name        = "/terraform/auth0-access/client-id"
-  description = "The client ID for the Auth0 'Terraform access' app for this environment"
-  type        = "SecureString"
-  value       = "dummy-value"
+removed {
+  from = aws_ssm_parameter.auth0_access_client_id
 
   lifecycle {
-    ignore_changes = [
-      value
-    ]
+    destroy = false
   }
 }
 
-resource "aws_ssm_parameter" "auth0_access_client_secret" {
-  #checkov:skip=CKV_AWS_337:The parameter is already using the default key
-
-  name        = "/terraform/auth0-access/client-secret"
-  description = "The client secret for the Auth0 'Terraform access' app for this environment"
-  type        = "SecureString"
-  value       = "dummy-value"
+removed {
+  from = aws_ssm_parameter.auth0_access_client_secret
 
   lifecycle {
-    ignore_changes = [
-      value
-    ]
+    destroy = false
   }
 }
