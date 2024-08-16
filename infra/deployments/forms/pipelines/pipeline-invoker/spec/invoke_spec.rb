d2433d0d57288304f1100bf0467c52d21526244f
Sarah Young <sarah.young1@digital.cabinet-office.gov.uk>
2024-08-16 10:28:58 +0100

    Add test to verify correct API parameters
    * Check we're passing the correct parameters to the AWS CodePipeline API
    * This is kind of testing our ability to test more than the code, but changing the valuse of `actionName`, `revisionType`, and `revisionValue` makes the test fail which is what I was hoping
    * It could be worth sense checking this testing approach with an Rspec/Ruby maestro

--- infra/deployments/forms/pipelines/pipeline-invoker/spec/invoke_spec.rb
+++ infra/deployments/forms/pipelines/pipeline-invoker/spec/invoke_spec.rb
@@ -11,11 +11,19 @@ describe Invoke do
   let(:container_image_uri) { 'CONTAINER_IMAGE_URI' }
   let(:event) do
     {
+      'client_request_token' => 'some_token',
       'name' => pipeline_name,
       'variables' => [
         {
           'name' => 'container_image_uri',
           'value' => container_image_uri
+        }
+      ],
+      'sourceRevisions' => [
+        {
+          'actionName' => 'actionName',
+          'revisionType' => 'revisionType',
+          'revisionValue' => 'revisionValue'
         }
       ]
     }
@@ -25,9 +33,33 @@ describe Invoke do
     allow(Aws::CodePipeline::Client).to receive(:new).and_return(codepipeline_client)
   end
 
-  describe "#main" do
+  describe '#main' do
     it 'invokes the AWS CodePipeline API' do
         expect(codepipeline_client).to receive(:start_pipeline_execution)
+
+        Invoke.main(event: event, context: double('context'))
+    end
+
+    it 'invokes the AWS CodePipeline API with correct parameters' do
+        expected_params = {
+          client_request_token: 'some_token',
+          name: pipeline_name,
+          source_revisions: [
+            {
+              action_name: 'actionName',
+              revision_type: 'revisionType',
+              revision_value: 'revisionValue'
+            }
+          ],
+          variables: [
+            {
+              name: 'container_image_uri',
+              value: container_image_uri
+            }
+          ]
+        }
+
+        expect(codepipeline_client).to receive(:start_pipeline_execution).with(expected_params)
 
         Invoke.main(event: event, context: double('context'))
     end
