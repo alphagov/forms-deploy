cc901f6213218394030651df5a3e228c949675ca
Sarah Young <sarah.young1@digital.cabinet-office.gov.uk>
2024-08-16 15:02:07 +0100

    Fix linting issues
    * Ran `rubocop -`a in the directory to identify and fix any Ruby linting issues

--- infra/deployments/forms/pipelines/pipeline-invoker/spec/invoke_spec.rb
+++ infra/deployments/forms/pipelines/pipeline-invoker/spec/invoke_spec.rb
@@ -1,31 +1,31 @@
-require 'json'
-require 'aws-sdk-codepipeline'
-require 'logger'
-require 'spec_helper'
+require "json"
+require "aws-sdk-codepipeline"
+require "logger"
+require "spec_helper"
 
-require_relative '../invoke.rb'
+require_relative "../invoke"
 
 describe Invoke do
   let(:codepipeline_client) { instance_double(Aws::CodePipeline::Client) }
-  let(:pipeline_name) { 'PIPELINE_TO_INVOKE' }
-  let(:container_image_uri) { 'CONTAINER_IMAGE_URI' }
+  let(:pipeline_name) { "PIPELINE_TO_INVOKE" }
+  let(:container_image_uri) { "CONTAINER_IMAGE_URI" }
   let(:event) do
     {
-      'client_request_token' => 'some_token',
-      'name' => pipeline_name,
-      'variables' => [
+      "client_request_token" => "some_token",
+      "name" => pipeline_name,
+      "variables" => [
         {
-          'name' => 'container_image_uri',
-          'value' => container_image_uri
-        }
+          "name" => "container_image_uri",
+          "value" => container_image_uri,
+        },
       ],
-      'sourceRevisions' => [
+      "sourceRevisions" => [
         {
-          'actionName' => 'some_action',
-          'revisionType' => 'some_revision_type',
-          'revisionValue' => 'some_revision_value'
-        }
-      ]
+          "actionName" => "some_action",
+          "revisionType" => "some_revision_type",
+          "revisionValue" => "some_revision_value",
+        },
+      ],
     }
   end
 
@@ -33,36 +33,36 @@ describe Invoke do
     allow(Aws::CodePipeline::Client).to receive(:new).and_return(codepipeline_client)
   end
 
-  describe '#main' do
-    it 'creates an Aws::CodePipeline::Client with the correct region' do
-      expect(Aws::CodePipeline::Client).to receive(:new).with(region: 'eu-west-2').and_return(codepipeline_client)
+  describe "#main" do
+    it "creates an Aws::CodePipeline::Client with the correct region" do
+      expect(Aws::CodePipeline::Client).to receive(:new).with(region: "eu-west-2").and_return(codepipeline_client)
       expect(codepipeline_client).to receive(:start_pipeline_execution)
 
-      Invoke.main(event: event, context: double('context'))
+      Invoke.main(event:, context: double("context"))
     end
 
-    it 'invokes the AWS CodePipeline API with correct parameters' do
-        expected_params = {
-          client_request_token: 'some_token',
-          name: pipeline_name,
-          source_revisions: [
-            {
-              action_name: 'some_action',
-              revision_type: 'some_revision_type',
-              revision_value: 'some_revision_value'
-            }
-          ],
-          variables: [
-            {
-              name: 'container_image_uri',
-              value: container_image_uri
-            }
-          ]
-        }
+    it "invokes the AWS CodePipeline API with correct parameters" do
+      expected_params = {
+        client_request_token: "some_token",
+        name: pipeline_name,
+        source_revisions: [
+          {
+            action_name: "some_action",
+            revision_type: "some_revision_type",
+            revision_value: "some_revision_value",
+          },
+        ],
+        variables: [
+          {
+            name: "container_image_uri",
+            value: container_image_uri,
+          },
+        ],
+      }
 
-        expect(codepipeline_client).to receive(:start_pipeline_execution).with(expected_params)
+      expect(codepipeline_client).to receive(:start_pipeline_execution).with(expected_params)
 
-        Invoke.main(event: event, context: double('context'))
+      Invoke.main(event:, context: double("context"))
     end
   end
 end
