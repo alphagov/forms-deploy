c1d0a08117ba608333fc49bfb5eccfc15f7dfad7
alice-carr <alice.carr@digital.cabinet-office.gov.uk>
2024-08-19 12:24:39 +0100

    Remove old /database/master-password
    We have removed the /database/master-password and replaced it with /{env}/database/root-password.
    This is to ensure we can have more than one environment in an account, and to move to the terminology of root/main over master

--- infra/modules/rds/parameters.tf
+++ infra/modules/rds/parameters.tf
@@ -1,9 +1,10 @@
 # The database passwords were manually generated when creating the environments
-resource "aws_ssm_parameter" "database_password_for_master_user" {
+# Note that the RDS docs use the terminology 'master password' - we are using 'root password'
+resource "aws_ssm_parameter" "database_password_for_root_user" {
   #checkov:skip=CKV_AWS_337:The parameter is already using the default key
 
-  name        = "/${var.env_name}/database/master-password"
-  description = "Password for the default master user created by Terraform"
+  name        = "/${var.env_name}/database/root-password"
+  description = "Password for the default root user created by Terraform"
   type        = "SecureString"
   value       = "dummy-value"
 
@@ -12,24 +13,6 @@ resource "aws_ssm_parameter" "database_password_for_master_user" {
       value
     ]
   }
-}
-
-# This will be deleted in a later commit
-data "aws_ssm_parameter" "database_password_for_copying" {
-  name = "/database/master-password"
-}
-
-# The database passwords were manually generated when creating the environments
-# Note that the RDS docs use the terminology 'master password' - we are using 'root password'
-# Lifecycle will be added in a later commit
-resource "aws_ssm_parameter" "database_password_for_root_user" {
-  #checkov:skip=CKV_AWS_337:The parameter is already using the default key
-  #checkov:skip=CKV2_FORMS_AWS_7:We want to alter the value in this PR. This will be removed in a subsequent PR
-
-  name        = "/${var.env_name}/database/root-password"
-  description = "Password for the default root user created by Terraform"
-  type        = "SecureString"
-  value       = data.aws_ssm_parameter.database_password.value
 }
 
 resource "aws_ssm_parameter" "database_password_for_forms_admin_app" {
