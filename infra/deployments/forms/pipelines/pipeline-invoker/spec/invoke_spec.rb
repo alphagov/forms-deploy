cf5a6cc5657990e62a2ee8cc09288e88521afb8c
Sarah Young <sarah.young1@digital.cabinet-office.gov.uk>
2024-08-19 14:30:29 +0100

    Add granular payload tests
    * Attempt to test generating the payload object from the `variables` and `sourceRevisions` block in an event.
    * I'm not wedded to this approach because it's not behaviour driven. Ideally tests should describe what the code does (its behavior) rather than how it does it (data structures).
    * I added them because a misconfigured data structure caused a failure but I'm happy to remove them.
    * We also implicitly test the data structure in the API call test.
    
    Signed-off-by: Sarah Young <sarah.young1@digital.cabinet-office.gov.uk>

--- infra/deployments/forms/pipelines/pipeline-invoker/spec/invoke_spec.rb
+++ infra/deployments/forms/pipelines/pipeline-invoker/spec/invoke_spec.rb
@@ -8,6 +8,7 @@ require_relative "../invoke"
 describe PipelineInvoker::Handler do
   let(:pipeline_invoker) { described_class.new }
   let(:codepipeline_client) { instance_double(Aws::CodePipeline::Client) }
+  let(:logger) { instance_double(Logger) }
   let(:pipeline_name) { "PIPELINE_TO_INVOKE" }
   let(:container_image_uri) { "CONTAINER_IMAGE_URI" }
   let(:event) do
@@ -35,6 +36,96 @@ describe PipelineInvoker::Handler do
   end
 
   describe "#process" do
+    it "initializes payload with the correct name from event" do
+      payload = {}
+      payload["name"] = event["name"]
+
+      expect(payload["name"]).to eq(pipeline_name)
+    end
+
+    it "populates payload['variables'] when event['variables'] is not nil" do
+      payload = {}
+      payload["variables"] = []
+
+      unless event["variables"].nil?
+        event["variables"].each do |var|
+          payload["variables"] << {
+            name: var["name"],
+            value: var["value"],
+          }
+        end
+      end
+
+      expect(payload["variables"]).to eq([
+        { name: "container_image_uri", value: container_image_uri },
+      ])
+    end
+
+    it "sets payload['variables'] to an empty array when event['variables'] is nil" do
+      event_with_nil_variables = { "name" => pipeline_name, "variables" => nil }
+
+      payload = {}
+      payload["variables"] = []
+
+      unless event_with_nil_variables["variables"].nil?
+        event_with_nil_variables["variables"].each do |variable|
+          payload["variables"] << {
+            name: variable["name"],
+            value: variable["value"],
+          }
+        end
+      end
+
+      expect(payload["variables"]).to eq([])
+    end
+
+    it "initializes payload with the correct sourceRevisions from event" do
+      payload = {}
+      payload["sourceRevisions"] = event["sourceRevisions"]
+
+      expect(payload["sourceRevisions"]).to eq([{ "actionName" => "some_action", "revisionType" => "some_revision_type", "revisionValue" => "some_revision_value" }])
+    end
+
+    it "populates payload['sourceRevisions'] when event['sourceRevisions'] is not nil" do
+      payload = {}
+      payload["sourceRevisions"] = []
+
+      unless event["sourceRevisions"].nil?
+        event["sourceRevisions"].each do |revision|
+          payload["sourceRevisions"] << {
+            action_name: revision["actionName"],
+            revision_type: revision["revisionType"],
+            revision_value: revision["revisionValue"],
+          }
+        end
+      end
+
+      expect(payload["sourceRevisions"]).to eq([{
+        action_name: "some_action",
+        revision_type: "some_revision_type",
+        revision_value: "some_revision_value",
+      }])
+    end
+
+    it "sets payload['sourceRevisions'] to an empty array when event['sourceRevisions'] is nil" do
+      event_with_nil_source_revisions = { "name" => pipeline_name, "sourceRevisions" => nil }
+
+      payload = {}
+      payload["sourceRevisions"] = []
+
+      unless event_with_nil_source_revisions["sourceRevisions"].nil?
+        event_with_nil_source_revisions["sourceRevisions"].each do |revision|
+          payload["sourceRevisions"] << {
+            action_name: revision["actionName"],
+            revision_type: revision["revisionType"],
+            revision_value: revision["revisionValue"],
+          }
+        end
+      end
+
+      expect(payload["sourceRevisions"]).to eq([])
+    end
+
     it "creates an Aws::CodePipeline::Client with the correct region" do
       expect(Aws::CodePipeline::Client).to receive(:new).with(region: "eu-west-2").and_return(codepipeline_client)
       expect(codepipeline_client).to receive(:start_pipeline_execution)
