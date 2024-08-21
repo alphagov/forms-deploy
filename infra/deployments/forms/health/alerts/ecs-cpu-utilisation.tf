76dbf2eab918d89c0b5f7e71d5c9b8658bc211f3
Radha Kotyankar <radha.kotyankar@digital.cabinet-office.gov.uk>
2024-08-21 16:00:43 +0100

    Add cpu utilisation alarm

---
+++ infra/deployments/forms/health/alerts/ecs-cpu-utilisation.tf
@@ -0,0 +1,26 @@
+data "aws_ecs_cluster" "cluster_name" {
+  cluster_name = "forms-${var.environment}"
+}
+
+resource "aws_cloudwatch_metric_alarm" "ecs_cpu_utilization_alarm" {
+  for_each = toset(local.apps)
+
+  alarm_name          = "ecs-${each.value}-${var.environment}-cpu-utilization-alarm"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/ECS"
+  period              = "300"
+  statistic           = "Average"
+  threshold           = "70"
+  alarm_description   = "This metric checks the CPU utilization of the ECS service: ${each.value} in ${var.environment} environment"
+
+  dimensions = {
+    ClusterName = data.aws_ecs_cluster.cluster_name.id
+    ServiceName = each.value
+  }
+
+  alarm_actions      = [local.chatbot_alerts_channel_sns_topic]
+  ok_actions         = [local.chatbot_alerts_channel_sns_topic]
+  treat_missing_data = "missing"
+}
