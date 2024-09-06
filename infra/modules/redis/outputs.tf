85dd3c0f49c1010f1520608505b0c0236c622ca0
Sarah Young <sarah.young1@digital.cabinet-office.gov.uk>
2024-09-06 10:46:40 +0100

    Fix validation issue
    * Our validation expects the aws_elasticache_replication_group resource to use the field "description" not "replication_group_description" despite the fact that the latter is what's on the Terraform documentation
    * Remove unnecessary comments -- we added these during pairing to aid learning and understanding but we don't need them anymore

--- infra/modules/redis/outputs.tf
+++ infra/modules/redis/outputs.tf
@@ -1,6 +1,3 @@
-# We need to output or "expose" values from the elasticache.tf resource
-# The only way we can expose the values is through outputs in a file
-# at the same level as elasticache.tf
 output "elasticache_primary_endpoint_address" {
   value = aws_elasticache_replication_group.forms-runner.primary_endpoint_address
 }
