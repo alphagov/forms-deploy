76226981492349062b5123f3064b4f901d9b58a4
Radha Kotyankar <radha.kotyankar@digital.cabinet-office.gov.uk>
2024-09-09 10:01:29 +0100

    Add explicit values for vpc and subnets using environment outputs

--- infra/modules/redis/network.tf
+++ infra/modules/redis/network.tf
@@ -1,23 +1,5 @@
-data "aws_vpc" "forms" {
-  filter {
-    name   = "tag:Name"
-    values = ["forms-${var.env_name}"]
-  }
-}
-
-data "aws_subnets" "private" {
-  filter {
-    name = "tag:Name"
-    values = [
-      "private-a-${var.env_name}",
-      "private-b-${var.env_name}",
-      "private-c-${var.env_name}"
-    ]
-  }
-}
-
 resource "aws_elasticache_subnet_group" "redis" {
   name        = "redis-${var.env_name}"
   description = "redis-${var.env_name} ElastiCache subnet group"
-  subnet_ids  = data.aws_subnets.private.ids
+  subnet_ids  = var.subnet_ids
 }
