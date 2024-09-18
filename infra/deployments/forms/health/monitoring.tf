83a6f92e98e58da51242dff8ed2fbf2e0f3f694b
Sarah Young <sarah.young1@digital.cabinet-office.gov.uk>
2024-09-18 17:02:36 +0100

    Update /health/monitoring to use Eventbridge DLQ url from dependency
    * Add a new `eventbridge_dead_letter_queue_url` property to the `monitoring` modular declaration, pointing to the dependency on the /environment state file
    * Update the variables file in the /monitoring module to include this new property
    * Replace hardcoded URL in the metric alarm with a reference to the variable
    * This is to improve code flexibility and make it easier to rebuild an environment from scratch by removing implicit lookups or unnecessary hardcoded values

--- infra/deployments/forms/health/monitoring.tf
+++ infra/deployments/forms/health/monitoring.tf
@@ -1,9 +1,10 @@
 module "monitoring" {
   source = "./monitoring"
 
-  environment_name               = var.environment_name
-  environment_type               = var.environment_type
-  scheduled_smoke_tests_settings = var.scheduled_smoke_tests_settings
-  deploy_account_id              = var.deploy_account_id
-  smoke_test_alarm_sns_topic_arn = module.pagerduty_eu_west_2.topic_arn
+  environment_name                  = var.environment_name
+  environment_type                  = var.environment_type
+  scheduled_smoke_tests_settings    = var.scheduled_smoke_tests_settings
+  deploy_account_id                 = var.deploy_account_id
+  smoke_test_alarm_sns_topic_arn    = module.pagerduty_eu_west_2.topic_arn
+  eventbridge_dead_letter_queue_url = data.terraform_remote_state.forms_environment.outputs.eventbridge_dead_letter_queue_url
 }
