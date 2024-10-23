0f87d43828d7711d3c0f029cc157d4a4c0908e24
Catalina Garcia <catalina.garcia@digital.cabinet-office.gov.uk>
2024-10-23 15:22:21 +0100

    Add link to Splunk report in alarm
    We want people to be able to see this report easily without needing to figure
    out what search to run.
    
    We're still unclear about when exactly we should block an ip.

--- infra/deployments/forms/health/alerts/cloudfront.tf
+++ infra/deployments/forms/health/alerts/cloudfront.tf
@@ -2,7 +2,7 @@ resource "aws_cloudwatch_metric_alarm" "cloudfront_reached_ip_rate_limit" {
   provider = aws.us-east-1
 
   alarm_name        = "${var.environment}-reached-ip-rate-limit"
-  alarm_description = "The number of blocked requests is greater than 1 in a 5-min window. Check Splunk to find the attacking IP and add it to the blocked list"
+  alarm_description = "The number of blocked requests is greater than 1 in a 5-min window. [Check the Splunk report](https://gds.splunkcloud.com/en-GB/app/gds-543-forms/report?s=%2FservicesNS%2Fnobody%2Fgds-543-forms%2Fsaved%2Fsearches%2FBlocks%2520from%2520AWS%2520WAF&sid=_Y2F0YWxpbmEuZ2FyY2lhQGRpZ2l0YWwuY2FiaW5ldC1vZmZpY2UuZ292LnVr_Y2F0YWxpbmEuZ2FyY2lhQGRpZ2l0YWwuY2FiaW5ldC1vZmZpY2UuZ292LnVr_Z2RzLTU0My1mb3Jtcw__RMD5fc9d53b739302fc6_at_1729692555_82682&display.page.search.mode=verbose&dispatch.sample_ratio=1&earliest=-7d%40h&latest=now) to find the attacking IP and add it to the blocked list (ips_to_block) if we are concerned."
 
   comparison_operator = "GreaterThanThreshold"
   threshold           = 1
