78c6b33748b4ba04f50a76db1eab7fae3f9dc7e2
Andy Hunt <andy.hunt@digital.cabinet-office.gov.uk>
2024-11-20 14:02:26 +0000

    Create new environment-specific SSM parameters for Auth0 creds
    The current parameter names for the Auth0 credentials don't contain the
    environment name. This makes them apply account-wide. We need them to be
    environment specific so each environment can get its own Auth0 tenancy if we
    want.
    
    This commit creates the new parameters, but does not set their value. The
    value will be set manually in the accounts. Future commits will remove the
    old ones and refactor the new ones to not include "_env_specific" in the
    resource name.

--- infra/deployments/forms/environment/parameters.tf
+++ infra/deployments/forms/environment/parameters.tf
@@ -18,10 +18,40 @@ resource "aws_ssm_parameter" "auth0_access_client_id" {
   }
 }
 
+resource "aws_ssm_parameter" "auth0_access_client_id_env_specific" {
+  #checkov:skip=CKV_AWS_337:The parameter is already using the default key
+
+  name        = "/terraform/${var.environment_name}/auth0-access/client-id"
+  description = "The client ID for the Auth0 'Terraform access' app for this environment"
+  type        = "SecureString"
+  value       = "dummy-value"
+
+  lifecycle {
+    ignore_changes = [
+      value
+    ]
+  }
+}
+
 resource "aws_ssm_parameter" "auth0_access_client_secret" {
   #checkov:skip=CKV_AWS_337:The parameter is already using the default key
 
   name        = "/terraform/auth0-access/client-secret"
+  description = "The client secret for the Auth0 'Terraform access' app for this environment"
+  type        = "SecureString"
+  value       = "dummy-value"
+
+  lifecycle {
+    ignore_changes = [
+      value
+    ]
+  }
+}
+
+resource "aws_ssm_parameter" "auth0_access_client_secret_env_specific" {
+  #checkov:skip=CKV_AWS_337:The parameter is already using the default key
+
+  name        = "/terraform/${var.environment_name}/auth0-access/client-secret"
   description = "The client secret for the Auth0 'Terraform access' app for this environment"
   type        = "SecureString"
   value       = "dummy-value"
