9749b74f2c4203755be1882b4c0a45e313c133e2
Andy Hunt <andy.hunt@digital.cabinet-office.gov.uk>
2024-11-20 10:01:57 +0000

    Remove state file locking from deploy/ deployments
    All of the deploy/ deployments are deployed by hand, so we don't have the
    risk of contention. We remove the state file locking piece we previously
    added so that we don't need to manage that additional complexity in code.

--- infra/deployments/deploy/tools/dependencies.tf
+++ infra/deployments/deploy/tools/dependencies.tf
@@ -2,9 +2,8 @@ data "terraform_remote_state" "deploy_ecr" {
   backend = "s3"
 
   config = {
-    key            = "ecr.tfstate"
-    bucket         = var.bucket
-    region         = "eu-west-2"
-    dynamodb_table = var.dynamodb_table
+    key    = "ecr.tfstate"
+    bucket = "gds-forms-deploy-tfstate"
+    region = "eu-west-2"
   }
 }
