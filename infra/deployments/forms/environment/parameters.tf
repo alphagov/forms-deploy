045955bb6c1404e4d5efa675ca42bb721039eee3
Andy Hunt <andy.hunt@digital.cabinet-office.gov.uk>
2024-11-20 14:38:01 +0000

    Remove now-unused Auth0 client credentials parameters
    The previous commit began using the environment-specific client credentials parameters. The original, account-wide parameters can now be deleted.

--- infra/deployments/forms/environment/parameters.tf
+++ infra/deployments/forms/environment/parameters.tf
@@ -1,23 +1,6 @@
-data "aws_caller_identity" "current" {}
-
 # Auth0
 # These are the client ID and client secret of the machine to machine application in the GOV.UK Forms tenant for the account you are terraforming
 # The values are different in each account, and some accounts may not used them (for example, User Research)
-resource "aws_ssm_parameter" "auth0_access_client_id" {
-  #checkov:skip=CKV_AWS_337:The parameter is already using the default key
-
-  name        = "/terraform/auth0-access/client-id"
-  description = "The client ID for the Auth0 'Terraform access' app for this environment"
-  type        = "SecureString"
-  value       = "dummy-value"
-
-  lifecycle {
-    ignore_changes = [
-      value
-    ]
-  }
-}
-
 resource "aws_ssm_parameter" "auth0_access_client_id_env_specific" {
   #checkov:skip=CKV_AWS_337:The parameter is already using the default key
 
@@ -32,22 +15,6 @@ resource "aws_ssm_parameter" "auth0_access_client_id_env_specific" {
     ]
   }
 }
-
-resource "aws_ssm_parameter" "auth0_access_client_secret" {
-  #checkov:skip=CKV_AWS_337:The parameter is already using the default key
-
-  name        = "/terraform/auth0-access/client-secret"
-  description = "The client secret for the Auth0 'Terraform access' app for this environment"
-  type        = "SecureString"
-  value       = "dummy-value"
-
-  lifecycle {
-    ignore_changes = [
-      value
-    ]
-  }
-}
-
 resource "aws_ssm_parameter" "auth0_access_client_secret_env_specific" {
   #checkov:skip=CKV_AWS_337:The parameter is already using the default key
 
