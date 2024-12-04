581e611e32a34bbc07ed253fc7a34be3e7a9b645
Catalina Garcia <catalina.garcia@digital.cabinet-office.gov.uk>
2024-12-04 14:11:34 +0000

    Add bucket and KMS policy for file upload feature
    We are using our secure-bucket module as a base for the private bucket. However
    we're overriding the default encryption configuration to use a KMS key.
    
    The KMS key policy only allows forms-runner to use the key.

---
+++ infra/deployments/forms/environment/file-upload.tf
@@ -0,0 +1,55 @@
+module "file_upload_bucket" {
+  source = "../../../modules/secure-bucket"
+
+  name = "govuk-forms-file-upload"
+}
+
+# This configuration overrides the one in the secure-bucket module
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  bucket = module.file_upload_bucket.name
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.this.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
+
+resource "aws_kms_key" "this" {
+  description             = "This key is used to encrypt/decrypt bucket objects"
+  enable_key_rotation     = true
+  deletion_window_in_days = 10
+
+  policy = data.aws_iam_policy_document.key_policy.json
+}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_iam_policy_document" "key_policy" {
+  statement {
+    sid    = "Enable Iam Access"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+    actions   = ["kms:*"]
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "Allow use of the key"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${var.environment_name}-forms-runner-ecs-task"]
+    }
+    actions = [
+      "kms:DescribeKey",
+      "kms:Decrypt",
+      "kms:Encrypt"
+    ]
+    resources = ["*"]
+  }
+}
