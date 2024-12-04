d0939fc3d4a29a4693bc796ac6250eb953a0b1ad
Catalina Garcia <catalina.garcia@digital.cabinet-office.gov.uk>
2024-12-04 14:29:41 +0000

    Add bucket policy to allow forms-runner to manage it
    Forms-runner needs to be able to add objects to the bucket

--- infra/deployments/forms/environment/file-upload.tf
+++ infra/deployments/forms/environment/file-upload.tf
@@ -1,8 +1,30 @@
 module "file_upload_bucket" {
   source = "../../../modules/secure-bucket"
+  name   = "govuk-forms-file-upload"
 
-  name = "govuk-forms-file-upload"
+  extra_bucket_policies = [data.aws_iam_policy_document.forms_runner_access.json]
 }
+
+data "aws_iam_policy_document" "forms_runner_access" {
+  statement {
+    sid = "Allow runner to manage objects"
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${var.environment_name}-forms-runner-ecs-task"]
+    }
+    actions = [
+      "s3:GetObject",
+      "s3:GetObjectVersion",
+      "s3:ListBucket",
+      "s3:PutObject"
+    ]
+    resources = [
+      "arn:aws:s3:::govuk-forms-file-upload",
+      "arn:aws:s3:::govuk-forms-file-upload/*"
+    ]
+  }
+}
+
 
 # This configuration overrides the one in the secure-bucket module
 resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
