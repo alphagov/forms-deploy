1be42fb6a7912e55fd90cdb6628ad337a3dedcad
Andy Hunt <andy.hunt@digital.cabinet-office.gov.uk>
2025-01-16 10:25:10 +0000

    Add "integration" account to "all-accounts" module

--- infra/modules/all-accounts/accounts.tf
+++ infra/modules/all-accounts/accounts.tf
@@ -1,5 +1,6 @@
 locals {
-  deploy_account_id = "711966560482"
+  deploy_account_id      = "711966560482"
+  integration_account_id = "842676007477"
   environment_accounts = {
     "development"   = "498160065950",
     "staging"       = "972536609845",
@@ -12,10 +13,14 @@ output "deploy_account_id" {
   value = local.deploy_account_id
 }
 
+output "integration_account_id" {
+  value = local.integration_account_id
+}
+
 output "environment_accounts_id" {
   value = local.environment_accounts
 }
 
 output "all_accounts_id" {
-  value = merge(local.environment_accounts, { "deploy" : local.deploy_account_id })
+  value = merge(local.environment_accounts, { "deploy" : local.deploy_account_id, "integration" : local.integration_account_id })
 }
