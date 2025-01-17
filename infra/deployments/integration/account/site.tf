b60f88a5df8be2902ec75819e1980f8bb64d9445
Andy Hunt <andy.hunt@digital.cabinet-office.gov.uk>
2025-01-17 15:52:45 +0000

    Replace engineer-access module usage in integration/account
    The engineer-access module has become tailored for forms environments over
    time. Instead of spending time and effort somehow refactoring it, we should
    integration/account to be driven by the same user config module, but create its
    own roles and permissions, appropriate to the integration account.
    
    For now, the support roles have no permissions. We will add those as we
    discover what is useful for it to have.

--- infra/deployments/integration/account/site.tf
+++ infra/deployments/integration/account/site.tf
@@ -1,7 +1,6 @@
 terraform {
-  # Comment out when bootstrapping
   backend "s3" {
-    bucket = "gds-forms-integration-tfstate"
+    # bucket set in backend config
     key    = "account.tfstate"
     region = "eu-west-2"
   }
--- infra/deployments/integration/inputs.tf
+++ infra/deployments/integration/inputs.tf
@@ -17,6 +17,19 @@ variable "aws_account_id" {
     error_message = "AWS account IDs are exactly 12 digits long"
   }
 }
+
+variable "bucket" {
+  description = "Name of the state file bucket. This is named to match the key in the S3 type backend"
+  type        = string
+  nullable    = false
+}
+
+variable "dynamodb_table" {
+  description = "Name of the DynamoDB table used for state file locking. This is named to match the key in the S3 type backend"
+  type        = string
+  nullable    = false
+}
+
 variable "require_vpn_to_access" {
   type        = bool
   description = "Whether this AWS account will require users to be on the VPN to access it"
