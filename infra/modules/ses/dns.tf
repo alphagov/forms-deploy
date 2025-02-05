c0f493ed62eb42eed76635ee1e6aa5122a214167
Andy Hunt <andy.hunt@digital.cabinet-office.gov.uk>
2025-02-05 14:23:26 +0000

    Move DNS records for AWS SES and general email security to another file
    Minor refactor. Tidies up main.tf a little.

---
+++ infra/modules/ses/dns.tf
@@ -0,0 +1,24 @@
+resource "aws_route53_record" "ses" {
+  count   = 3
+  zone_id = var.hosted_zone_id
+  name    = "${aws_ses_domain_dkim.ses.dkim_tokens[count.index]}._domainkey"
+  type    = "CNAME"
+  ttl     = 600
+  records = ["${aws_ses_domain_dkim.ses.dkim_tokens[count.index]}.dkim.amazonses.com"]
+}
+
+resource "aws_route53_record" "ses_email_receiving" {
+  zone_id = var.hosted_zone_id
+  name    = aws_ses_domain_identity.ses.domain
+  type    = "MX"
+  records = ["10 inbound-smtp.eu-west-2.amazonaws.com"]
+  ttl     = 3600
+}
+
+resource "aws_route53_record" "ses_feedback" {
+  zone_id = var.hosted_zone_id
+  name    = "mail.${aws_ses_domain_identity.ses.domain}"
+  type    = "MX"
+  records = ["10 feedback-smtp.eu-west-2.amazonses.com"]
+  ttl     = 3600
+}
--- infra/modules/ses/main.tf
+++ infra/modules/ses/main.tf
@@ -54,31 +54,6 @@ resource "aws_ses_domain_dkim" "ses" {
   domain = aws_ses_domain_identity.ses.domain
 }
 
-resource "aws_route53_record" "ses" {
-  count   = 3
-  zone_id = var.hosted_zone_id
-  name    = "${aws_ses_domain_dkim.ses.dkim_tokens[count.index]}._domainkey"
-  type    = "CNAME"
-  ttl     = 600
-  records = ["${aws_ses_domain_dkim.ses.dkim_tokens[count.index]}.dkim.amazonses.com"]
-}
-
-resource "aws_route53_record" "ses_email_receiving" {
-  zone_id = var.hosted_zone_id
-  name    = aws_ses_domain_identity.ses.domain
-  type    = "MX"
-  records = ["10 inbound-smtp.eu-west-2.amazonaws.com"]
-  ttl     = 3600
-}
-
-resource "aws_route53_record" "ses_feedback" {
-  zone_id = var.hosted_zone_id
-  name    = "mail.${aws_ses_domain_identity.ses.domain}"
-  type    = "MX"
-  records = ["10 feedback-smtp.eu-west-2.amazonses.com"]
-  ttl     = 3600
-}
-
 resource "aws_ses_domain_mail_from" "mail" {
   domain           = aws_ses_domain_identity.ses.domain
   mail_from_domain = "mail.${aws_ses_domain_identity.ses.domain}"
