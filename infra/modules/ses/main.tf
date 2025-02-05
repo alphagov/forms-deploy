1fabaed4e39d3b804152b53df4c5a686bd8ed15c
Andy Hunt <andy.hunt@digital.cabinet-office.gov.uk>
2025-02-05 15:46:15 +0000

    Move email security configuration to its own file
    Another minor refactor aimed at making the main.tf file simpler

--- infra/modules/ses/main.tf
+++ infra/modules/ses/main.tf
@@ -15,16 +15,3 @@ resource "aws_ses_configuration_set" "bounces_and_complaints_handling_rule" {
 
   reputation_metrics_enabled = true
 }
-
-resource "aws_ses_domain_identity" "ses" {
-  domain = var.email_domain
-}
-
-resource "aws_ses_domain_dkim" "ses" {
-  domain = aws_ses_domain_identity.ses.domain
-}
-
-resource "aws_ses_domain_mail_from" "mail" {
-  domain           = aws_ses_domain_identity.ses.domain
-  mail_from_domain = "mail.${aws_ses_domain_identity.ses.domain}"
-}
