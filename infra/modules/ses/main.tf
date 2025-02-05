f12cfd17da842e09f29e1efa63cc06716dc6c04c
Andy Hunt <andy.hunt@digital.cabinet-office.gov.uk>
2025-02-05 16:07:04 +0000

    Set default configuration set for from address email identity
    Creates a new AWS SES v2 configuration set with identical configuration to the
    existing v1 set, used for Auth0.
    
    We name the new set auth0 and set it as the default configuration set to use.
    We do this because Auth0 sends OTP emails via SMTP, and it does not set the
    headers required to change which configuration set is being used.
    
    Other users of the email identity will need to set the configuration set when
    they make the API call.

--- infra/modules/ses/main.tf
+++ infra/modules/ses/main.tf
@@ -1,7 +1,43 @@
 resource "aws_sesv2_email_identity" "from_address" {
   email_identity = var.from_address
+
+  # Default configuration set name.
+  # It can be overridden when sending mail.
+  #
+  # Default is Auth0 set because Auth0 sends via
+  # SMTP and does not set the required headers.
+  configuration_set_name = aws_sesv2_configuration_set.auth0.configuration_set_name
 }
 
+##
+# SES v2 configuration
+##
+resource "aws_sesv2_configuration_set" "auth0" {
+  configuration_set_name = "${var.environment_name}_auth0"
+
+  reputation_options {
+    reputation_metrics_enabled = true
+  }
+}
+
+resource "aws_sesv2_configuration_set_event_destination" "auth0" {
+  configuration_set_name = aws_sesv2_configuration_set.auth0.configuration_set_name
+  event_destination_name = "auth0_bounces_and_complaints"
+
+  event_destination {
+    enabled              = true
+    matching_event_types = ["BOUNCE", "COMPLAINT", "REJECT"]
+
+    sns_destination {
+      topic_arn = aws_sns_topic.ses_bounces_and_complaints.arn
+    }
+  }
+}
+
+
+##
+# SES v1 configuration
+##
 resource "aws_ses_event_destination" "failed_email_notification" {
   name                   = "failed_email_notification"
   configuration_set_name = aws_ses_configuration_set.bounces_and_complaints_handling_rule.name
