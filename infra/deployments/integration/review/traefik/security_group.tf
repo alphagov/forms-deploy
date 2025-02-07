0b08f828fe9f3acd1dd6cb7b647de4dcc3061d5c
Andy Hunt <andy.hunt@digital.cabinet-office.gov.uk>
2025-02-07 15:04:19 +0000

    Allow Traefik to egress the full TCP port range within the VPC
    Traefik doesn't know ahead of time what ports services within the cluster will
    be listening on. It should be able to send TCP traffic to all ports within the
    VPC cidr range.

--- infra/deployments/integration/review/traefik/security_group.tf
+++ infra/deployments/integration/review/traefik/security_group.tf
@@ -3,44 +3,36 @@ resource "aws_security_group" "traefik" {
   name        = "review-traefik"
   description = "Inbound and outbound traffic to Traefik"
   vpc_id      = var.vpc_id
-}
 
-resource "aws_security_group_rule" "ingress_from_vpc" {
-  description       = "Permit inbound form the VPC to the container port"
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  cidr_blocks       = [var.cidr_block]
-  security_group_id = aws_security_group.traefik.id
-}
+  ingress {
+    description = "Permit inbound form the VPC to the container port"
+    from_port   = local.http_port
+    to_port     = local.http_port
+    protocol    = "tcp"
+    cidr_blocks = [var.cidr_block]
+  }
 
-resource "aws_security_group_rule" "egress_to_vpc" {
-  description       = "Permit outbound from the container to the VPC"
-  type              = "egress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  cidr_blocks       = [var.cidr_block]
-  security_group_id = aws_security_group.traefik.id
-}
+  ingress {
+    description = "Permit inbound from the ALB to Traefik healthcheck endpoint"
+    from_port   = local.ping_port
+    to_port     = local.ping_port
+    protocol    = "tcp"
+    cidr_blocks = [var.cidr_block]
+  }
 
-resource "aws_security_group_rule" "egress_to_internet_via_tls" {
-  description       = "Permit outbound from the container to the internet on port 443 (for retrieving Docker containers)"
-  type              = "egress"
-  from_port         = 0
-  to_port           = 443
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.traefik.id
-}
+  egress {
+    description = "Permit TCP outbound from the container to the VPC"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = [var.cidr_block]
+  }
 
-resource "aws_security_group_rule" "ingress_to_traefik_healthcheck" {
-  description       = "Permit inbound from the ALB to Traefik healthcheck endpoint"
-  type              = "ingress"
-  from_port         = 0
-  to_port           = 8080
-  protocol          = "tcp"
-  cidr_blocks       = [var.cidr_block]
-  security_group_id = aws_security_group.traefik.id
+  egress {
+    description = "Permit outbound from the container to the internet on port 443 (for retrieving Docker containers)"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
