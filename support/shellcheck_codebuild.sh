f5dc6280f4fc01f5110c27f5efa492f8f0c6a4bc
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2025-02-10 15:06:53 +0200

    Add YAML files in buildspecs paths to list of files to be shellchecked
    Previously some CodeBuild buildspecs were not being included in the
    Infra as Code checks in our GitHub Actions, because the action assumed
    that buildspecs would be named `buildspec.yml`; however they can also be
    YAML files with any name in a `buildspecs/**` directory.
    
    This commit tweaks the script to find and lint buildspecs to include
    these files.

--- support/shellcheck_codebuild.sh
+++ support/shellcheck_codebuild.sh
@@ -12,4 +12,4 @@ do
     yq '.phases|to_entries|.[]|select(.value|has("commands"))|.value.commands|.[]' "${buildspec}" > "${file}"
     shellcheck -s bash "${file}"
   popd >/dev/null || exit
-done <  <(find "${search_dir}" -type f \( -name "buildspec.yml" -or -name "buildspec.yaml" \) -print0)
+done <  <(find "${search_dir}" -type f \( -name "buildspec.yml" -or -name "buildspec.yaml" -or -path "*/buildspecs/*.yml" -or -path "*/buildspecs/*.yaml" \) -print0)
