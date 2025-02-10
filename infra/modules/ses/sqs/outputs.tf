f4e011d42133dc2ae7b30b80c57024e495131c3e
alice-carr <alice.carr@digital.cabinet-office.gov.uk>
2025-02-10 16:44:48 +0000

    refactor SES module to allow multiple sqs queues
    For file upload we will need an additional sqs queue and sns topic for bounced emails. This commit refactors the existing ses module to add a new sub module for sqs that we can invoke as many times as we need.

---
+++ infra/modules/ses/sqs/outputs.tf
@@ -0,0 +1,3 @@
+output "aws_sns_topic" {
+  value = aws_sns_topic.ses_bounces_and_complaints.arn
+}
---
+++ infra/modules/ses/sqs/variables.tf
@@ -0,0 +1,9 @@
+variable "environment_type" {
+  type        = string
+  description = "The type of environment to be used."
+}
+
+variable "account_id" {
+  type        = string
+  description = "The current account id."
+}
