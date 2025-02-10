f35a6b89a636d0dfb42362b9c8346866b7585338
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2025-02-10 16:23:15 +0200

    Change shellcheck_codebuild.sh to show all errors before exiting

--- support/shellcheck_codebuild.sh
+++ support/shellcheck_codebuild.sh
@@ -3,6 +3,7 @@
 set -euo pipefail
 
 search_dir="$(pwd)"
+result=0
 
 while IFS= read -r -d '' buildspec
 do
@@ -18,7 +19,11 @@ do
     then
       echo "OK ${buildspec#"${search_dir}/"}"
       # errors are written to stdout and are obvious
+    else
+      result=1
     fi
 
   popd >/dev/null || exit
 done <  <(find "${search_dir}" -type f \( -name "buildspec.yml" -or -name "buildspec.yaml" -or -path "*/buildspecs/*.yml" -or -path "*/buildspecs/*.yaml" \) -print0)
+
+exit $result
