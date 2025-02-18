7056aa6e3692def2830b5ef3334d1a897f08669a
Sarah Young <sarah.young1@digital.cabinet-office.gov.uk>
2025-02-18 10:25:53 +0000

    Ensure correct image architecture
    * Use aarch64 instead of x86 since that's what we use in production

--- infra/deployments/integration/review/github_actions_runner.tf
+++ infra/deployments/integration/review/github_actions_runner.tf
@@ -212,8 +212,8 @@ resource "aws_codebuild_project" "forms_admin_github_actions_runner" {
 
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:5.0"
-    type                        = "LINUX_CONTAINER"
+    image                       = "aws/codebuild/amazonlinux2-aarch64-standard:3.0"
+    type                        = "ARM_CONTAINER"
     image_pull_credentials_type = "CODEBUILD"
 
     environment_variable {
