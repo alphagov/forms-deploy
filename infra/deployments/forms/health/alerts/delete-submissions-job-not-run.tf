980b3540f4c7ec774efdc71cb241addd61812610
Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>
2025-03-17 13:02:36 +0000

    Add alerts for the jobs related to sending submissions via SES
    Add alerts for:
    - If submissions are taking a long time to be sent. Currently this is
      set to alert if the average time to send in the past minute is
      greater than a minute. Under normal circumstances, we should expect
      submissions to be sent within a few seconds.
    - If there are 10 failures sending submissions within 15 minutes.
    Individual failures will either be retried or logged to Sentry, but we
    want to know if there are a large number of failures.
    - If the job to delete submissions has not run within the expected
    timeframe. We will add a similar alert for other recurring jobs we
    will add.

---
+++ infra/deployments/forms/health/alerts/delete-submissions-job-not-run.tf
@@ -0,0 +1,31 @@
+resource "aws_cloudwatch_metric_alarm" "delete_submissions_job_not_run" {
+  alarm_name          = "${var.environment}-delete-submissions-job-not-run"
+  alarm_description   = <<EOF
+    The forms-runner job to delete submissions has not in the ${var.environment} environment in the past 2 hours. It is
+    expected that the job is run every hour. This job is run by Solid Queue, which is started in the forms-runner ECS
+    task.
+
+    NEXT STEPS:
+    1. Check the Splunk logs and Sentry for any errors running the job.
+    2. Restart the forms-runner ECS tasks and check whether the job starts running.
+
+EOF
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = 1
+  metric_name         = "Started"
+  namespace           = "Forms/Jobs"
+  period              = 7200
+  statistic           = "Average"
+  threshold           = 0
+
+  dimensions = {
+    Environment = "${var.environment}"
+    ServiceName = "forms-runner"
+    JobName     = "DeleteSubmissionsJob"
+  }
+
+  treat_missing_data = "breaching"
+
+  alarm_actions = [local.alert_severity.eu_west_2.info]
+  ok_actions    = [local.alert_severity.eu_west_2.info]
+}
---
+++ infra/deployments/forms/health/alerts/send-submission-job-failures.tf
@@ -0,0 +1,36 @@
+resource "aws_cloudwatch_metric_alarm" "send_submission_job_failures" {
+  alarm_name          = "${var.environment}-send-submission-job-failures"
+  alarm_description   = <<EOF
+    The forms-runner job to send submissions has failed more than 10 times in the last 15 minutes in the
+    ${var.environment} environment. This job is run by Solid Queue, which is started in the forms-runner ECS task.
+
+    NEXT STEPS:
+    1. Check whether there are any forms-runner errors in Sentry related to the SendSubmissionJob. We retry to job
+    automatically if there are errors calling AWS, but for all other errors we immediately send the error to Sentry and
+    the job is not scheduled for retry.
+
+    If there are no errors in Sentry, check the Splunk logs for the exception messages if the job has been retried.
+    Search for "Retrying SendSubmissionJob" to find these log lines.
+
+    2. If the failed jobs have not been scheduled to be retried, you will need to manually trigger resending the
+    submission.
+EOF
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 1
+  metric_name         = "Failure"
+  namespace           = "Forms/Jobs"
+  period              = 900
+  statistic           = "Sum"
+  threshold           = 10
+
+  dimensions = {
+    Environment = "${var.environment}"
+    ServiceName = "forms-runner"
+    JobName     = "SendSubmissionJob"
+  }
+
+  treat_missing_data = "notBreaching"
+
+  alarm_actions = [local.alert_severity.eu_west_2.warn]
+  ok_actions    = [local.alert_severity.eu_west_2.warn]
+}
