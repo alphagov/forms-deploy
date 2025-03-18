981e846f09cce4804852bfc5ad42d8d26e5cfadc
Andy Hunt <andy.hunt@digital.cabinet-office.gov.uk>
2025-03-18 11:22:21 +0000

    Move deployer-access variables to their own file
    roles.tf was getting a bit confused

--- infra/modules/deployer-access/roles.tf
+++ infra/modules/deployer-access/roles.tf
@@ -1,29 +1,3 @@
-variable "env_name" {
-  type        = string
-  description = "The name of the environment to be used in resource names."
-}
-
-variable "environment_type" {
-  type        = string
-  description = "The type of environment the deployer-access role is being used in"
-}
-
-variable "hosted_zone_id" {
-  description = "The ID of the AWS hosted zone in the account, to which DNS records will be added"
-  type        = string
-  nullable    = false
-}
-
-variable "codestar_connection_arn" {
-  type        = string
-  description = "ARN of the CodeStar connection in the account"
-}
-
-variable "dynamodb_state_file_locks_table_arn" {
-  type        = string
-  description = "The arn of the DynamoDB table being used for state file locking"
-}
-
 locals {
   deploy_account_id = "711966560482"
 
