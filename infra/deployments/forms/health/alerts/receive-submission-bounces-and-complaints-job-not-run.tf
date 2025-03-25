7ea10db486ef15dada39f9be47d121e4c5b0908f
Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>
2025-03-25 16:54:51 +0000

    Add alerts for forms-runner jobs not running
    Add alerts that will fire if the ReceiveSubmissionDeliveriesJob or
    ReceiveSubmissionBouncesAndComplaintsJob forms-runner jobs haven't
    run in the past half an hour. This will alert at info level, so send
    an email to Zendesk.

---
+++ infra/deployments/forms/health/alerts/receive-submission-bounces-and-complaints-job-not-run.tf
@@ -0,0 +1,31 @@
+resource "aws_cloudwatch_metric_alarm" "receive_submission_bounces_and_complaints_job_not_run" {
+  alarm_name          = "${var.environment}-receive-submission-bounces-and-complaints-job-not-run"
+  alarm_description   = <<EOF
+    The forms-runner job to receive SQS notifications about bounces and complaints has not in the ${var.environment} environment in
+    the past 30 minutes. It is expected that the job is run every 10 minutes. This job is run by Solid Queue, which is
+    started in the forms-runner ECS task.
+
+    NEXT STEPS:
+    1. Check the Splunk logs and Sentry for any errors running the job.
+    2. Restart the forms-runner ECS tasks and check whether the job starts running.
+
+EOF
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = 1
+  metric_name         = "Started"
+  namespace           = "Forms/Jobs"
+  period              = 30 * 60
+  statistic           = "Average"
+  threshold           = 0
+
+  dimensions = {
+    Environment = "${var.environment}"
+    ServiceName = "forms-runner"
+    JobName     = "ReceiveSubmissionBouncesAndComplaintsJob"
+  }
+
+  treat_missing_data = "breaching"
+
+  alarm_actions = [local.alert_severity.eu_west_2.info]
+  ok_actions    = [local.alert_severity.eu_west_2.info]
+}
