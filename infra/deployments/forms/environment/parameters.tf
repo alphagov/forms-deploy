62db51db5cca7e53522fc6d881c43886ebd7c447
Andy Hunt <andy.hunt@digital.cabinet-office.gov.uk>
2025-03-26 16:43:42 +0000

    Generate a new shared secret between forms-runner and end-to-end tests
    Used to secure the endpoint which reports whether an email was sent for use by
    the end-to-end tests. We don't want that endpoint to be completely
    unauthenticated and unauthorized, out of an abundance of caution.

--- infra/deployments/forms/environment/parameters.tf
+++ infra/deployments/forms/environment/parameters.tf
@@ -30,3 +30,24 @@ resource "aws_ssm_parameter" "auth0_access_client_secret" {
     ]
   }
 }
+
+# Shared secret between end-to-end tests and runner
+# Used to secure the endpoint which reports whether an email
+# was sent for use by the end-to-end tests. We don't want that
+# endpoint to be completely unauthenticated and unauthorized, out of an abundance
+# of caution
+resource "random_password" "submission_status_api_shared_secret" {
+  length = 20
+}
+
+resource "aws_ssm_parameter" "submission_status_api_shared_secret" {
+  #checkov:skip=CKV_AWS_337:It's fine to use the managed key
+  name        = "/forms-runner-${var.environment_name}/submission_status_api_shared_secret"
+  type        = "SecureString"
+  description = "Secret value shared by forms-runner and end-to-end tests to authorize the attachment sent endpoint"
+  value       = random_password.submission_status_api_shared_secret.result
+
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
