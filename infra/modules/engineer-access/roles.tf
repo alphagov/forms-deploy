10e8f8646e93f0d861d9aab30e19248b71afc2e2
Catalina Garcia <catalina.garcia@digital.cabinet-office.gov.uk>
2025-04-07 15:47:40 +0100

    Allow support role to release the lock on state files
    We need the support role to be able to `terraform apply`, therefore it needs to
    be able to get/release the lock on state files.
    
    The role also needs to be able to put objects in and get objects from the state
    file buckets, but we already gave it those permissions as part of allowing it to
    manage_maintenance_page.

--- infra/modules/engineer-access/roles.tf
+++ infra/modules/engineer-access/roles.tf
@@ -29,6 +29,7 @@ module "support_role" {
     aws_iam_policy.manage_deployments.arn,
     aws_iam_policy.manage_maintenance_page.arn,
     aws_iam_policy.lock_state_files.arn,
+    aws_iam_policy.release_lock_on_state_files.arn,
     var.allow_rds_data_api_access ? [aws_iam_policy.query_rds_with_data_api[0].arn] : [],
     var.allow_ecs_task_usage ? [aws_iam_policy.run_task[0].arn, aws_iam_policy.stop_task[0].arn] : []
   ])
