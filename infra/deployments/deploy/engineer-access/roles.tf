477c7e0a4d0936e4de5bcb04e8ea66dc0bd23434
Catalina Garcia <catalina.garcia@digital.cabinet-office.gov.uk>
2025-04-11 14:27:28 +0100

    Remove unused policies and variables for state locking
    Since we are using terraform's native s3 state locking
    (https://github.com/alphagov/forms-deploy/pull/1476) we no longer need have
    dynamodb tables or the permissions associated with it

--- infra/deployments/deploy/engineer-access/roles.tf
+++ infra/deployments/deploy/engineer-access/roles.tf
@@ -3,16 +3,15 @@ module "users" {
 }
 
 module "engineer_access" {
-  source                              = "../../../modules/engineer-access"
-  admins                              = module.users.with_role["deploy_admin"]
-  support                             = module.users.with_role["deploy_support"]
-  readonly                            = module.users.with_role["deploy_readonly"]
-  env_name                            = "deploy"
-  environment_type                    = "deploy"
-  codestar_connection_arn             = var.codestar_connection_arn
-  dynamodb_state_file_locks_table_arn = "arn:aws:dynamodb::${var.deploy_account_id}:table/*"
-  allow_ecs_task_usage                = false
-  allow_rds_data_api_access           = false
+  source                    = "../../../modules/engineer-access"
+  admins                    = module.users.with_role["deploy_admin"]
+  support                   = module.users.with_role["deploy_support"]
+  readonly                  = module.users.with_role["deploy_readonly"]
+  env_name                  = "deploy"
+  environment_type          = "deploy"
+  codestar_connection_arn   = var.codestar_connection_arn
+  allow_ecs_task_usage      = false
+  allow_rds_data_api_access = false
 
   # Pentesters may not have GDS domains so our pattern using the 'users' module
   # doesn't necessarily work.
--- infra/deployments/forms/account/deployer-access.tf
+++ infra/deployments/forms/account/deployer-access.tf
@@ -1,21 +1,11 @@
-locals {
-  # The user research account uses an underscore for environment type
-  # and a dash for environment name. This is a bug fix to get the user-research
-  # account Terraform working again while we determine what impact changing
-  # the values will have.
-  dynamodb_table_name = (var.environment_type == "user_research" ?
-    "govuk-forms-user-research-tfstate-locking" :
-  "govuk-forms-${var.environment_type}-tfstate-locking")
-}
 module "deployer_access" {
-  source                              = "../../../modules/deployer-access"
-  environment_name                    = var.environment_name
-  environment_type                    = replace(var.environment_type, "_", "-") # See comment about dynamodb table name
-  account_id                          = var.aws_account_id
-  deploy_account_id                   = var.deploy_account_id
-  hosted_zone_id                      = aws_route53_zone.public.id
-  codestar_connection_arn             = var.codestar_connection_arn
-  dynamodb_state_file_locks_table_arn = "arn:aws:dynamodb:eu-west-2:${var.aws_account_id}:table/*"
+  source                  = "../../../modules/deployer-access"
+  environment_name        = var.environment_name
+  environment_type        = replace(var.environment_type, "_", "-") # The user research account uses an underscore for environment type and a dash for environment name
+  account_id              = var.aws_account_id
+  deploy_account_id       = var.deploy_account_id
+  hosted_zone_id          = aws_route53_zone.public.id
+  codestar_connection_arn = var.codestar_connection_arn
 
   depends_on = [aws_route53_zone.public]
 }
