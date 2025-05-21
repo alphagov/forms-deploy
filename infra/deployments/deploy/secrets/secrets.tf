e5cc20b3a07aaf57d01d8a06ab49e0884b9f9bec
Catalina Garcia <catalina.garcia@digital.cabinet-office.gov.uk>
2025-05-21 16:35:44 +0100

    Keep KMS keys in a separate file

--- infra/deployments/deploy/secrets/secrets.tf
+++ infra/deployments/deploy/secrets/secrets.tf
@@ -1,67 +1,6 @@
-locals {
-  environment_type = toset(["development", "staging", "production", "user_research", "deploy", "integration", "ithc"])
-}
-
-module "users" {
-  source = "../../../modules/users"
-}
-
-data "aws_caller_identity" "current" {}
-
 resource "aws_secretsmanager_secret" "mailchimp_api_key" {
   for_each = local.environment_type
 
   name       = "external/${each.key}/mailchimp/api-key"
-  kms_key_id = aws_kms_key.this[each.key].id
-}
-
-resource "aws_kms_key" "this" {
-  for_each = local.environment_type
-
-  description         = "Symmetric encryption KMS key for external secrets in ${each.key} environments"
-  enable_key_rotation = true
-}
-
-resource "aws_kms_key_policy" "secretsmanager" {
-  for_each = local.environment_type
-
-  key_id = aws_kms_key.this[each.key].id
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Id      = "key-default-1"
-    Statement = [
-      {
-        Sid    = "Enable IAM Access"
-        Effect = "Allow"
-        Principal = {
-          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
-        },
-        Action   = "kms:*"
-        Resource = "*"
-      },
-      {
-        Sid    = "Allow administration of the key by admins of this account"
-        Effect = "Allow"
-        Principals = {
-          type        = "AWS"
-          identifiers = [for admin in module.users.with_role["deploy_admin"] : "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${admin}-admin"]
-        },
-        Action = [
-          "kms:Create*",
-          "kms:Describe*",
-          "kms:Enable*",
-          "kms:List*",
-          "kms:Put*",
-          "kms:Update*",
-          "kms:Revoke*",
-          "kms:Disable*",
-          "kms:Get*",
-          "kms:Delete*",
-          "kms:ScheduleKeyDeletion",
-          "kms:CancelKeyDeletion"
-        ],
-        Resource = "*"
-      },
-    ]
-  })
+  kms_key_id = aws_kms_key.external_env_type[each.key].id
 }
