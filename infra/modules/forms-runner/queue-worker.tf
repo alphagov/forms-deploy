356052e1aaa1061e55f79962031cc283d91de0a2
Sarah Young <sarah.young1@digital.cabinet-office.gov.uk>
2025-06-02 11:47:32 +0100

    Remove SSM Parameter
    * Remove SSM Parameter and raise this in a separate PR.
    * This is to ensure the secret exists before the ECS tasks are created in all environments so there are no failures on creation.
    * If I leave the SSM Parameter in this PR the dummy value will exist when the ECS task gets created and there will be failures.

--- infra/modules/forms-runner/queue-worker.tf
+++ infra/modules/forms-runner/queue-worker.tf
@@ -121,20 +121,6 @@ resource "aws_security_group" "queue_worker" {
   }
 }
 
-resource "aws_ssm_parameter" "queue_worker_sentry_dsn" {
-  #checkov:skip=CKV_AWS_337:The parameter is already using the default key
-  name  = "/${local.queue_worker_name}-${var.env_name}/sentry/dsn"
-  type  = "SecureString"
-  value = "dummy_value"
-
-  description = "Sentry DSN value for ${local.queue_worker_name} in the ${var.env_name} environment"
-
-  lifecycle {
-    ignore_changes  = [value]
-    prevent_destroy = true
-  }
-}
-
 resource "aws_iam_role" "ecs_task_role" {
   name               = "${var.env_name}-${local.queue_worker_name}-ecs-task"
   description        = "Used by ${local.queue_worker_name} tasks when running"
