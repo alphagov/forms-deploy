7685bf2c51da03f20a7d4fe75c3f27fab2b7879a
Sarah Young <sarah.young1@digital.cabinet-office.gov.uk>
2025-06-04 15:44:42 +0100

    Fix rebase error
    * Add SSM parameter created from PR #1591
    * I rebased PR #1591 on to this branch in the GitHub UI (not a good idea), and it absorbed the queue_worker.tf file from PR #1591 as a separate file instead of merging the files together.
    * This happened because I didn't take into account the lack of shared file history from git's perspective.
    * The queue_worker.tf on the `create-solidqueue-worker-ecs-task` branch was never branched off from the queue_worker.tf in main (i.e., it was created independently rather than modified from the original). Therefore, Git doesn’t know they’re related. It treats them as two different files that just happen to have the same name.

--- infra/modules/forms-runner/queue_worker.tf
+++ infra/modules/forms-runner/queue_worker.tf
@@ -176,3 +176,17 @@ data "aws_iam_policy_document" "queue_worker_ecs_task_exec_additional_policy" {
     effect = "Allow"
   }
 }
+
+resource "aws_ssm_parameter" "queue_worker_sentry_dsn" {
+  #checkov:skip=CKV_AWS_337:The parameter is already using the default key
+  name  = "/${local.queue_worker_name}-${var.env_name}/sentry/dsn"
+  type  = "SecureString"
+  value = "dummy_value"
+
+  description = "Sentry DSN value for ${local.queue_worker_name} in the ${var.env_name} environment"
+
+  lifecycle {
+    ignore_changes  = [value]
+    prevent_destroy = true
+  }
+}
