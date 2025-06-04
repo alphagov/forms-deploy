947b8e62f91a44ba1913761883007ff6cb471deb
Catalina Garcia <catalina.garcia@digital.cabinet-office.gov.uk>
2025-06-04 10:37:25 +0100

    Refactor kms/policy creation
    We want to be able to re-use this policy for other kms keys

--- infra/deployments/deploy/secrets/kms_keys.tf
+++ infra/deployments/deploy/secrets/kms_keys.tf
@@ -9,48 +9,42 @@ resource "aws_kms_key" "external_env_type" {
 
   description         = "Symmetric encryption KMS key for external secrets in ${each.key} environments"
   enable_key_rotation = true
+  policy              = data.aws_iam_policy_document.kms_management.json
 }
 
-resource "aws_kms_key_policy" "secretsmanager_external_env_type" {
-  for_each = toset(local.environment_types)
+data "aws_iam_policy_document" "kms_management" {
+  statement {
+    sid    = "Enable IAM Access"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+    actions   = ["kms:*"]
+    resources = ["*"]
+  }
 
-  key_id = aws_kms_key.external_env_type[each.key].id
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Id      = "key-default-1"
-    Statement = [
-      {
-        Sid    = "Enable IAM Access"
-        Effect = "Allow"
-        Principal = {
-          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
-        },
-        Action   = "kms:*"
-        Resource = "*"
-      },
-      {
-        Sid    = "Allow administration of the key by admins of this account"
-        Effect = "Allow"
-        Principals = {
-          type        = "AWS"
-          identifiers = [for admin in module.users.with_role["deploy_admin"] : "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${admin}-admin"]
-        },
-        Action = [
-          "kms:Create*",
-          "kms:Describe*",
-          "kms:Enable*",
-          "kms:List*",
-          "kms:Put*",
-          "kms:Update*",
-          "kms:Revoke*",
-          "kms:Disable*",
-          "kms:Get*",
-          "kms:Delete*",
-          "kms:ScheduleKeyDeletion",
-          "kms:CancelKeyDeletion"
-        ],
-        Resource = "*"
-      },
+  statement {
+    sid    = "Allow administration of the key by admins of this account"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = [for admin in module.users.with_role["deploy_admin"] : "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${admin}-admin"]
+    }
+    actions = [
+      "kms:Create*",
+      "kms:Describe*",
+      "kms:Enable*",
+      "kms:List*",
+      "kms:Put*",
+      "kms:Update*",
+      "kms:Revoke*",
+      "kms:Disable*",
+      "kms:Get*",
+      "kms:Delete*",
+      "kms:ScheduleKeyDeletion",
+      "kms:CancelKeyDeletion"
     ]
-  })
+    resources = ["*"]
+  }
 }
