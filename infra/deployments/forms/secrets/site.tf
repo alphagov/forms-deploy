26422a30043645e982bbfa796dd6ac18de02982c
Catalina Garcia <catalina.garcia@digital.cabinet-office.gov.uk>
2025-06-09 10:14:52 +0100

    Add root structure for secrets

---
+++ infra/deployments/forms/secrets/site.tf
@@ -0,0 +1,22 @@
+terraform {
+  backend "s3" {
+    # bucket set in backend config file
+    key    = "secrets.tfstate"
+    region = "eu-west-2"
+
+    use_lockfile = true
+  }
+}
+
+provider "aws" {
+  allowed_account_ids = var.allowed_account_ids
+
+  default_tags {
+    tags = merge(var.default_tags,
+      {
+        Deployment = "${var.environment_name}/secrets"
+      }
+    )
+  }
+}
+
