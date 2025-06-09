c35bede311e2299f1448a6f91ecd063315786d71
Catalina Garcia <catalina.garcia@digital.cabinet-office.gov.uk>
2025-06-09 10:31:32 +0100

    Add internal secrets to Secrets Manager in forms root
    These secrets are for use with/by internal system components and live under
    "internal/ENV_NAME".
    
    We add a kms key for each account.

---
+++ infra/deployments/forms/secrets/kms_keys.tf
@@ -0,0 +1,54 @@
+module "users" {
+  source = "../../../modules/users"
+}
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_kms_key" "internal" {
+  description         = "Symmetric encryption KMS key for internal secrets"
+  enable_key_rotation = true
+  policy              = data.aws_iam_policy_document.kms_management.json
+}
+
+
+data "aws_iam_policy_document" "kms_management" {
+  # See https://docs.aws.amazon.com/kms/latest/developerguide/key-policy-default.html#key-policy-default-allow-root-enable-iam
+  #checkov:skip=CKV_AWS_111:AWS suggest the EnableIamAccess statement for key policies.
+  #checkov:skip=CKV_AWS_109:AWS suggest the EnableIamAccess statement for key policies.
+  #checkov:skip=CKV_AWS_356:Resource "*" is OK here because the only resource it can refer to is the key to which the policy is attached
+
+  statement {
+    sid    = "Enable IAM Access"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+    actions   = ["kms:*"]
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "Allow administration of the key by admins of this account"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = [for admin in module.users.with_role["deploy_admin"] : "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${admin}-admin"]
+    }
+    actions = [
+      "kms:Create*",
+      "kms:Describe*",
+      "kms:Enable*",
+      "kms:List*",
+      "kms:Put*",
+      "kms:Update*",
+      "kms:Revoke*",
+      "kms:Disable*",
+      "kms:Get*",
+      "kms:Delete*",
+      "kms:ScheduleKeyDeletion",
+      "kms:CancelKeyDeletion"
+    ]
+    resources = ["*"]
+  }
+}
---
+++ infra/deployments/forms/secrets/secrets.auto.tfvars
@@ -0,0 +1,7 @@
+internal_secrets = {
+  # Secrets for use with/by internal components
+  databases_forms_admin_url = {
+    name        = "databases/forms-admin/url"
+    description = ""
+  }
+}
