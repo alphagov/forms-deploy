0953f0c120afcf2c3cc6307272f1f33cd3b49924
Tom Iles <tom.iles@digital.cabinet-office.gov.uk>
2025-06-26 14:01:30 +0100

    Rename error policies
    The error page uses two managed polices to handle CORS and caching.
    
    These must be defined as data sources, and referenced in the ordered cache
    behaviour.
    
    This commit renames the policies to make the code clearer.

--- infra/modules/cloudfront/cloudfront.tf
+++ infra/modules/cloudfront/cloudfront.tf
@@ -15,11 +15,11 @@ data "aws_cloudfront_response_headers_policy" "cors" {
   name = "Managed-SimpleCORS"
 }
 
-data "aws_cloudfront_cache_policy" "caching_policy" {
+data "aws_cloudfront_cache_policy" "caching_optimized" {
   name = "Managed-CachingOptimized"
 }
 
-data "aws_cloudfront_origin_request_policy" "origin_request_policy" {
+data "aws_cloudfront_origin_request_policy" "cors_s3_origin" {
   name = "Managed-CORS-S3Origin"
 }
 
@@ -119,8 +119,8 @@ resource "aws_cloudfront_distribution" "main" {
     cached_methods           = ["GET", "HEAD"]
     target_origin_id         = "error_page"
     viewer_protocol_policy   = "allow-all"
-    cache_policy_id          = data.aws_cloudfront_cache_policy.caching_policy.id
-    origin_request_policy_id = data.aws_cloudfront_origin_request_policy.origin_request_policy.id
+    cache_policy_id          = data.aws_cloudfront_cache_policy.caching_optimized.id
+    origin_request_policy_id = data.aws_cloudfront_origin_request_policy.cors_s3_origin.id
   }
 }
 
