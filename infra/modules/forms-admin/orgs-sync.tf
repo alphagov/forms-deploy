bc56c02c8c8dfea35d0e5a2a7456033bb0dd1158
chao-xian <kelvin.gan@digital.cabinet-office.gov.uk>
2025-07-31 15:34:41 +0100

    Add cronjob to run org list sync

--- infra/modules/forms-admin/orgs-sync.tf
+++ infra/modules/forms-admin/orgs-sync.tf
@@ -4,28 +4,28 @@
 locals {
   # Take the exported task container definition
   # and override some parts of it, so that it doesn't fall out of sync
-  mailchimp_sync_container_definitions = merge(
+  organisations_sync_container_definitions = merge(
     module.ecs_service.task_container_definition,
     {
-      name    = "forms-admin_mailchimp_sync",
-      command = ["rake", "mailchimp:synchronize_audiences"]
+      name    = "forms-admin_organisations_sync",
+      command = ["rake", "organisations:fetch"]
       logConfiguration = {
         logDriver = "awslogs",
         options = {
           awslogs-group         = module.ecs_service.application_log_group_name,
           awslogs-region        = "eu-west-2",
-          awslogs-stream-prefix = "forms-admin-${var.env_name}-mailchimp-sync"
+          awslogs-stream-prefix = "forms-admin-${var.env_name}-organisations-sync"
         }
       },
     }
   )
 }
 
-resource "aws_ecs_task_definition" "mailchimp_cron_job" {
-  count = var.enable_mailchimp_sync ? 1 : 0
+resource "aws_ecs_task_definition" "orgs_cron_job" {
+  count = var.enable_organisations_sync ? 1 : 0
 
-  family                = "${var.env_name}_forms-admin_mailchimp_sync"
-  container_definitions = jsonencode([local.mailchimp_sync_container_definitions])
+  family                = "${var.env_name}_forms-admin_organisations_sync"
+  container_definitions = jsonencode([local.organisations_sync_container_definitions])
 
   execution_role_arn       = module.ecs_service.task_definition.execution_role_arn
   task_role_arn            = module.ecs_service.task_definition.task_role_arn
@@ -43,23 +43,23 @@ resource "aws_ecs_task_definition" "mailchimp_cron_job" {
 ##
 # EventBridge
 ##
-resource "aws_cloudwatch_event_rule" "sync_mailchimp_cron_job" {
-  count = var.enable_mailchimp_sync ? 1 : 0
+resource "aws_cloudwatch_event_rule" "sync_orgs_cron_job" {
+  count = var.enable_organisations_sync ? 1 : 0
 
   name                = "${var.env_name}-forms-admin-sync-cron"
-  description         = "Trigger the forms-admin MailChimp synchronisation on a schedule"
-  schedule_expression = "cron(30 10 * * ? *)" # 10:30AM daily. In office hours so that we can respond to failures
+  description         = "Trigger the forms-admin organisations synchronisation on a schedule"
+  schedule_expression = "cron(30 11 * * 2 *)" # 11:30AM every Tuesday. In office hours so that we can respond to failures
 }
 
-resource "aws_cloudwatch_event_target" "ecs_mailchimp_sync_job" {
-  count = var.enable_mailchimp_sync ? 1 : 0
+resource "aws_cloudwatch_event_target" "ecs_org_sync_job" {
+  count = var.enable_organisations_sync ? 1 : 0
 
   arn      = var.ecs_cluster_arn
-  rule     = aws_cloudwatch_event_rule.sync_mailchimp_cron_job[0].name
-  role_arn = aws_iam_role.ecs_mailchimp_cron_scheduler[0].arn
+  rule     = aws_cloudwatch_event_rule.sync_orgs_cron_job[0].name
+  role_arn = aws_iam_role.ecs_orgs_cron_scheduler[0].arn
 
   ecs_target {
-    task_definition_arn = aws_ecs_task_definition.mailchimp_cron_job[0].arn
+    task_definition_arn = aws_ecs_task_definition.orgs_cron_job[0].arn
     launch_type         = "FARGATE"
     platform_version    = "1.4.0"
 
@@ -76,9 +76,9 @@ resource "aws_cloudwatch_event_target" "ecs_mailchimp_sync_job" {
 }
 
 ## Monitor for failure
-resource "aws_cloudwatch_event_rule" "sync_mailchimp_cron_job_failed" {
+resource "aws_cloudwatch_event_rule" "sync_orgs_cron_job_failed" {
   name        = "${var.env_name}-forms-admin-sync-failed"
-  description = "Trigger when the MailChimp sync job has exited with a non-zero exit code"
+  description = "Trigger when the organisations sync job has exited with a non-zero exit code"
 
   event_pattern = jsonencode({
     source      = ["aws.ecs"]
@@ -92,15 +92,15 @@ resource "aws_cloudwatch_event_rule" "sync_mailchimp_cron_job_failed" {
     detail = {
       lastStatus = ["STOPPED"]
       containers = {
-        name     = [local.mailchimp_sync_container_definitions.name]
+        name     = [local.organisations_sync_container_definitions.name]
         exitCode = [{ "anything-but" : [0] }]
       }
     }
   })
 }
 
-resource "aws_cloudwatch_event_target" "sync_mailchimp_cron_job_alert_message" {
-  rule = aws_cloudwatch_event_rule.sync_mailchimp_cron_job_failed.name
+resource "aws_cloudwatch_event_target" "sync_orgs_cron_job_alert_message" {
+  rule = aws_cloudwatch_event_rule.sync_orgs_cron_job_failed.name
 
   # defined in 'environment' module. Sends alarms/errors via ZenDesk
   arn = var.zendesk_sns_topic_arn
@@ -108,11 +108,11 @@ resource "aws_cloudwatch_event_target" "sync_mailchimp_cron_job_alert_message" {
   input_transformer {
     input_template = <<EOF
     {
-      "title": "WARNING: Synchronising mailing lists with MailChimp has failed.",
-      "description": "GOV.UK Forms has a scheduled ECS task to sync our Mailchimp mailing list with new users in the users database (only applied in production). When this task fails an email is sent to Zendesk.",
+      "title": "WARNING: Synchronising organisations from GOV.UK has failed.",
+      "description": "GOV.UK Forms has a scheduled ECS task to sync our organisations from GOV.UK. When this task fails an email is sent to Zendesk.",
       "next-steps": {
         "1": "Navigate to Splunk: https://gds.splunkcloud.com/en-GB/app/gds-543-forms/search.",
-        "2": "Search for index=gds_dsp_production_forms log_stream=forms-admin-production-mailchimp-sync/forms-admin_mailchimp_sync/*. Use the 'Today' date-time preset to find today's logs.",
+        "2": "Search for index=gds_dsp_production_forms log_stream=forms-admin-production-organisations-sync/forms-admin_organisations_sync/*. Use the 'Today' date-time preset to find today's logs.",
         "3": "Review logs for errors."
       }
     }
@@ -127,8 +127,8 @@ resource "aws_cloudwatch_event_target" "sync_mailchimp_cron_job_alert_message" {
 ##
 # IAM
 ##
-resource "aws_iam_role" "ecs_mailchimp_cron_scheduler" {
-  count = var.enable_mailchimp_sync ? 1 : 0
+resource "aws_iam_role" "ecs_orgs_cron_scheduler" {
+  count = var.enable_organisations_sync ? 1 : 0
 
   name = "${var.env_name}-forms-admin-ecs-cron-scheduler"
 
@@ -146,9 +146,9 @@ resource "aws_iam_role" "ecs_mailchimp_cron_scheduler" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "ecs_mailchimp_events_policy" {
-  count = var.enable_mailchimp_sync ? 1 : 0
+resource "aws_iam_role_policy_attachment" "ecs_orgs_events_policy" {
+  count = var.enable_organisations_sync ? 1 : 0
 
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceEventsRole"
-  role       = aws_iam_role.ecs_mailchimp_cron_scheduler[0].name
+  role       = aws_iam_role.ecs_orgs_cron_scheduler[0].name
 }
