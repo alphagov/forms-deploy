07c39d5991c7bc018ab3c6d341c9baff018ba840
Sean Rankine <sean.rankine@me.com>
2025-08-20 14:55:40 +0100

    Add CNAME record for custom statuspage domain
    This allows us to use status.forms.service.gov.uk as our statuspage
    domain. Using a gov.uk domain builds trusts and allows us to use GOV.UK
    assets (such as the font and crown). It also makes it easier if we need
    to change statuspage provider.

--- infra/deployments/deploy/account/dns.tf
+++ infra/deployments/deploy/account/dns.tf
@@ -6,3 +6,12 @@ resource "aws_route53_zone" "tools_zone" {
     prevent_destroy = true
   }
 }
+
+resource "aws_route53_zone" "status_zone" {
+  #checkov:skip=CKV2_AWS_39:No desire for DNS query logging
+  #checkov:skip=CKV2_AWS_38:No need for DNSSEC
+  name = "status.forms.service.gov.uk."
+  lifecycle {
+    prevent_destroy = true
+  }
+}
