0050ce33fb64e130c6f26fb6d68391b9758ca0b0
Tom Whitwell <tom.whitwell@digital.cabinet-office.gov.uk>
2025-09-03 11:24:41 +0100

    wip

--- infra/deployments/deploy/secrets/main.tf
+++ infra/deployments/deploy/secrets/main.tf
@@ -33,6 +33,50 @@ resource "aws_cloudwatch_event_target" "forward_to_shared" {
   rule           = aws_cloudwatch_event_rule.forward_secrets.name
   event_bus_name = data.aws_cloudwatch_event_bus.default.name
   arn            = aws_cloudwatch_event_bus.shared.arn
+  role_arn       = aws_iam_role.forward_to_shared.arn
+}
+
+## Role assumed by EventBridge to forward to the custom bus
+data "aws_iam_policy_document" "forward_assume_role" {
+  statement {
+    sid     = "AllowEventsToAssumeForForwarding"
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["events.amazonaws.com"]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceAccount"
+      values   = [data.aws_caller_identity.this.account_id]
+    }
+    condition {
+      test     = "ArnEquals"
+      variable = "aws:SourceArn"
+      values   = [aws_cloudwatch_event_rule.forward_secrets.arn]
+    }
+  }
+}
+
+resource "aws_iam_role" "forward_to_shared" {
+  name               = "evb-forward-secrets-to-shared"
+  assume_role_policy = data.aws_iam_policy_document.forward_assume_role.json
+}
+
+data "aws_iam_policy_document" "forward_put_events" {
+  statement {
+    sid       = "AllowPutEventsToSharedBus"
+    effect    = "Allow"
+    actions   = ["events:PutEvents"]
+    resources = [aws_cloudwatch_event_bus.shared.arn]
+  }
+}
+
+resource "aws_iam_role_policy" "forward_put_events" {
+  name   = "allow-put-to-shared-bus"
+  role   = aws_iam_role.forward_to_shared.id
+  policy = data.aws_iam_policy_document.forward_put_events.json
 }
 
 ## Org-scoped policy on the custom bus, namespaced by caller account ID
