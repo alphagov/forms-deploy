165df43a9ea999e97220e4610de3a9792995f3e3
Sarah Young <sarah.young1@digital.cabinet-office.gov.uk>
2025-09-03 15:37:10 +0100

    Update GDS IP allow list
    * Add the new dual egress IP for GovWifi/Brattain
    * GDS IT notified teams that "As part of Network refresh in the White Chapel Building, our egress IPs for the networks in the building (Brattain and GovWifi) will be changing. Please ensure that you have updated any services that rely on these egress IPs for access from the 15th September."

--- infra/modules/common-values/locals.tf
+++ infra/modules/common-values/locals.tf
@@ -1,6 +1,7 @@
 locals {
 
   vpn_ip_addresses = [
+    "51.149.11.24/29",   # New GovWifi and Brattain (Whitechapel Building Wifi) active 15 September 2025
     "217.196.229.77/32", # GovWifi
     "217.196.229.79/32", # Brattain (Whitechapel Building Wifi)
     "217.196.229.80/32", # BYOD VPN
