ffbc43e80b203dc96d2d759c23d37fcf73f77b75
Tom Whitwell <tom.whitwell@digital.cabinet-office.gov.uk>
2025-09-04 10:30:22 +0100

    wip: move from forms-admin to another deployment

---
+++ infra/deployments/forms/forms-secrets-spike/dependencies.tf
@@ -0,0 +1,11 @@
+data "terraform_remote_state" "forms_environment" {
+  backend = "s3"
+
+  config = {
+    key    = "network.tfstate"
+    bucket = var.bucket
+    region = "eu-west-2"
+
+    use_lockfile = true
+  }
+}
---
+++ infra/deployments/forms/forms-secrets-spike/site.tf
@@ -0,0 +1,22 @@
+terraform {
+  # Comment out when bootstrapping
+  backend "s3" {
+    # bucket set in backend config file
+    key    = "forms-secrets-spike.tfstate"
+    region = "eu-west-2"
+
+    use_lockfile = true
+  }
+}
+
+provider "aws" {
+  allowed_account_ids = var.allowed_account_ids
+
+  default_tags {
+    tags = merge(var.default_tags,
+      {
+        Deployment = "${var.environment_name}/forms-secrets-spike"
+      }
+    )
+  }
+}
