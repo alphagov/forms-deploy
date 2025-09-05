b76fd6bea34ecb7ddb5ca1c43782b74c41585d67
Sarah Young <sarah.young1@digital.cabinet-office.gov.uk>
2025-09-05 11:26:43 +0100

    Add DNS and required internal ALB outputs
    * Add Route53 hosted zone configuration for the internal ALB in our existing VPC. Connect it with the internal domain name
    * We need the Route53 hosted zone config to manage and control how the internal domain is connected to IP addresses and DNS records, as well as traffic routing.
    * Add required internal ALB outputs to the forms root module so it can be used by other modules as necessary
    
    Work completed by sean.rankine@digita.cabinet-office.gov.uk

--- infra/modules/environment/outputs.tf
+++ infra/modules/environment/outputs.tf
@@ -22,6 +22,30 @@ output "alb_main_listener_arn" {
   value = aws_lb_listener.listener.arn
 }
 
+output "internal_alb_arn" {
+  value = aws_lb.internal_alb.arn
+}
+
+output "internal_alb_arn_suffix" {
+  value = aws_lb.internal_alb.arn_suffix
+}
+
+output "internal_alb_dns_name" {
+  value = aws_lb.internal_alb.dns_name
+}
+
+output "internal_alb_listener_arn" {
+  value = aws_lb_listener.internal_listener.arn
+}
+
+output "internal_alb_zone_id" {
+  value = aws_lb.internal_alb.zone_id
+}
+
+output "private_internal_zone_id" {
+  value = aws_route53_zone.private_internal.id
+}
+
 output "cloudfront_arn" {
   value = module.cloudfront[0].cloudfront_arn
 }
