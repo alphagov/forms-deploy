1dd14a48bef7c419b272f7f84d2664d25451d9f1
Tom Whitwell <tom.whitwell@digital.cabinet-office.gov.uk>
2025-09-08 13:09:56 +0100

    Add VPC endpoint for AWS SSM
    Add VPC endpoints for AWS Systems Manager (SSM) to both the main forms
    environment and the deploy tools environment. This allows ECS tasks to
    retrieve SSM parameters without traffic going over the public internet,
    improving security by restricting egress to the internet.
    
    - Added ssm VPC endpoint to forms environment (endpoints.tf)
    - Added ssm VPC endpoint to deploy tools environment (vpc_endpoints.tf)
    - Both endpoints use Interface type with private DNS enabled
    - Endpoints are deployed across all private subnets for high availability

--- infra/modules/environment/endpoints.tf
+++ infra/modules/environment/endpoints.tf
@@ -85,3 +85,20 @@ resource "aws_vpc_endpoint" "cloudwatch" {
     Name = "cloudwatch-endpoint-${var.env_name}"
   }
 }
+
+resource "aws_vpc_endpoint" "ssm" {
+  vpc_id              = aws_vpc.forms.id
+  service_name        = "com.amazonaws.eu-west-2.ssm"
+  vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true
+  security_group_ids  = [aws_security_group.vpc_endpoints.id]
+  subnet_ids = [
+    aws_subnet.private_a.id,
+    aws_subnet.private_b.id,
+    aws_subnet.private_c.id,
+  ]
+
+  tags = {
+    Name = "ssm-endpoint-${var.env_name}"
+  }
+}
