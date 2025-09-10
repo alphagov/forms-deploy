5a3f864ec1c9d557e379b862a4ed3e3a9e2d2f37
Tom Whitwell <tom.whitwell@digital.cabinet-office.gov.uk>
2025-09-10 15:30:04 +0100

    Get forms/auth0 working properly when disabled.
    We previously pinned the provider because auth0 made the settings
    not-nullable. This meant that when Auth0 was disabled and the settings
    were empty strings, the provider would error out.
    
    Instead, pass dummy values to the provider when Auth0 is disabled. The
    credentials are not used because the actual auth0 resources are all
    conditionally created based on whether Auth0 is enabled or not.
    
    This may bite us in future if the provider does more validation of
    the credentials, but for now it lets us keep the provider up to date.

--- infra/deployments/forms/auth0/site.tf
+++ infra/deployments/forms/auth0/site.tf
@@ -19,12 +19,15 @@ data "aws_ssm_parameter" "auth0_client_secret" {
 }
 
 locals {
-  auth0_client_id     = var.environmental_settings.disable_auth0 ? "" : data.aws_ssm_parameter.auth0_client_id[0].value
-  auth0_client_secret = var.environmental_settings.disable_auth0 ? "" : data.aws_ssm_parameter.auth0_client_secret[0].value
+  # When Auth0 is disabled, we set these to dummy values to avoid provider errors.
+  # The actual Auth0 resources will not be created because the module is not instantiated.
+  auth0_domain        = var.environmental_settings.disable_auth0 ? "DISABLED_INVALID" : var.environmental_settings.auth0_domain
+  auth0_client_id     = var.environmental_settings.disable_auth0 ? "DISABLED_INVALID" : data.aws_ssm_parameter.auth0_client_id[0].value
+  auth0_client_secret = var.environmental_settings.disable_auth0 ? "DISABLED_INVALID" : data.aws_ssm_parameter.auth0_client_secret[0].value
 }
 
 provider "auth0" {
-  domain = var.environmental_settings.auth0_domain
+  domain = local.auth0_domain
 
   client_id     = local.auth0_client_id
   client_secret = local.auth0_client_secret
