aa2b81826baa5dfe8e1ae4210d846e8b342c1939
whi-tw <3595020+whi-tw@users.noreply.github.com>
2025-09-24 15:23:45 +0100

    Revert "CRUD SLOs in terraform"

--- infra/deployments/forms/health/outputs.tf
+++ infra/deployments/forms/health/outputs.tf
@@ -9,3 +9,15 @@ data "aws_lb_target_group" "forms_admin_tg" {
 data "aws_lb_target_group" "forms_runner_tg" {
   arn = data.terraform_remote_state.forms_runner.outputs.target_group_arn
 }
+
+output "load_balancer_name" {
+  value = data.aws_lb.forms_lb.arn_suffix
+}
+
+output "forms_admin_target_group_name" {
+  value = data.aws_lb_target_group.forms_admin_tg.arn_suffix
+}
+
+output "forms_runner_target_group_name" {
+  value = data.aws_lb_target_group.forms_runner_tg.arn_suffix
+}
