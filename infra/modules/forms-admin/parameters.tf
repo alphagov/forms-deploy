be49ee0fc114455d521d5c84278737c98ad68304
Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>
2025-10-01 15:19:08 +0100

    Delete SSM parameters for forms-api tokens
    These are no longer used, so can be deleted.

--- infra/modules/forms-admin/parameters.tf
+++ infra/modules/forms-admin/parameters.tf
@@ -46,21 +46,6 @@ resource "aws_ssm_parameter" "sentry_dsn" {
   }
 }
 
-resource "aws_ssm_parameter" "forms_api_key" {
-  #checkov:skip=CKV_AWS_337:KMS managed key is fine
-  name  = "/forms-admin-${var.env_name}/forms-api-key"
-  type  = "SecureString"
-  value = "dummy_value"
-
-  description = "API key to access forms-api"
-
-  lifecycle {
-    ignore_changes = [
-      value
-    ]
-  }
-}
-
 resource "aws_ssm_parameter" "notify_api_key" {
   #checkov:skip=CKV_AWS_337:The parameter is already using the default key
 
--- infra/modules/forms-runner/parameters.tf
+++ infra/modules/forms-runner/parameters.tf
@@ -1,16 +1,3 @@
-resource "aws_ssm_parameter" "forms_api_key" {
-  #checkov:skip=CKV_AWS_337:KMS managed key is fine
-  name  = "/forms-runner-${var.env_name}/forms-api-key"
-  type  = "SecureString"
-  value = "dummy_value"
-
-  description = "API key to access forms-api"
-
-  lifecycle {
-    ignore_changes = [value]
-  }
-}
-
 resource "aws_ssm_parameter" "notify_api_key" {
   #checkov:skip=CKV_AWS_337:KMS managed key is fine
   name  = "/forms-runner-${var.env_name}/notify-api-key"
