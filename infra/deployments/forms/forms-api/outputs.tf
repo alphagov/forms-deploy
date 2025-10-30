6dcfb32b42ad863ed9fd45d0dbafafeca19541ce
Catalina Garcia <catalina.garcia@digital.cabinet-office.gov.uk>
2025-10-30 12:51:58 +0000

    Remove forms-api (clean up code)
    We have already remove forms-api from our infrastructure (https://github.com/alphagov/forms-deploy/pull/1769). This removes it from
    our code

--- infra/deployments/forms/forms-api/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "task_definition_family" {
-  value = module.forms_api.task_definition_family
-}
--- infra/modules/forms-api/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "task_definition_family" {
-  value = module.ecs_service.task_definition_family
-}
