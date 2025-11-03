a8742ede19549b1bd17f553f04417c24e73ac432
Catalina Garcia <catalina.garcia@digital.cabinet-office.gov.uk>
2025-11-03 12:04:50 +0000

    Create second destination in us-east-1 to same kinesis stream
    We have some logs groups in us-east-1. When we create a subscription, the
    subscription filter has to live in the same region, and we cannot set a
    destination in a different region.
    
    So we create everything (log group, subscription filter and destination) in
    us-east-1, but target the kinesis stream in eu-west-2

--- infra/deployments/deploy/tools/log_to_splunk/outputs.tf
+++ infra/deployments/deploy/tools/log_to_splunk/outputs.tf
@@ -2,3 +2,8 @@ output "destination_arn" {
   description = "Source account Cloudwatch Logs Destination ARN"
   value       = aws_cloudwatch_log_destination.kinesis_log_destination.arn
 }
+
+output "destination_arn_us_east_1" {
+  description = "Source account Cloudwatch Logs Destination ARN in the us-east-1 region"
+  value       = aws_cloudwatch_log_destination.kinesis_log_destination_us_east_1.arn
+}
