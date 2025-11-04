69208c2b461cf8b34216078de555dc948323a82e
Tom Whitwell <tom.whitwell@digital.cabinet-office.gov.uk>
2025-11-04 12:51:38 +0000

    BAU: Switch to using mise for hcl2json in GHA
    This allows us to manage the version of hcl2json in a single place
    (.mise.toml) rather than hardcoding it in each workflow.
    
    Also, improve the mise compatibility in `forms/pipelines/pre-init.sh`:
    - Explicitly check if rbenv is available before using it, otherwise
      fall back to using mise to show the active Ruby version.
    - Change the mise command used to show the version to match rbenv's format

--- infra/deployments/forms/pipelines/pre-init.sh
+++ infra/deployments/forms/pipelines/pre-init.sh
@@ -7,24 +7,27 @@
 
 repo_root="${1}"
 
-init_ruby_project(){
-    DIR="${1}"
-    (
-        echo "Initialising Ruby project at ${DIR}"
-        # shellcheck disable=SC2164
-        # we're confident the directories will exist
-        cd "${DIR}";
+init_ruby_project() {
+  DIR="${1}"
+  (
+    echo "Initialising Ruby project at ${DIR}"
+    # shellcheck disable=SC2164
+    # we're confident the directories will exist
+    cd "${DIR}"
 
-        # Use already installed Ruby if running in AWS CodeBuild
-        if [ "${CODEBUILD_CI:-false}" = true ]; then
-          RBENV_VERSION="$(rbenv global)"
-          export RBENV_VERSION
-        fi
+    # Use already installed Ruby if running in AWS CodeBuild
+    if [ "${CODEBUILD_CI:-false}" = true ]; then
+      RBENV_VERSION="$(rbenv global)"
+      export RBENV_VERSION
+    fi
+    if command -v rbenv &>/dev/null; then
+      rbenv version
+    else
+      mise current ruby
+    fi
 
-        rbenv version || mise list -c ruby
-
-        BUNDLE_PATH="vendor/bundle" BUNDLE_WITHOUT="development:test" bundle install
-    )
+    BUNDLE_PATH="vendor/bundle" BUNDLE_WITHOUT="development:test" bundle install
+  )
 }
 
 init_ruby_project "${repo_root}/infra/deployments/forms/pipelines/pipeline-invoker"
