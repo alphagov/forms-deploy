be7a9c02435e80d51865059772f19cdd037f8356
Thomas Iles <tomiles@gmail.com>
2025-11-05 13:27:28 +0000

    Restore data_api cli tool
    When the API was removed, the data_api command was removed from the CLI tool in this PR:
    https://github.com/alphagov/forms-deploy/pull/1769
    
    This broke the CLI tool as it still requires the file when starting up.
    
    I think it was a mistake to remove the command.
    
    This commit restores the data_api command and tests exactly as they
    were.

---
+++ support/forms-cli/lib/commands/data_api.rb
@@ -0,0 +1,83 @@
+# frozen_string_literal: true
+
+require "colorize"
+require_relative "../utilities/data_api_connection"
+require_relative "../utilities/helpers"
+
+# Executes statements on AWS RDS using the Data API.
+class DataApi
+  include Helpers
+
+  def run
+    @options = {}
+    parse_options
+    return unless aws_authenticated? && valid_options?
+
+    @connection = DataApiConnection.new(fetch_environment, @options[:database], @options[:cluster])
+
+    begin
+      print execute_statement
+    rescue RuntimeError => e
+      puts "Something went wrong: #{e.message}".red
+    end
+  end
+
+private
+
+  def print(results)
+    puts JSON.pretty_generate({
+      updated: results.number_of_records_updated,
+      records: results.records,
+    })
+  end
+
+  def valid_options?
+    %i[statement database].each do |arg|
+      if @options[arg].nil?
+        puts "#{arg} must be provided".red
+        return false
+      end
+    end
+
+    unless %w[forms-api forms-admin forms-runner].include? @options[:database]
+      puts "database must be either 'forms-api', 'forms-admin' or 'forms-runner'".red
+      return false
+    end
+
+    true
+  end
+
+  def parse_options
+    OptionParser.new { |opts|
+      opts.banner = "
+      Executes the provided statement on the provide database for the currently
+      authenticated shell.
+
+      Run in a authorized shell using gds-cli or aws-vault.
+
+      Example:
+      gds aws forms-dev-support -- forms data_api --database forms-api --cluster aurora-v2-cluster-dev --statement 'select * from forms;'\n\n"
+
+      opts.on("-h", "--help", "Prints help") do
+        puts opts
+        exit
+      end
+
+      opts.on("-dDATABASE", "--database=DATABASE", "[Mandatory] database to query, forms-api forms-admin") do |database|
+        @options[:database] = database
+      end
+
+      opts.on("-cCLUSTER", "--cluster=CLUSTER", "The cluster the database is in") do |cluster|
+        @options[:cluster] = cluster
+      end
+
+      opts.on("-sSTATEMENT", "--statement=STATEMENT", "[Mandatory] The statement to execute") do |statement|
+        @options[:statement] = statement
+      end
+    }.parse!
+  end
+
+  def execute_statement
+    @connection.execute_statement(@options[:statement])
+  end
+end
---
+++ support/forms-cli/spec/commands/data_api_spec.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+require "commands/data_api"
+require "utilities/data_api_connection"
+
+require_relative "../fixtures/rdsdataservice"
+
+describe DataApi do
+  context "when not authenticated" do
+    it "prompts the user to authenticate" do
+      expect { described_class.new.run }.to output(/You must be authenticated/).to_stdout
+    end
+  end
+
+  context "when authenticated" do
+    let(:data_api_connection_mock) do
+      data_api_connection_mock = instance_double(DataApiConnection)
+      allow(data_api_connection_mock)
+        .to receive(:execute_statement)
+        .and_return(RDSDataServiceFixtures.execute_statement)
+
+      data_api_connection_mock
+    end
+
+    before do
+      stub_const("ARGV", ["-d", "forms-api", "-c", "cluster-name", "-s", "select * from testing;"])
+
+      allow_any_instance_of(Helpers) # rubocop:todo RSpec/AnyInstance
+        .to receive_messages(aws_authenticated?: true, fetch_environment: "dev")
+
+      allow(DataApiConnection)
+        .to receive(:new)
+        .and_return(data_api_connection_mock)
+    end
+
+    it "-d, --database and -c, --cluster are correctly passed to DataApiConnection" do
+      described_class.new.run
+
+      expect(DataApiConnection)
+        .to have_received(:new)
+        .with("dev", "forms-api", "cluster-name")
+        .at_least(:once)
+    end
+
+    it "-s, --statement is correctly passed to DataApiConnection" do
+      described_class.new.run
+
+      expect(data_api_connection_mock)
+        .to have_received(:execute_statement)
+        .with("select * from testing;")
+        .at_least(:once)
+    end
+  end
+end
