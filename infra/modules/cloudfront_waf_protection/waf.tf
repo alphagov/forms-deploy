5e516a5dc6a91987c27fc153b1a11a637a1faf36
Tom Whitwell <tom.whitwell@digital.cabinet-office.gov.uk>
2025-11-13 10:54:46 +0000

    BAU: Enable AWSManagedRulesKnownBadInputsRuleSet
    This specifically enables protection against the items listed here:
    https://docs.aws.amazon.com/waf/latest/developerguide/aws-managed-rule-groups-baseline.html#aws-managed-rule-groups-baseline-known-bad-inputs

--- infra/modules/cloudfront_waf_protection/waf.tf
+++ infra/modules/cloudfront_waf_protection/waf.tf
@@ -419,6 +419,25 @@ resource "aws_wafv2_web_acl" "this" {
   }
 
   rule {
+    name     = "AWS-AWSManagedRulesKnownBadInputsRuleSet"
+    priority = 6
+    override_action {
+      none {}
+    }
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesKnownBadInputsRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWS-AWSManagedRulesKnownBadInputsRuleSet"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
     name     = "OriginIPRateLimit"
     priority = 100
 
