116291f36f51a541e0425c0e83e20b39bf9c41c0
Catalina Garcia <catalina.garcia@digital.cabinet-office.gov.uk>
2025-11-27 16:46:29 +0000

    Create log group and send logs to Splunk

--- infra/modules/forms-runner/queue_worker.tf
+++ infra/modules/forms-runner/queue_worker.tf
@@ -195,3 +195,27 @@ resource "aws_ssm_parameter" "queue_worker_sentry_dsn" {
     prevent_destroy = true
   }
 }
+
+resource "aws_cloudwatch_log_group" "queue_worker" {
+  #checkov:skip=CKV_AWS_338:We're happy with 30 days retention for now
+  #checkov:skip=CKV_AWS_158:Default AWS SSE is sufficient, no need for CM KMS.
+  name              = local.queue_worker_log_group_name
+  retention_in_days = 30
+}
+
+module "cribl_well_known" {
+  source = "../well-known/cribl"
+}
+
+resource "aws_cloudwatch_log_subscription_filter" "via_cribl_to_splunk" {
+  count = var.kinesis_subscription_role_arn != "" ? 1 : 0
+
+  name = "via-cribl-to-splunk"
+
+  log_group_name = aws_cloudwatch_log_group.queue_worker.name
+
+  filter_pattern  = ""
+  destination_arn = module.cribl_well_known.kinesis_destination_arns["eu-west-2"]
+  distribution    = "ByLogStream"
+  role_arn        = var.kinesis_subscription_role_arn
+}
