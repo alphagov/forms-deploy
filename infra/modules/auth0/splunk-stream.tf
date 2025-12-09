06cabe433be47af38b985ddd8494e9fa06bdfe83
Catalina Garcia <catalina.garcia@digital.cabinet-office.gov.uk>
2025-12-09 10:55:00 +0000

    Add new log stream via CRIBL
    Cyber would like us to have the logs go through CRIBL first for pre-processing.
    We can setup the sink in a similar way to a Splunk one, just different
    domain/port

--- infra/modules/auth0/splunk-stream.tf
+++ infra/modules/auth0/splunk-stream.tf
@@ -12,6 +12,21 @@ resource "auth0_log_stream" "splunk_log_stream" {
   }
 }
 
+resource "auth0_log_stream" "to_splunk" {
+  # Note that the splunk_domain is actually for CRIBL, which pre-processes the logs
+  count = var.enable_splunk_log_stream ? 1 : 0
+
+  name = "Auth0 to Splunk log stream"
+  type = "splunk"
+
+  sink {
+    splunk_domain = "gds-general.main.heuristic-bondo-ebxaqj9.cribl.cloud"
+    splunk_port   = "8088"
+    splunk_secure = true
+    splunk_token  = data.aws_ssm_parameter.splunk_hec_token.value
+  }
+}
+
 resource "aws_ssm_parameter" "splunk_hec_token" {
 
   #checkov:skip=CKV_AWS_337:The parameter is already using the default key
