1b6189c4c0e21f70e20311f52ac751308665db66
alice-carr <alice.carr@digital.cabinet-office.gov.uk>
2026-01-06 11:55:49 +0000

    Add cronjob to run org list sync
    This PR runs a cronjob every tuesday at 1130 that makes a rake call that fetches all organisations and syncs them to update the orgs list. We are currently running this as a manual job - the commit runs it automatically.

--- infra/deployments/forms/inputs.tf
+++ infra/deployments/forms/inputs.tf
@@ -129,18 +129,19 @@ EOF
 variable "forms_admin_settings" {
   description = "Forms Admin configuration values"
   type = object({
-    cpu                        = number
-    memory                     = number
-    min_capacity               = number
-    max_capacity               = number
-    enable_maintenance_mode    = bool
-    auth_provider              = string
-    previous_auth_provider     = string
-    cloudwatch_metrics_enabled = bool
-    analytics_enabled          = bool
-    act_as_user_enabled        = bool
-    govuk_app_domain           = string
-    synchronize_to_mailchimp   = bool
+    cpu                         = number
+    memory                      = number
+    min_capacity                = number
+    max_capacity                = number
+    enable_maintenance_mode     = bool
+    auth_provider               = string
+    previous_auth_provider      = string
+    cloudwatch_metrics_enabled  = bool
+    analytics_enabled           = bool
+    act_as_user_enabled         = bool
+    govuk_app_domain            = string
+    synchronize_to_mailchimp    = bool
+    synchronize_orgs_from_govuk = bool
   })
   nullable = false
 }
