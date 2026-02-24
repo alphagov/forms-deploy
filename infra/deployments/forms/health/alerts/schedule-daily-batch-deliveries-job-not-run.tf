aac3e4382f43905520c253eb695e4c3b817f2dc8
Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>
2026-02-24 17:56:15 +0000

    Add alarm for the ScheduleDailyBatchDeliveriesJob not running
    Add an alarm that will fire if the ScheduleDailyBatchDeliveriesJob
    does not run for over a day. If this job does not run, then batch
    submission deliveries will not be sent out.
    
    Alarm if there are 25 consecutive 1-hour periods with no runs of the
    ScheduleDailyBatchDeliveriesJob. This allows for some flexibility in
    the schedule in the case of downtime but will still alert us if the
    batch isn't sent out for the day.
    
    Alert at info level, meaning that we send an email to the infra
    test email for dev/staging and to Zendesk for production.

---
+++ infra/deployments/forms/health/alerts/schedule-daily-batch-deliveries-job-not-run.tf
@@ -0,0 +1,31 @@
+resource "aws_cloudwatch_metric_alarm" "schedule_daily_batch_deliveries_job_not_run" {
+  alarm_name          = "${var.environment}-schedule-daily-batch-deliveries-job-not-run"
+  alarm_description   = <<EOF
+    The forms-runner job to schedule daily batch submission deliveries has not run in the ${var.environment} environment
+    in the past 25 hours. It is expected that the job is run every day. This job is run by Solid Queue, which is started
+    in the forms-runner ECS task.
+
+    NEXT STEPS:
+    1. Check the Splunk logs and Sentry for any errors running the job.
+    2. Restart the forms-runner ECS tasks and check whether the job starts running.
+
+EOF
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = 25
+  metric_name         = "Started"
+  namespace           = "Forms/Jobs"
+  period              = 3600
+  statistic           = "SampleCount"
+  threshold           = 0
+
+  dimensions = {
+    Environment = "${var.environment}"
+    ServiceName = "forms-runner"
+    JobName     = "ScheduleDailyBatchDeliveriesJob"
+  }
+
+  treat_missing_data = "breaching"
+
+  alarm_actions = [local.alert_severity.eu_west_2.info]
+  ok_actions    = [local.alert_severity.eu_west_2.info]
+}
