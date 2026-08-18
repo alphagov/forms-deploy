78ad4c742c34aa072bcead9834ef1cecad6f9bac
Catalina Garcia <catalina.garcia@digital.cabinet-office.gov.uk>
2026-08-18 12:51:20 +0100

    Give the health root access to the product-page target group

---
+++ infra/deployments/forms/forms-product-page/outputs.tf
@@ -0,0 +1,3 @@
+output "target_group_arn" {
+  value = module.forms_product_page.target_group_arn
+}
--- infra/deployments/forms/health/lookups.tf
+++ infra/deployments/forms/health/lookups.tf
@@ -10,6 +10,10 @@ data "aws_lb_target_group" "forms_runner_tg" {
   arn = data.terraform_remote_state.forms_runner.outputs.target_group_arn
 }
 
+data "aws_lb_target_group" "forms_product_page_tg" {
+  arn = data.terraform_remote_state.forms_product_page.outputs.target_group_arn
+}
+
 data "aws_s3_bucket" "logs_bucket" {
   bucket = "govuk-forms-alb-logs-${var.environment_name}"
 }
